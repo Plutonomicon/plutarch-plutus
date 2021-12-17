f20b8ce1b52c0995a46417e9bd22d086df54dacb
Las Safin <me@las.rs>
2021-12-17 10:18:59 +0000

    Add pif'

--- Plutarch/Bool.hs
+++ Plutarch/Bool.hs
@@ -1,16 +1,16 @@
-module Plutarch.Bool (PBool (..), PEq (..), POrd (..), pif) where
+module Plutarch.Bool (PBool (..), PEq (..), POrd (..), pif, pif') where
 
-import Plutarch (POpaque, PlutusType (PInner, pcon', pmatch'), punsafeBuiltin, punsafeConstant)
+import Plutarch (PlutusType (PInner, pcon', pmatch'), punsafeBuiltin, punsafeConstant)
 import Plutarch.Prelude
 import qualified PlutusCore as PLC
 
 data PBool s = PTrue | PFalse
 
 instance PlutusType PBool where
-  type PInner PBool _ = POpaque
+  type PInner PBool _ = PBool
   pcon' PTrue = punsafeConstant . PLC.Some $ PLC.ValueOf PLC.DefaultUniBool True
   pcon' PFalse = punsafeConstant . PLC.Some $ PLC.ValueOf PLC.DefaultUniBool False
-  pmatch' b f = pforce $ pforce (punsafeBuiltin PLC.IfThenElse) £ b £ pdelay (f PTrue) £ pdelay (f PFalse)
+  pmatch' b f = pforce $ pif' £ b £ pdelay (f PTrue) £ pdelay (f PFalse)
 
 class PEq t where
   (£==) :: Term s t -> Term s t -> Term s PBool
@@ -24,6 +24,13 @@ class POrd t where
 infix 4 £<=
 infix 4 £<
 
+{- | Strict version of 'pif'.
+ Emits slightly less code.
+-}
+pif' :: Term s (PBool :--> a :--> a :--> a)
+pif' = phoistAcyclic $ pforce $ punsafeBuiltin PLC.IfThenElse
+
+-- | Lazy if-then-else.
 pif :: Term s PBool -> Term s a -> Term s a -> Term s a
 pif b case_true case_false = pmatch b $ \case
   PTrue -> case_true
