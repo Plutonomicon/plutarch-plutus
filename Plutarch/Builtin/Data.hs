918c0e4e5842fbc9b9c381611262a8abc94a606b
Sridhar Ratnakumar <srid@srid.ca>
2021-12-21 16:14:34 -0500

    Delay ChooseData's args

--- Plutarch/Builtin/Data.hs
+++ Plutarch/Builtin/Data.hs
@@ -3,10 +3,14 @@
 module Plutarch.Builtin.Data (
   ) where
 
+import Data.Data
 import Plutarch
-import Plutarch.Builtin ((#£))
+import Plutarch.Builtin
 import qualified Plutarch.Builtin as B
 import Plutarch.Builtin.Data.Type
+import Plutarch.Builtin.List.Type
+import qualified PlutusCore.Data as PLC
+import qualified PlutusCore.Default as PLC
 
 instance PlutusType PData where
   type PInner PData _ = POpaque
@@ -16,9 +20,14 @@ instance PlutusType PData where
     _ ->
       undefined
   pmatch' x f =
-    B.ChooseData #£ x
-      £ f (PDataConstr (B.UnConstrData #£ x))
-      £ perror
-      £ perror
-      £ perror
-      £ perror
+    pforce $
+      B.ChooseData #£ x
+        £ pdelay (f (PDataConstr (B.UnConstrData #£ x)))
+        £ pdelay perror
+        £ pdelay perror
+        £ pdelay perror
+        £ pdelay perror
+
+instance ListElemUni PData where
+  type ListElemType PData = PLC.Data
+  listElemUni Proxy = PLC.DefaultUniData
