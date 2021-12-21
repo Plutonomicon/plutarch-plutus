c1c40cec22bb28c7b7dad026cf6d2256496b0429
Sridhar Ratnakumar <srid@srid.ca>
2021-12-21 14:18:14 -0500

    Add rest of list elem instances

--- Plutarch/Builtin/List/Type.hs
+++ Plutarch/Builtin/List/Type.hs
@@ -3,14 +3,19 @@ module Plutarch.Builtin.List.Type (
   ListElemUni (..),
 ) where
 
-import Data.Proxy
-import Plutarch
+import Data.Proxy ( Proxy(..) )
+import Plutarch ( POpaque, Term )
 import Plutarch.Integer (PInteger)
-import Plutarch.Prelude
+import Plutarch.Prelude ( Type )
 import qualified PlutusCore as PLC
 import qualified PlutusCore.Data as PLC
+import Plutarch.String (PString)
+import Data.Text (Text)
+import Plutarch.ByteString (PByteString)
+import qualified Data.ByteString as BS
+import Plutarch.Unit (PUnit)
+import Plutarch.Bool (PBool)
 
--- | A builtin list of `Data`.
 data PList a s
   = PNil
   | PCons (Term s a) (Term s (PList a))
@@ -19,10 +24,25 @@ class ListElemUni (a :: k -> Type) where
   type ListElemType a :: Type
   listElemUni :: Proxy a -> PLC.DefaultUni (PLC.Esc (ListElemType a))
 
+instance ListElemUni POpaque where
+  type ListElemType POpaque = PLC.Data
+  listElemUni Proxy = PLC.DefaultUniData
 instance ListElemUni PInteger where
   type ListElemType PInteger = Integer
   listElemUni Proxy = PLC.DefaultUniInteger
 
-instance ListElemUni POpaque where
-  type ListElemType POpaque = PLC.Data
-  listElemUni Proxy = PLC.DefaultUniData
+instance ListElemUni PString where 
+  type ListElemType PString = Text
+  listElemUni Proxy = PLC.DefaultUniString
+
+instance ListElemUni PByteString where 
+  type ListElemType PByteString = BS.ByteString 
+  listElemUni Proxy = PLC.DefaultUniByteString 
+
+instance ListElemUni PUnit where 
+  type ListElemType PUnit = ()
+  listElemUni Proxy = PLC.DefaultUniUnit  
+
+instance ListElemUni PBool where 
+  type ListElemType PBool = Bool
+  listElemUni Proxy = PLC.DefaultUniBool
