f99d1fd9d3d144036d83320117aa2060ab0350df
Sridhar Ratnakumar <srid@srid.ca>
2021-12-21 11:54:36 -0500

    Add Builtin GADT, PList and PPair, with fib example (WIP)

--- Plutarch/Builtin.hs
+++ Plutarch/Builtin.hs
@@ -1,9 +1,35 @@
-module Plutarch.Builtin (PData (..), pfstBuiltin, psndBuiltin, pasConstr, PBuiltinPair, PBuiltinList) where
+module Plutarch.Builtin (
+  PData (..),
+  pfstBuiltin,
+  psndBuiltin,
+  pasConstr,
+  PBuiltinPair,
+  PBuiltinList,
+  pTrace,
+  (#£),
+  (!£),
+  singleton,
+  hasElem,
+  atIndex,
+  append,
+  cons,
+  nil,
+  mkList,
+  headL,
+  PBuiltin (..),
+  PListData (..),
+  PPairData,
+) where
 
-import Plutarch (punsafeBuiltin)
+import Data.Proxy
+import Data.Text (Text)
+import Data.Type.Nat
+import Plutarch
+import Plutarch.Bool
 import Plutarch.ByteString (PByteString)
 import Plutarch.Integer (PInteger)
 import Plutarch.Prelude
+import Plutarch.String
 import qualified PlutusCore as PLC
 
 data PBuiltinPair (a :: k -> Type) (b :: k -> Type) (s :: k)
@@ -25,3 +51,188 @@ psndBuiltin = phoistAcyclic $ pforce . pforce . punsafeBuiltin $ PLC.SndPair
 
 pasConstr :: Term s (PData :--> PBuiltinPair PInteger (PBuiltinList PData))
 pasConstr = punsafeBuiltin PLC.UnConstrData
+
+-- Builtins
+
+{- | Type spec for PLC's untyped builtin
+
+ The `forces` value determines the repeated application of `FORCE` when
+ evaluating the builtin function.
+
+ Example: UnConstrData #£ someData
+-}
+data PBuiltin (forces :: Nat) (args :: [k -> Type]) (res :: k -> Type) where
+  UnConstrData :: PBuiltin Nat0 '[POpaque] (PPairData PInteger (PListData POpaque))
+  UnListData :: PBuiltin Nat0 '[POpaque] (PListData POpaque)
+  MkPairData :: PBuiltin Nat0 '[a, b] (PPairData a b)
+  FstPair :: PBuiltin Nat2 '[PPairData a b] a
+  SndPair :: PBuiltin Nat2 '[PPairData a b] b
+  MkCons :: PBuiltin Nat1 '[a, PListData a] (PListData a)
+  NullList :: PBuiltin Nat1 '[a] PBool
+  HeadList :: PBuiltin Nat1 '[PListData a] a
+  TailList :: PBuiltin Nat1 '[PListData a] (PListData a)
+  EqualsData :: PBuiltin Nat0 '[POpaque, POpaque] PBool
+  IData :: PBuiltin Nat0 '[PInteger] POpaque
+  UnIData :: PBuiltin Nat0 '[POpaque] PInteger
+  Trace :: PBuiltin Nat1 '[PString, a] a
+
+type family PBuiltinType (args :: [k -> Type]) (res :: k -> Type) where
+  PBuiltinType '[] res = res
+  PBuiltinType (a ': as) res = a :--> PBuiltinType as res
+
+pBuiltinTerm ::
+  forall args res forces s.
+  PBuiltin forces args res ->
+  Term s (PBuiltinType args res)
+pBuiltinTerm b =
+  phoistAcyclic $ case b of
+    UnConstrData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.UnConstrData
+    UnListData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.UnListData
+    MkPairData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.MkPairData
+    FstPair ->
+      force @forces Proxy . punsafeBuiltin $ PLC.FstPair
+    SndPair ->
+      force @forces Proxy . punsafeBuiltin $ PLC.SndPair
+    MkCons ->
+      force @forces Proxy . punsafeBuiltin $ PLC.MkCons
+    NullList ->
+      force @forces Proxy . punsafeBuiltin $ PLC.NullList
+    HeadList ->
+      force @forces Proxy . punsafeBuiltin $ PLC.HeadList
+    TailList ->
+      force @forces Proxy . punsafeBuiltin $ PLC.TailList
+    EqualsData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.EqualsData
+    IData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.IData
+    UnIData ->
+      force @forces Proxy . punsafeBuiltin $ PLC.UnIData
+    Trace ->
+      force @forces Proxy . punsafeBuiltin $ PLC.Trace
+  where
+    force :: forall forces s a. SNatI forces => Proxy (forces :: Nat) -> Term s a -> Term s a
+    force Proxy =
+      let sn = snat :: SNat forces
+       in forceN (snatToNat sn)
+    forceN :: forall s a. Nat -> Term s a -> Term s a
+    forceN Z = id
+    forceN (S n) = pforce . punsafeCoerce . forceN n
+
+(#£) ::
+  forall
+    k
+    (args :: [k -> Type])
+    (res :: k -> Type)
+    (a :: k -> Type)
+    (b :: k -> Type)
+    (forces :: Nat)
+    (s :: k).
+  (PBuiltinType args res ~ (a :--> b)) =>
+  PBuiltin forces args res ->
+  Term s a ->
+  Term s b
+(#£) b = (pBuiltinTerm b £)
+infixl 9 #£
+
+-- Handy builtin aliases
+
+pTrace :: Text -> Term s a -> Term s a
+pTrace s f = Trace #£ pfromText s £ f
+
+(!£) :: forall k (s :: k) (a :: k -> Type). Text -> Term s a -> Term s a
+(!£) = pTrace
+infixl 8 !£
+
+-- | A builtin list of `Data`.
+data PListData a s
+  = PNil
+  | PCons (Term s a) (Term s (PListData a))
+
+instance PlutusType (PListData a) where
+  type PInner (PListData a) _ = PListData a
+  pcon' PNil =
+    punsafeConstant $
+      PLC.Some $
+        PLC.ValueOf (PLC.DefaultUniProtoList `PLC.DefaultUniApply` PLC.DefaultUniData) []
+  pcon' (PCons x xs) = MkCons #£ x £ xs
+  pmatch' list f =
+    plet (NullList #£ list) $ \isEmpty ->
+      pif
+        (punsafeCoerce isEmpty)
+        (f PNil)
+        $ plet
+          (HeadList #£ list)
+          ( \head ->
+              plet (TailList #£ list) $ \tail ->
+                f $ PCons head tail
+          )
+
+cons :: forall k (s :: k) (a :: k -> Type). Term s a -> Term s (PListData a) -> Term s (PListData a)
+cons x xs = pcon' $ PCons x xs
+
+nil :: forall k (s :: k) (a :: k -> Type). Term s (PListData a)
+nil = pcon' PNil
+
+mkList :: forall k (s :: k) (a :: k -> Type). [Term s a] -> Term s (PListData a)
+mkList = \case
+  [] -> nil
+  (x : xs) -> cons x (mkList xs)
+
+headL :: forall k (s :: k) (c :: k -> Type). Term s (PListData c) -> Term s c
+headL list =
+  pmatch' list $ \case
+    PNil -> perror
+    PCons x _ -> x
+
+singleton :: Term s (a :--> PListData a)
+singleton =
+  plam $ \x ->
+    pcon' (PCons x $ pcon' PNil)
+
+hasElem :: PEq a => ClosedTerm (a :--> PListData a :--> PBool)
+hasElem =
+  pfix £$ plam $ \self k list ->
+    pmatch' list $ \case
+      PNil ->
+        pcon PFalse
+      PCons x xs ->
+        pif
+          (k £== x)
+          (pcon PTrue)
+          (self £ k £ xs)
+
+atIndex :: ClosedTerm (PInteger :--> PListData a :--> a)
+atIndex =
+  pfix £$ plam $ \self n' list ->
+    pmatch' ("plu:n" !£ list) $ \case
+      PNil ->
+        "plu:atIndex:err"
+          !£ perror
+      PCons x xs ->
+        pif
+          (n' £== 0)
+          x
+          (self £ (n' - 1) £ xs)
+
+append :: ClosedTerm (PListData a :--> PListData a :--> PListData a)
+append =
+  pfix £$ plam $ \self list1 list2 ->
+    pmatch' ("plu:l1" !£ list1) $ \case
+      PNil ->
+        list2
+      PCons x xs ->
+        pcon' (PCons x $ self £ xs £ list2)
+
+data PPairData a b s = PPairData (Term s a) (Term s b)
+
+instance PlutusType (PPairData a b) where
+  type PInner (PPairData a b) _ = PPairData a b
+  pcon' (PPairData a b) = MkPairData #£ a £ b -- There is no MkPair
+  pmatch' pair f =
+    -- TODO: use delay/force to avoid evaluating `pair` twice?
+    plet (FstPair #£ pair) $ \a ->
+      plet (SndPair #£ pair) $ \b ->
+        f $ PPairData a b
--- examples/Main.hs
+++ examples/Main.hs
@@ -5,8 +5,12 @@ module Main (main) where
 import Test.Tasty
 import Test.Tasty.HUnit
 
-import Plutarch (ClosedTerm, compile, printScript, printTerm)
+import Plutarch (ClosedTerm, POpaque, compile, printScript, printTerm)
 import Plutarch.Bool (PBool (PTrue), pif, (£==))
+import Plutarch.Builtin (
+  (#£),
+ )
+import qualified Plutarch.Builtin as B
 import Plutarch.ByteString (phexByteStr)
 import Plutarch.Either (PEither (PLeft, PRight))
 import Plutarch.Evaluate (evaluateScript)
@@ -44,6 +48,21 @@ fib = phoistAcyclic $
         1
         $ self £ (n - 1) + self £ (n - 2)
 
+fibs :: Term s (PInteger :--> B.PListData POpaque)
+fibs = phoistAcyclic $
+  pfix £$ plam $ \self n ->
+    pif
+      (n £== 0)
+      (B.singleton £ (B.IData #£ 0))
+      $ pif
+        (n £== 1)
+        (B.mkList [B.IData #£ 1, B.IData #£ 0])
+        $ plet (self £ (n - 1)) $ \a ->
+          plet (self £ (n - 2)) $ \b ->
+            B.cons
+              (B.IData #£ ((B.UnIData #£ B.headL a) + (B.UnIData #£ B.headL b)))
+              a
+
 uglyDouble :: Term s (PInteger :--> PInteger)
 uglyDouble = plam $ \n -> plet n $ \n1 -> plet n1 $ \n2 -> n2 + n2
 
@@ -71,34 +90,37 @@ expect = equal (pcon PTrue :: Term s PBool)
 -- FIXME: Use property tests
 tests :: TestTree
 tests =
-  testGroup
-    "unit tests"
-    [ testCase "add1" $ (printTerm add1) @?= "(program 1.0.0 (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1))"
-    , testCase "add1Hoisted" $ (printTerm add1Hoisted) @?= "(program 1.0.0 ((\\i0 -> i1) (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1)))"
-    , testCase "example1" $ (printTerm example1) @?= "(program 1.0.0 ((\\i0 -> addInteger (i1 12 32) (i1 5 4)) (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1)))"
-    , testCase "example2" $ (printTerm example2) @?= "(program 1.0.0 (\\i0 -> i1 (\\i0 -> addInteger i1 1) (\\i0 -> subtractInteger i1 1)))"
-    , testCase "pfix" $ (printTerm pfix) @?= "(program 1.0.0 ((\\i0 -> i1) (\\i0 -> (\\i0 -> i2 (\\i0 -> i2 i2 i1)) (\\i0 -> i2 (\\i0 -> i2 i2 i1)))))"
-    , testCase "fib" $ (printTerm fib) @?= "(program 1.0.0 ((\\i0 -> (\\i0 -> (\\i0 -> i1) (i1 (\\i0 -> \\i0 -> force (i4 (equalsInteger i1 0) (delay 0) (delay (force (i4 (equalsInteger i1 1) (delay 1) (delay (addInteger (i2 (subtractInteger i1 1)) (i2 (subtractInteger i1 2))))))))))) (\\i0 -> (\\i0 -> i2 (\\i0 -> i2 i2 i1)) (\\i0 -> i2 (\\i0 -> i2 i2 i1)))) (force ifThenElse)))"
-    , testCase "fib 9 == 34" $ equal (fib £ 9) (34 :: Term s PInteger)
-    , testCase "uglyDouble" $ (printTerm uglyDouble) @?= "(program 1.0.0 (\\i0 -> addInteger i1 i1))"
-    , testCase "1 + 2 == 3" $ equal (1 + 2 :: Term s PInteger) (3 :: Term s PInteger)
-    , testCase "fails: perror" $ fails perror
-    , testCase "() == ()" $ expect $ pmatch (pcon PUnit) (\case PUnit -> (pcon PTrue))
-    , testCase "0x02af == 0x02af" $ expect $ phexByteStr "02af" £== phexByteStr "02af"
-    , testCase "\"foo\" == \"foo\"" $ expect $ "foo" £== ("foo" :: Term s PString)
-    , testCase "PByteString :: mempty <> a == a <> mempty == a" $ do
-        expect $ let a = phexByteStr "152a" in (mempty <> a) £== a
-        expect $ let a = phexByteStr "4141" in (a <> mempty) £== a
-    , testCase "PString :: mempty <> a == a <> mempty == a" $ do
-        expect $ let a = "foo" :: Term s PString in (mempty <> a) £== a
-        expect $ let a = "bar" :: Term s PString in (a <> mempty) £== a
-    , testCase "PByteString :: 0x12 <> 0x34 == 0x1234" $
-        expect $
-          (phexByteStr "12" <> phexByteStr "34") £== phexByteStr "1234"
-    , testCase "PString :: \"ab\" <> \"cd\" == \"abcd\"" $
-        expect $
-          ("ab" <> "cd") £== ("abcd" :: Term s PString)
-    , testCase "PByteString mempty" $ expect $ mempty £== phexByteStr ""
-    , testCase "PString mempty" $ expect $ mempty £== ("" :: Term s PString)
-    , testCase "pfromText \"abc\" `equal` \"abc\"" $ equal (pfromText "abc") ("abc" :: Term s PString)
-    ]
+  let exp :: Term s (B.PListData POpaque)
+      exp = B.mkList $ (B.IData #£) <$> reverse [0, 1, 1, 2, 3, 5]
+   in testGroup
+        "unit tests"
+        [ testCase "add1" $ (printTerm add1) @?= "(program 1.0.0 (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1))"
+        , testCase "add1Hoisted" $ (printTerm add1Hoisted) @?= "(program 1.0.0 ((\\i0 -> i1) (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1)))"
+        , testCase "example1" $ (printTerm example1) @?= "(program 1.0.0 ((\\i0 -> addInteger (i1 12 32) (i1 5 4)) (\\i0 -> \\i0 -> addInteger (addInteger i2 i1) 1)))"
+        , testCase "example2" $ (printTerm example2) @?= "(program 1.0.0 (\\i0 -> i1 (\\i0 -> addInteger i1 1) (\\i0 -> subtractInteger i1 1)))"
+        , testCase "pfix" $ (printTerm pfix) @?= "(program 1.0.0 ((\\i0 -> i1) (\\i0 -> (\\i0 -> i2 (\\i0 -> i2 i2 i1)) (\\i0 -> i2 (\\i0 -> i2 i2 i1)))))"
+        , testCase "fib" $ (printTerm fib) @?= "(program 1.0.0 ((\\i0 -> (\\i0 -> (\\i0 -> i1) (i1 (\\i0 -> \\i0 -> force (i4 (equalsInteger i1 0) (delay 0) (delay (force (i4 (equalsInteger i1 1) (delay 1) (delay (addInteger (i2 (subtractInteger i1 1)) (i2 (subtractInteger i1 2))))))))))) (\\i0 -> (\\i0 -> i2 (\\i0 -> i2 i2 i1)) (\\i0 -> i2 (\\i0 -> i2 i2 i1)))) (force ifThenElse)))"
+        , testCase "fib 9 == 34" $ equal (fib £ 9) (34 :: Term s PInteger)
+        , testCase "fibs 3 == [..]" $ equal (fibs £ 5) exp
+        , testCase "uglyDouble" $ (printTerm uglyDouble) @?= "(program 1.0.0 (\\i0 -> addInteger i1 i1))"
+        , testCase "1 + 2 == 3" $ equal (1 + 2 :: Term s PInteger) (3 :: Term s PInteger)
+        , testCase "fails: perror" $ fails perror
+        , testCase "() == ()" $ expect $ pmatch (pcon PUnit) (\case PUnit -> (pcon PTrue))
+        , testCase "0x02af == 0x02af" $ expect $ phexByteStr "02af" £== phexByteStr "02af"
+        , testCase "\"foo\" == \"foo\"" $ expect $ "foo" £== ("foo" :: Term s PString)
+        , testCase "PByteString :: mempty <> a == a <> mempty == a" $ do
+            expect $ let a = phexByteStr "152a" in (mempty <> a) £== a
+            expect $ let a = phexByteStr "4141" in (a <> mempty) £== a
+        , testCase "PString :: mempty <> a == a <> mempty == a" $ do
+            expect $ let a = "foo" :: Term s PString in (mempty <> a) £== a
+            expect $ let a = "bar" :: Term s PString in (a <> mempty) £== a
+        , testCase "PByteString :: 0x12 <> 0x34 == 0x1234" $
+            expect $
+              (phexByteStr "12" <> phexByteStr "34") £== phexByteStr "1234"
+        , testCase "PString :: \"ab\" <> \"cd\" == \"abcd\"" $
+            expect $
+              ("ab" <> "cd") £== ("abcd" :: Term s PString)
+        , testCase "PByteString mempty" $ expect $ mempty £== phexByteStr ""
+        , testCase "PString mempty" $ expect $ mempty £== ("" :: Term s PString)
+        , testCase "pfromText \"abc\" `equal` \"abc\"" $ equal (pfromText "abc") ("abc" :: Term s PString)
+        ]
