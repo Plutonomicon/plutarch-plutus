36b6877ca7c1744446a245525012475346705c68
Chase <totallynotchase42@gmail.com>
2021-12-29 14:46:28 +0300

    Remove haddocks from concrete implementations

--- plutarch-trace/impl/Plutarch/Trace/Disable.hs
+++ plutarch-trace/impl/Plutarch/Trace/Disable.hs
@@ -7,18 +7,14 @@ import Plutarch.String (PString)
 pf :: Term s (b :--> a :--> a)
 pf = phoistAcyclic $ plam $ \_ y -> y
 
--- | A strict version of 'ptrace'.
 ptrace' :: Term s (PString :--> a :--> a)
 ptrace' = pf
 
--- | Trace a message, then evaluate and return given argument.
 ptrace :: Term s PString -> Term s a -> Term s a
 ptrace _ a = a
 
--- | Trace a message if given argument evaluates to true.
 ptraceIfTrue :: Term s (PString :--> PBool :--> PBool)
 ptraceIfTrue = pf
 
--- | Trace a message if given argument evaluates to false.
 ptraceIfFalse :: Term s (PString :--> PBool :--> PBool)
 ptraceIfFalse = pf
