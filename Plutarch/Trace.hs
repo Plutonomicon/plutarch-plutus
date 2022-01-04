f959a1d2ddeb8aae40d84b5110f5815e46007939
Chase <totallynotchase42@gmail.com>
2022-01-04 14:47:08 +0530

    Add `ptraceError` + some haddocks

--- Plutarch/Trace.hs
+++ Plutarch/Trace.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE CPP #-}
 
-module Plutarch.Trace (ptrace, ptraceIfTrue, ptraceIfFalse) where
+module Plutarch.Trace (ptrace, ptraceIfTrue, ptraceIfFalse, ptraceError) where
 
 -- CPP support isn't great in fourmolu.
 {- ORMOLU_DISABLE -}
@@ -25,6 +25,7 @@ ptrace' :: Term s (PString :--> a :--> a)
 ptrace' = phoistAcyclic $ pforce $ punsafeBuiltin PLC.Trace
 #endif
 
+-- | Trace the given message before evaluating the argument.
 ptrace :: Term s PString -> Term s a -> Term s a
 #ifdef Development
 ptrace s a = pforce $ ptrace' # s # pdelay a
@@ -32,6 +33,15 @@ ptrace s a = pforce $ ptrace' # s # pdelay a
 ptrace _ a = a
 #endif
 
+-- | Trace the given message and terminate evaluation with a 'perror'.
+ptraceError :: Term s PString -> Term s a
+#ifdef Development
+ptraceError s = pforce $ ptrace' # s # pdelay perror
+#else
+ptraceError _ = perror
+#endif
+
+-- | Trace the given message if the argument evaluates to true.
 ptraceIfTrue :: Term s PString -> Term s PBool -> Term s PBool
 #ifdef Development
 ptraceIfTrue s a' = plet a' $ \a -> pif a (ptrace' # s # a) a
@@ -39,8 +49,8 @@ ptraceIfTrue s a' = plet a' $ \a -> pif a (ptrace' # s # a) a
 ptraceIfTrue _ a = a
 #endif
 
+-- | Trace the given message if the argument evaluates to False.
 ptraceIfFalse :: Term s PString -> Term s PBool -> Term s PBool
-
 #ifdef Development
 ptraceIfFalse s a' = plet a' $ \a -> pif a a (ptrace' # s # a)
 #else
