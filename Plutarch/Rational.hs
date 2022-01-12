6a8c66a7401da9769b1b4f943bc0f2e302921d4b
Geometer1729 <16kuhnb@gmail.com>
2022-01-12 11:19:01 -0500

    More hoisting

--- Plutarch/Rational.hs
+++ Plutarch/Rational.hs
@@ -23,62 +23,125 @@ instance PlutusType PRational where
   pmatch' p f = p #$ plam $ \x y -> f (PRational x y)
 
 instance PEq PRational where
-  l #== r =
-    pmatch l $ \(PRational ln ld) ->
-      pmatch r $ \(PRational rn rd) ->
-        rd * ln #== rn * ld
+  l' #== r' =
+    phoistAcyclic
+      ( plam $ \l r ->
+          pmatch l $ \(PRational ln ld) ->
+            pmatch r $ \(PRational rn rd) ->
+              rd * ln #== rn * ld
+      )
+      # l'
+      # r'
 
 instance POrd PRational where
-  l #<= r =
-    pmatch l $ \(PRational ln ld) ->
-      pmatch r $ \(PRational rn rd) ->
-        rd * ln #<= rn * ld
-  l #< r =
-    pmatch l $ \(PRational ln ld) ->
-      pmatch r $ \(PRational rn rd) ->
-        rd * ln #< rn * ld
+  l' #<= r' =
+    phoistAcyclic
+      ( plam $ \l r ->
+          pmatch l $ \(PRational ln ld) ->
+            pmatch r $ \(PRational rn rd) ->
+              rd * ln #<= rn * ld
+      )
+      # l'
+      # r'
+
+  l' #< r' =
+    phoistAcyclic
+      ( plam $ \l r ->
+          pmatch l $ \(PRational ln ld) ->
+            pmatch r $ \(PRational rn rd) ->
+              rd * ln #< rn * ld
+      )
+      # l'
+      # r'
 
 instance Num (Term s PRational) where
-  x + y = preduce
-    #$ pmatch x
-    $ \(PRational xn xd) ->
-      pmatch y $ \(PRational yn yd) ->
-        pcon $ PRational (xn * yd + yn * xd) (xd * yd)
-  x - y = preduce
-    #$ pmatch x
-    $ \(PRational xn xd) ->
-      pmatch y $ \(PRational yn yd) ->
-        pcon $ PRational (xn * yd - yn * xd) (xd * yd)
-  x * y = preduce
-    #$ pmatch x
-    $ \(PRational xn xd) ->
-      pmatch y $ \(PRational yn yd) ->
-        pcon $ PRational (xn * yn) (xd * yd)
-  negate x =
-    pmatch x $ \(PRational xn xd) ->
-      pcon $ PRational (negate xn) xd
-  abs x =
-    pmatch x $ \(PRational xn xd) ->
-      pcon $ PRational (abs xn) (abs xd)
-  signum x' = plet x' $ \x ->
-    pif
-      (x #== 0)
-      0
-      $ pif
-        (x #< 0)
-        (-1)
-        1
+  x' + y' =
+    phoistAcyclic
+      ( plam $ \x y ->
+          preduce #$ pmatch x $
+            \(PRational xn xd) ->
+              pmatch y $ \(PRational yn yd) ->
+                pcon $ PRational (xn * yd + yn * xd) (xd * yd)
+      )
+      # x'
+      # y'
+
+  x' - y' =
+    phoistAcyclic
+      ( plam $ \x y ->
+          preduce
+            #$ pmatch x
+            $ \(PRational xn xd) ->
+              pmatch y $ \(PRational yn yd) ->
+                pcon $ PRational (xn * yd - yn * xd) (xd * yd)
+      )
+      # x'
+      # y'
+
+  x' * y' =
+    phoistAcyclic
+      ( plam $ \x y ->
+          preduce
+            #$ pmatch x
+            $ \(PRational xn xd) ->
+              pmatch y $ \(PRational yn yd) ->
+                pcon $ PRational (xn * yn) (xd * yd)
+      )
+      # x'
+      # y'
+
+  negate x' =
+    phoistAcyclic
+      ( plam $ \x ->
+          pmatch x $ \(PRational xn xd) ->
+            pcon $ PRational (negate xn) xd
+      )
+      # x'
+
+  abs x' =
+    phoistAcyclic
+      ( plam $ \x ->
+          pmatch x $ \(PRational xn xd) ->
+            pcon $ PRational (abs xn) (abs xd)
+      )
+      # x'
+
+  signum x'' =
+    phoistAcyclic
+      ( plam $ \x' -> plet x' $ \x ->
+          pif
+            (x #== 0)
+            0
+            $ pif
+              (x #< 0)
+              (-1)
+              1
+      )
+      # x''
+
   fromInteger n = pcon $ PRational (fromInteger n) 1
 
 instance Fractional (Term s PRational) where
-  recip x =
-    pmatch x $ \(PRational xn xd) ->
-      pcon (PRational xd xn)
-  x / y = preduce
-    #$ pmatch x
-    $ \(PRational xn xd) ->
-      pmatch y $ \(PRational yn yd) ->
-        pcon (PRational (xn * yd) (xd * yn))
+  recip x' =
+    phoistAcyclic
+      ( plam $ \x ->
+          pmatch x $ \(PRational xn xd) ->
+            pcon (PRational xd xn)
+      )
+      # x'
+
+  x' / y' =
+    phoistAcyclic
+      ( plam $ \x y ->
+          preduce
+            #$ pmatch x
+            $ \(PRational xn xd) ->
+              pmatch y $ \(PRational yn yd) ->
+                pcon (PRational (xn * yd) (xd * yn))
+      )
+      # x'
+      # y'
+
   fromRational r =
     pcon $ PRational (fromInteger $ numerator r) (fromInteger $ denominator r)
 
@@ -101,7 +164,7 @@ pgcd = phoistAcyclic $
 
 -- assumes inputs are non negative and a >= b
 pgcd' :: Term s (PInteger :--> PInteger :--> PInteger)
-pgcd' = pfix #$ plam $ f
+pgcd' = phoistAcyclic $ pfix #$ plam $ f
   where
     f self a b =
       pif
@@ -110,16 +173,16 @@ pgcd' = pfix #$ plam $ f
         $ self # b #$ pmod # a # b
 
 pmin :: POrd a => Term s (a :--> a :--> a)
-pmin = plam $ \a b -> pif (a #<= b) a b
+pmin = phoistAcyclic $ plam $ \a b -> pif (a #<= b) a b
 
 pmax :: POrd a => Term s (a :--> a :--> a)
-pmax = plam $ \a b -> pif (a #<= b) b a
+pmax = phoistAcyclic $ plam $ \a b -> pif (a #<= b) b a
 
 pnumerator :: Term s (PRational :--> PInteger)
-pnumerator = plam $ \x -> pmatch x $ \(PRational n _) -> n
+pnumerator = phoistAcyclic $ plam $ \x -> pmatch x $ \(PRational n _) -> n
 
 pdenominator :: Term s (PRational :--> PInteger)
-pdenominator = plam $ \x -> pmatch x $ \(PRational _ d) -> d
+pdenominator = phoistAcyclic $ plam $ \x -> pmatch x $ \(PRational _ d) -> d
 
 pfromInteger :: Term s (PInteger :--> PRational)
-pfromInteger = plam $ \n -> pcon $ PRational n 1
+pfromInteger = phoistAcyclic $ plam $ \n -> pcon $ PRational n 1
