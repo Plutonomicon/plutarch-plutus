ca795de4ce7b0a47f64ab1f229ae3324ac050ddd
Las Safin <me@las.rs>
2022-01-14 17:42:22 +0000

    Export `extraSources` from flake

--- flake.nix
+++ flake.nix
@@ -43,6 +43,81 @@
 
   outputs = inputs@{ self, nixpkgs, haskell-nix, plutus, flake-compat-ci, ... }:
     let
+      extraSources = [
+        {
+          src = inputs.protolude;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.foundation;
+          subdirs = [
+            "foundation"
+            "basement"
+          ];
+        }
+        {
+          src = inputs.cardano-prelude;
+          subdirs = [
+            "cardano-prelude"
+            # "cardano-prelude-test"
+          ];
+        }
+        {
+          src = inputs.hs-memory;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.cardano-crypto;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.cryptonite;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.flat;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.cardano-base;
+          subdirs = [
+           # "base-deriving-via"
+            "binary"
+           # "binary/test"
+           "cardano-crypto-class"
+           # "cardano-crypto-praos"
+           # "cardano-crypto-tests"
+           # "measures"
+           # "orphans-deriving-via"
+           # "slotting"
+           # "strict-containers"
+          ];
+        }
+        {
+          src = inputs.sized-functors;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.th-extras;
+          subdirs = [ "." ];
+        }
+        {
+          src = inputs.plutus;
+          subdirs = [
+            #"plutus-benchmark"
+            "plutus-core"
+            #"plutus-errors"
+            "plutus-ledger-api"
+            #"plutus-metatheory"
+            "plutus-tx"
+            #"plutus-tx-plugin"
+            "prettyprinter-configurable"
+            "word-array"
+            #"stubs/plutus-ghc-stub"
+          ];
+        }
+      ];
+
       supportedSystems = with nixpkgs.lib.systems.supported; tier1 ++ tier2 ++ tier3;
 
       perSystem = nixpkgs.lib.genAttrs supportedSystems;
@@ -57,92 +132,7 @@
           src = ./.;
           compiler-nix-name = "ghc921";
           cabalProjectFileName = "cabal.project";
-          extraSources = [
-            {
-              src = inputs.protolude;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.foundation;
-              subdirs = [
-                "foundation"
-                "basement"
-              ];
-            }
-            {
-              src = inputs.cardano-prelude;
-              subdirs = [
-                "cardano-prelude"
-                # "cardano-prelude-test"
-              ];
-            }
-            {
-              src = inputs.hs-memory;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.cardano-crypto;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.cryptonite;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.flat;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.cardano-base;
-              subdirs = [
-               # "base-deriving-via"
-                "binary"
-               # "binary/test"
-               "cardano-crypto-class"
-               # "cardano-crypto-praos"
-               # "cardano-crypto-tests"
-               # "measures"
-               # "orphans-deriving-via"
-               # "slotting"
-               # "strict-containers"
-              ];
-            }
-            {
-              src = inputs.sized-functors;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.th-extras;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.plutus;
-              subdirs = [
-                #"plutus-benchmark"
-                "plutus-core"
-                #"plutus-errors"
-                "plutus-ledger-api"
-                #"plutus-metatheory"
-                "plutus-tx"
-                #"plutus-tx-plugin"
-                "prettyprinter-configurable"
-                "word-array"
-                #"stubs/plutus-ghc-stub"
-              ];
-            }
-          ];
-          /*
-          extraSources = [
-            {
-              src = inputs.Win32-network;
-              subdirs = [ "." ];
-            }
-            {
-              src = inputs.Shrinker;
-              subdirs = [ "." "testing" ];
-            }
-          ];
-          */
+          inherit extraSources;
           modules = [{
             packages = {
               basement.src = "${inputs.foundation}/basement";
@@ -202,8 +192,6 @@
 
             additional = ps: [
               ps.plutus-ledger-api
-              #ps.plutus-tx
-              #ps.plutus-ledger-api
               #ps.shrinker
               #ps.shrinker-testing
             ];
@@ -227,6 +215,8 @@
           ;
     in
     {
+      inherit extraSources;
+
       project = perSystem projectFor;
       flake = perSystem (system: (projectFor system).flake {});
 
