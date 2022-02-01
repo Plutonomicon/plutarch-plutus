0611de58d86e0bd4f7b29f3f2f65d71b66feb30a
Las Safin <me@las.rs>
2022-02-01 14:49:41 +0000

    Make `r` in `TermCont r s a` implicit

--- Plutarch/TermCont.hs
+++ Plutarch/TermCont.hs
@@ -1,44 +1,49 @@
+{-# OPTIONS_GHC -Wno-unused-foralls #-}
+
 module Plutarch.TermCont (
   hashOpenTerm,
-  TermCont (TermCont, runTermCont),
+  TermCont (TermCont),
+  runTermCont,
   runTermContId,
   tcont,
 ) where
 
+import Data.Kind (Type)
 import Data.String (fromString)
-import Plutarch.Internal (Dig, Term (Term), asRawTerm, getTerm, hashRawTerm)
+import Plutarch.Internal (Dig, PType, S, Term (Term), asRawTerm, getTerm, hashRawTerm)
 import Plutarch.Trace (ptraceError)
 
-newtype TermCont r s a = TermCont {runTermCont :: (a -> Term s r) -> Term s r}
+newtype TermCont :: forall (r :: PType). S -> Type -> Type where
+  TermCont :: forall r s a. {runTermCont :: ((a -> Term s r) -> Term s r)} -> TermCont @r s a
 
-instance Functor (TermCont r s) where
+runTermContId :: TermCont @a s (Term s a) -> Term s a
+runTermContId t = runTermCont t id
+
+instance Functor (TermCont s) where
   fmap f (TermCont g) = TermCont $ \h -> g (h . f)
 
-instance Applicative (TermCont r s) where
+instance Applicative (TermCont s) where
   pure x = TermCont $ \f -> f x
   x <*> y = do
     x <- x
     y <- y
     pure (x y)
 
-instance Monad (TermCont r s) where
+instance Monad (TermCont s) where
   (TermCont f) >>= g = TermCont $ \h ->
     f
       ( \x ->
           runTermCont (g x) h
       )
 
-instance MonadFail (TermCont r s) where
+instance MonadFail (TermCont s) where
   fail s = TermCont $ \_ ->
     ptraceError $ fromString s
 
-runTermContId :: TermCont a s (Term s a) -> Term s a
-runTermContId x = runTermCont x id
-
-tcont :: ((a -> Term s r) -> Term s r) -> TermCont r s a
+tcont :: ((a -> Term s r) -> Term s r) -> TermCont @r s a
 tcont = TermCont
 
-hashOpenTerm :: Term s a -> TermCont r s Dig
+hashOpenTerm :: Term s a -> TermCont s Dig
 hashOpenTerm x = TermCont $ \f -> Term $ \i ->
   let inner = f $ hashRawTerm . getTerm $ asRawTerm x i
    in asRawTerm inner i
