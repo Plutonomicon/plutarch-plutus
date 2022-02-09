7d5ace1eea3fffa43f8af4c4420d40cfaf50a683
Geometer1729 <16kuhnb@gmail.com>
2022-02-09 16:00:46 -0500

    set encoding for property tests

--- properties/Spec.hs
+++ properties/Spec.hs
@@ -1,9 +1,12 @@
 import Test.Tasty (TestTree, defaultMain, testGroup)
+import GHC.IO.Encoding (setLocaleEncoding, utf8)
 
 import Tests.List (listTests)
 
 main :: IO ()
-main = defaultMain tests
+main = do
+  setLocaleEncoding utf8
+  defaultMain tests
 
 tests :: TestTree
 tests =
