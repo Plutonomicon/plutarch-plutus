a0817a237f3cf2a93c5089942791e52c6e033a19
Sridhar Ratnakumar <srid@srid.ca>
2022-02-13 18:44:14 -0500

    Delete already ported over tests

--- examples/Examples/PlutusType.hs
+++ examples/Examples/PlutusType.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE CPP #-}
 
-module Examples.PlutusType (AB (..), swap, tests) where
+module Examples.PlutusType (tests) where
 
 import Plutarch
 import Plutarch.Prelude
@@ -10,49 +10,11 @@ import Test.Tasty.HUnit (Assertion, testCase)
 
 import Utils
 
-{- |
-  A Sum type, which can be encoded as an Enum
--}
-data AB (s :: S) = A | B
-
-{- |
-  AB is encoded as an Enum, using values of PInteger
-  internally.
--}
-instance PlutusType AB where
-  type PInner AB _ = PInteger
-
-  pcon' A = 0
-  pcon' B = 1
-
-  pmatch' x f =
-    pif (x #== 0) (f A) (f B)
-
-{- |
-  Instead of using `pcon'` and `pmatch'` directly,
-  use 'pcon' and 'pmatch', to hide the `PInner` type.
--}
-swap :: Term s AB -> Term s AB
-swap x = pmatch x $ \case
-  A -> pcon B
-  B -> pcon A
-
 tests :: HasTester => TestTree
 tests =
   testGroup
     "PlutusType tests"
     [ testGroup
-        "PlutusType examples"
-        [ testCase "A encoded as 0" $ do
-            pcon A `equal` (0 :: Term s PInteger)
-        , testCase "B encoded as 2" $ do
-            pcon B `equal` (1 :: Term s PInteger)
-        , testCase "swap A == B" $ do
-            swap (pcon A) `equal` pcon B
-        , testCase "swap B == A" $ do
-            swap (pcon B) `equal` pcon A
-        ]
-    , testGroup
         "PlutusType instances sanity"
         -- TODO: Add more sanity tests here!
         [ testCase "PBuiltinList" $ do
