d29bd09ee695bf769be783222c09e65acc69b268
Sridhar Ratnakumar <srid@srid.ca>
2022-02-14 14:58:30 -0500

    Add TraceSpec.hs

---
+++ plutarch-test/src/Plutarch/TraceSpec.hs
@@ -0,0 +1,40 @@
+module Plutarch.TraceSpec (spec) where
+
+import Test.Syd
+
+import Plutarch.Prelude
+import Plutarch.Test
+
+spec :: Spec
+spec = do
+  describe "trace" . plutarchDevFlagDescribe $ do
+    describe "ptrace" $ do
+      let p1 = ptrace "foo" (pcon PUnit)
+          p2 = ptrace "foo" (ptrace "bar" (pcon PUnit))
+      goldens
+        All
+        [ ("one", p1)
+        , ("two", p2)
+        ]
+      it "traces one" $ p1 `ptraces` ["foo"]
+      it "traces two" $ p2 `ptraces` ["foo", "bar"]
+    describe "ptraceIfTrue" $ do
+      let p1 = ptraceIfTrue "foo" (pcon PTrue)
+          p2 = ptraceIfTrue "foo" (pcon PFalse)
+      goldens
+        All
+        [ ("true", p1)
+        , ("false", p2)
+        ]
+      it "true" $ p1 `ptraces` ["foo"]
+      it "false" $ p2 `ptraces` []
+    describe "ptraceIfFalse" $ do
+      let p1 = ptraceIfFalse "foo" (pcon PTrue)
+          p2 = ptraceIfFalse "foo" (pcon PFalse)
+      goldens
+        All
+        [ ("true", p1)
+        , ("false", p2)
+        ]
+      it "true" $ p1 `ptraces` []
+      it "false" $ p2 `ptraces` ["foo"]
