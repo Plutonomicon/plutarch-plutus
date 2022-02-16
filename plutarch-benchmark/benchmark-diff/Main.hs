160966bf6342a631b63f7f39be4b223c2d187934
Emily Martins <emi@haskell.fyi>
2022-02-16 17:34:41 +0100

    use optparse-applicative in benchmark-diff too

--- plutarch-benchmark/benchmark-diff/Main.hs
+++ plutarch-benchmark/benchmark-diff/Main.hs
@@ -3,21 +3,25 @@ module Main (main) where
 import Plutarch.Benchmark (decodeBenchmarks, diffBenchmarks, renderDiffTable)
 
 import qualified Data.ByteString.Lazy as BSL
-import System.Environment (getArgs)
+import Options.Applicative
 import qualified Text.PrettyPrint.Boxes as B
 
 --------------------------------------------------------------------------------
 
 main :: IO ()
-main =
-  getArgs >>= \case
-    [oldPath, newPath] -> do
-      Right old <- decodeBenchmarks <$> BSL.readFile oldPath
-      Right new <- decodeBenchmarks <$> BSL.readFile newPath
-
-      putStrLn . B.render . renderDiffTable $ diffBenchmarks old new
-    _ -> usage
-
-usage :: IO ()
-usage =
-  putStrLn "usage: benchmark-diff [old] [new]"
+main = do
+  let cmdOptions =
+        liftA2
+          (,)
+          (argument str (metavar "BEFORE"))
+          (argument str (metavar "AFTER"))
+      opts =
+        info
+          (cmdOptions <**> helper)
+          ( fullDesc
+              <> progDesc "Diff two benchmark CSVs"
+          )
+  (oldPath, newPath) <- execParser opts
+  Right old <- decodeBenchmarks <$> BSL.readFile oldPath
+  Right new <- decodeBenchmarks <$> BSL.readFile newPath
+  putStrLn . B.render . renderDiffTable $ diffBenchmarks old new
