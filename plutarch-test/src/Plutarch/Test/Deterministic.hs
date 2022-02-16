c456789966e070a7fd92590b3566cb50fd2232c0
Sridhar Ratnakumar <srid@srid.ca>
2022-02-16 13:59:50 -0500

    Introduce walkScript, and fix 8.10 failure

--- plutarch-test/src/Plutarch/Test/Deterministic.hs
+++ plutarch-test/src/Plutarch/Test/Deterministic.hs
@@ -2,49 +2,67 @@
 
 module Plutarch.Test.Deterministic (compileD) where
 
-import Control.Monad (void)
 import qualified Data.Text as T
 import Data.Void (Void)
 import Plutarch (ClosedTerm, compile)
 import qualified Plutus.V1.Ledger.Scripts as Scripts
-import PlutusCore.Default
+import PlutusCore.Default (
+  DefaultFun (Trace),
+  DefaultUni (DefaultUniString),
+  Some (Some),
+  ValueOf (ValueOf),
+  someValueOf,
+ )
 import Replace.Megaparsec (streamEdit)
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Char as M
-import UntypedPlutusCore (Program (Program), Term (Apply, Builtin, Constant, Delay, Force, LamAbs))
+import UntypedPlutusCore (
+  Program (Program),
+  Term (Apply, Builtin, Constant, Delay, Force, LamAbs),
+ )
 import qualified UntypedPlutusCore as UPLC
 
 {- Like `compile`, but the result is deterministic -}
 compileD :: ClosedTerm a -> Scripts.Script
-compileD = rewrite . compile
+compileD p = rewriteTraces $ compile p
 
--- TODO: refactor
-rewrite :: Scripts.Script -> Scripts.Script
-rewrite (Scripts.Script (Program ann ver term)) =
+{- Rewrite the string used by `Trace` so that the script becomes deterministic. -}
+rewriteTraces :: Scripts.Script -> Scripts.Script
+rewriteTraces =
+  walkScript $ \term -> do
+    -- Replace the 's' in `trace s`.
+    Apply () b@(Force _ (Builtin _ Trace)) (Constant () (Some (ValueOf DefaultUniString s))) <- pure term
+    let s' = T.pack . streamEdit ghcPatternMatchReplacement id . T.unpack $ s
+    pure $ Apply () b (Constant () (someValueOf DefaultUniString $ s'))
+  where
+    ghcPatternMatchReplacement :: M.Parsec Void String String
+    ghcPatternMatchReplacement = do
+      -- What's being replaced (which is varying).
+      s <- M.string "Pattern match failure" <* M.takeRest
+      -- The replacement (which is constant)
+      pure $ s <> "..."
+
+{- Walk the Plutus script, transforming matching terms -}
+walkScript ::
+  forall term.
+  (term ~ UPLC.Term UPLC.DeBruijn DefaultUni DefaultFun ()) =>
+  (term -> Maybe term) ->
+  Scripts.Script ->
+  Scripts.Script
+walkScript f (Scripts.Script (Program ann ver term)) =
   Scripts.Script (Program ann ver (go term))
   where
-    go :: UPLC.Term name DefaultUni DefaultFun () -> UPLC.Term name DefaultUni DefaultFun ()
-    go = \case
-      (Apply ann b@(Force _ (Builtin _ Trace)) (Constant ann1 (Some (ValueOf DefaultUniString s)))) ->
-        (Apply ann b (Constant ann1 (someValueOf DefaultUniString $ T.pack . rewriteFailMsgToBeDeterministic . T.unpack $ s)))
-      LamAbs ann name t ->
-        LamAbs ann name (go t)
-      Apply ann t1 t2 ->
-        Apply ann (go t1) (go t2)
-      Force ann t ->
-        Force ann (go t)
-      Delay ann t ->
-        Delay ann (go t)
-      x -> x
-
-rewriteFailMsgToBeDeterministic :: String -> String
-rewriteFailMsgToBeDeterministic =
-  streamEdit ghcPatternMatchMsg id
-  where
-    ghcPatternMatchMsg :: M.Parsec Void String String
-    ghcPatternMatchMsg = do
-      -- What's being replaced.
-      s <- M.string "Pattern match failure"
-      void $ M.takeRest
-      -- The replacement
-      pure $ s <> "..."
+    go :: term -> term
+    go term =
+      case f term of
+        Just term' -> term'
+        Nothing -> case term of
+          LamAbs ann name t ->
+            LamAbs ann name (go t)
+          Apply ann t1 t2 ->
+            Apply ann (go t1) (go t2)
+          Force ann t ->
+            Force ann (go t)
+          Delay ann t ->
+            Delay ann (go t)
+          x -> x
