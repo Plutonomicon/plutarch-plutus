30ed5f28ddcb6d55018d9b9eeb6a1e45464e95f3
Geometer1729 <16kuhnb@gmail.com>
2022-02-17 10:32:39 -0500

    add goldens

--- plutarch-test/src/Plutarch/ListPropSpec.hs
+++ plutarch-test/src/Plutarch/ListPropSpec.hs
@@ -2,27 +2,64 @@ module Plutarch.ListPropSpec (spec) where
 
 import Hedgehog (Property)
 
-import Plutarch.List (pmergeSort, pelemAt, pfind, preverse, ptimSort)
+import Plutarch.List (pelemAt, pfind, pmergeSort, preverse, ptimSort)
 import Plutarch.Prelude
 
 import Data.List (find, sort)
 
 import Gen (genList, integerGen)
-import Util (haskPlutEquiv, viaBothPartial, viaPEq)
+import Util (haskPlutEquiv, marshal, viaBothPartial, viaPEq)
 
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+import Plutarch.Test (PlutarchGolden (All), goldens)
 import Test.Syd (Spec, describe, it)
 import Test.Syd.Hedgehog ()
 
 spec :: Spec
 spec = describe "list spec" $ do
-  it "find" findTest
-  it "reverse" reverseTest
-  it "elemAt" elemAtTest
-  it "pmergeSort" pmergeSortTest
-  it "ptimSort" ptimSortTest
+  describe "find" $ do
+    it "base agreement" $ findTest
+    goldens
+      All
+      [ ("find (== 3) [1..4]", pfind # plam (#== 3) #$ marshal [1 .. 4 :: Integer])
+      , ("find (== 5) [1..4]", pfind # plam (#== 5) #$ marshal [1 .. 4 :: Integer])
+      ]
+  describe "reverse" $ do
+    it "base agreement" reverseTest
+    goldens
+      All
+      [ ("reverse [1..5]", preverse # marshal [1 .. 5 :: Integer])
+      , ("reverse []", preverse # marshal ([] :: [Integer]))
+      ]
+  describe "elemAt" $ do
+    it "base agreement" elemAtTest
+    goldens
+      All
+      [ ("elemAt 3 [1..10]", pelemAt # 3 # marshal [1 .. 10 :: Integer])
+      , ("elemAt 0 [1..10]", pelemAt # 0 # marshal [1 .. 10 :: Integer])
+      , ("elemAt 9 [1..10]", pelemAt # 9 # marshal [1 .. 10 :: Integer])
+      ]
+  let xs1 = marshal [1 .. 10 :: Integer]
+      xs2 = marshal $ reverse [1 .. 10 :: Integer]
+      xs3 = marshal $ [1 .. 10 :: Integer] ++ reverse [11 .. 20]
+  describe "pmergeSort" $ do
+    it "base agreement" pmergeSortTest
+    goldens
+      All
+      [ ("sort xs1", pmergeSort # xs1)
+      , ("sort xs2", pmergeSort # xs2)
+      , ("sort xs3", pmergeSort # xs3)
+      ]
+  describe "ptimSort" $ do
+    it "base agreement" ptimSortTest
+    goldens
+      All
+      [ ("sort xs1", ptimSort # xs1)
+      , ("sort xs2", ptimSort # xs2)
+      , ("sort xs3", ptimSort # xs3)
+      ]
 
 findTest :: Property
 findTest =
@@ -31,9 +68,9 @@ findTest =
     (find @[] @Integer even)
     (pfind # peven)
     (genList integerGen)
-      where
-        peven :: Term s (PInteger :--> PBool)
-        peven = plam $ \n -> pmod # n # 2 #== 0
+  where
+    peven :: Term s (PInteger :--> PBool)
+    peven = plam $ \n -> pmod # n # 2 #== 0
 
 reverseTest :: Property
 reverseTest =
