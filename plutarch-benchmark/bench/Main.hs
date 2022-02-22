4a88a3cfbc03d58153dfd2a0e2ef25ecaab0f97a
Sridhar Ratnakumar <srid@srid.ca>
2022-02-22 13:00:08 -0500

    Remove int/bool benchmarks
    They already exist sufficiently in plutarch-test goldens.

--- plutarch-benchmark/bench/Main.hs
+++ plutarch-benchmark/bench/Main.hs
@@ -28,48 +28,10 @@ benchmarks :: [NamedBenchmark]
 benchmarks =
   benchGroup
     "types"
-    [ benchGroup "int" integerBench
-    , benchGroup "bool" boolBench
-    , benchGroup "builtin:intlist" intListBench
+    [ benchGroup "builtin:intlist" intListBench
     , benchGroup "data" dataBench
     , benchGroup "syn" syntaxBench
     ]
-
-integerBench :: [[NamedBenchmark]]
-integerBench =
-  [ -- Calling add twice
-    benchGroup
-      "add(2)"
-      $ let addInlined :: Term s PInteger -> Term s PInteger -> Term s PInteger
-            addInlined x y = x + y + 1
-            addUnhoisted :: Term s (PInteger :--> PInteger :--> PInteger)
-            addUnhoisted = plam $ \x y -> x + y + 1
-            addHoisted :: Term s (PInteger :--> PInteger :--> PInteger)
-            addHoisted = phoistAcyclic $ plam $ \x y -> x + y + 1
-         in [ bench "inlined" $ addInlined 12 32 + addInlined 5 4
-            , bench "unhoist" $ addUnhoisted # 12 # 32 + addUnhoisted # 5 # 4
-            , bench "hoisted" $ addHoisted # 12 # 32 + addHoisted # 5 # 4
-            ]
-  ]
-
-boolBench :: [[NamedBenchmark]]
-boolBench =
-  let true = pconstant @PBool True
-      false = pconstant @PBool False
-      pandNoHoist = phoistAcyclic $ plam $ \x y -> pif' # x # y # (pdelay $ pcon PFalse)
-   in [ benchGroup
-          "and"
-          [ bench "strict" $ pand' # true # false
-          , bench "lazy" $ (#&&) true false
-          , -- Calling `pand` twice.
-            bench "pand(2)" $
-              let x = pand # true # pdelay false
-               in pand # true # x
-          , bench "pand(2):unhoisted" $
-              let x = pandNoHoist # true # pdelay false
-               in pandNoHoist # true # x
-          ]
-      ]
 
 intListBench :: [[NamedBenchmark]]
 intListBench =
