c080e48705cffee6d8e0ab1d3283df5cc605f925
Sridhar Ratnakumar <srid@srid.ca>
2022-02-22 13:08:14 -0500

    Port list benchmarks

--- plutarch-benchmark/bench/Main.hs
+++ plutarch-benchmark/bench/Main.hs
@@ -36,61 +36,7 @@ benchmarks =
 intListBench :: [[NamedBenchmark]]
 intListBench =
   let numList = pconstant @(PBuiltinList PInteger) [1 .. 5]
-   in [ bench "phead" $ List.phead # numList
-      , bench "ptail" $ List.ptail # numList
-      , -- Accessing the first two elements, and adds them.
-        benchGroup
-          "x1+x2"
-          [ -- Via HeadList and TailList only
-            bench "builtin" $
-              (List.phead #$ List.ptail # numList) + (List.phead # numList)
-          , -- Via ChooseList (twice invoked)
-            bench "pmatch" $
-              pmatch numList $ \case
-                PNil -> perror
-                PCons x xs ->
-                  pmatch xs $ \case
-                    PNil -> perror
-                    PCons y _ ->
-                      x + y
-          ]
-      , -- Various ways to uncons a list
-        benchGroup
-          "uncons"
-          [ -- ChooseList builtin, like uncons but fails on null lists
-            bench "ChooseList" $
-              pmatch numList $ \case
-                PNil -> perror
-                PCons _x xs ->
-                  xs
-          , -- Retrieving head and tail of a list
-            bench "head-and-tail" $
-              plet (List.phead # numList) $ \_x ->
-                List.ptail # numList
-          , -- Retrieve head and tail using builtins, but fail on null lists.
-            bench "head-and-tail-and-null" $
-              plet (List.pnull # numList) $ \isEmpty ->
-                pmatch isEmpty $ \case
-                  PTrue -> perror
-                  PFalse -> plet (List.phead # numList) $ \_x ->
-                    List.ptail # numList
-          ]
-      , bench
-          "plength"
-          $ List.plength # pconstant @(PBuiltinList PInteger) [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-      , bench
-          "pelem"
-          $ List.pelem # 1 # pconstant @(PBuiltinList PInteger) [5, 2, 3, 4, 7, 5, 1, 6, 2]
-      , bench
-          "pall"
-          $ List.pall @PBuiltinList @PInteger # plam (const $ pconstant @PBool False) # pconstant [1, 2, 3, 4, 5, 6]
-      , benchGroup
-          "plistEquals"
-          [ bench "==(n=3)" $ List.plistEquals @PBuiltinList @PInteger # pconstant [1, 2, 3] # pconstant [1, 2, 3]
-          , bench "/=(n=4)" $ List.plistEquals @PBuiltinList @PInteger # pconstant [1, 2, 3, 4] # pconstant [1, 2, 3]
-          , bench "/=(empty;n=3)" $ List.plistEquals @PBuiltinList @PInteger # pconstant [] # pconstant [1, 2, 3]
-          ]
-      , benchGroup
+   in [ benchGroup
           "primitives"
           [ bench' $ plam $ \_ -> pconstant True
           , bench' $ plam $ \_ -> (0 :: Term _ PInteger)
--- plutarch-test/src/Plutarch/ListSpec.hs
+++ plutarch-test/src/Plutarch/ListSpec.hs
@@ -29,6 +29,16 @@ spec = do
         let xs :: Term s (PList PInteger)
             xs = psingleton # (fromInteger @(Term _ PInteger) 0)
         "identity" @| pconcat # xs # pnil @== xs
+      "plength"
+        @| plength # xs10 @== pconstant @PInteger 10
+      "pelem"
+        @| pelem # 5 # xs10 @-> passert
+      "pall"
+        @| pall # plam (const $ pconstant @PBool False) # xs10 @-> passertNot
+      "plistEquals" @\ do
+        "true" @| plistEquals # xs10 # integerList [1 .. 10] @-> passert
+        "false" @| plistEquals # xs10 # integerList [1 .. 3] @-> passertNot
+        "empty" @| plistEquals # xs10 # integerList [] @-> passertNot
       "pmap" @\ do
         "eg" @| pmap # (plam $ \x -> x + x) # xs10
           @== (integerList $ fmap (* 2) [1 .. 10])
@@ -52,3 +62,42 @@ spec = do
             @== pconstant @PInteger (foldl (-) 0 [1 .. 10])
           "empty" @| pfoldl # plam (-) # 0 # integerList []
             @== pconstant @PInteger 0
+    -- Benchmarking different ways of using lists
+    describe "bench" . pgoldenSpec $ do
+      let numList = pconstant @(PBuiltinList PInteger) [1 .. 5]
+      -- Two ways of matching on a list
+      "x1+x2" @\ do
+        -- Via HeadList and TailList only.
+        "builtin" @| (phead #$ ptail # numList) + (phead # numList)
+        -- Via ChooseList (twice invoked)
+        "pmatch"
+          @| pmatch numList
+          $ \case
+            PNil -> perror
+            PCons x xs ->
+              pmatch xs $ \case
+                PNil -> perror
+                PCons y _ ->
+                  x + y
+      -- Various ways of uncons'ing a list
+      "uncons" @\ do
+        -- ChooseList builtin, like uncons but fails on null lists
+        "ChooseList"
+          @| pmatch numList
+          $ \case
+            PNil -> perror
+            PCons _x xs ->
+              xs
+        -- Retrieving head and tail of a list
+        "head-and-tail"
+          @| plet (phead # numList)
+          $ \_x ->
+            ptail # numList
+        -- Retrieve head and tail using builtins, but fail on null lists.
+        "head-and-tail-and-null"
+          @| plet (pnull # numList)
+          $ \isEmpty ->
+            pmatch isEmpty $ \case
+              PTrue -> perror
+              PFalse -> plet (phead # numList) $ \_x ->
+                ptail # numList
