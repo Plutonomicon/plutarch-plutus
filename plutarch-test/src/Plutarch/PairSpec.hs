ece6771fe644f8074e13ae2f893b44f0e4667046
Sridhar Ratnakumar <srid@srid.ca>
2022-02-28 10:11:26 -0500

    Add PEq tests

---
+++ plutarch-test/src/Plutarch/PairSpec.hs
@@ -0,0 +1,28 @@
+module Plutarch.PairSpec (spec) where
+
+import Test.Syd
+
+import Plutarch.Prelude
+import Plutarch.Test
+
+spec :: Spec
+spec = do
+  describe "pair" . pgoldenSpec $ do
+    "eq" @\ do
+      "true"
+        @| pcon @(PPair PInteger PString) (PPair 42 "Hello")
+          #== pcon (PPair 42 "Hello")
+          @-> passert
+      "false" @\ do
+        "fst"
+          @| pcon @(PPair PInteger PString) (PPair 42 "Hello")
+            #== pcon (PPair 24 "Hello")
+          @-> passertNot
+        "snd"
+          @| pcon @(PPair PInteger PString) (PPair 42 "Hello")
+            #== pcon (PPair 42 "World")
+          @-> passertNot
+        "both"
+          @| pcon @(PPair PInteger PString) (PPair 42 "Hello")
+            #== pcon (PPair 24 "World")
+          @-> passertNot
