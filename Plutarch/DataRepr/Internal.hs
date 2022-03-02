dc2ebbc96efb99aa66d2917c7439beedce662066
gnumonik <gnumonik@protonmail.com>
2022-03-02 18:05:10 +0000

    Added an explanation of how this is supposed to work, changed some constructor names for clarity

--- Plutarch/DataRepr/Internal.hs
+++ Plutarch/DataRepr/Internal.hs
@@ -189,21 +189,66 @@ type PDataSum :: [[PLabeledType]] -> PType
 data PDataSum (defs :: [[PLabeledType]]) (s :: S) where 
   MkPDataSum :: PDataSum' n 'True defs s -> PDataSum defs s 
 
+{- Overview of how this works: 
+
+1) We need constructors for PDataSum in order to define a PlutusType instance. 
+
+2) Ideally, we want the PDataSum data type to be structured so as to enforce the invariant that each sum contains 
+   *exactly one* of its choices
+
+3) We can't do that with PDataSum alone. To enforce those invariants at the type level, we need an index type which encodes 
+   the fact that the sum contains exactly one choice. 
+
+4) PDataSum' is the helper type which allows us to express those invariants. The structure of the type has to be inductive, but 
+   we need four constructors (two Zero / two Suc) to cover each of the possible cases. The Boolean type argument indicates whether or not the 
+   sum contains an element, while the Natural type argument represents the position of the element in the list of choices. 
+
+      a) 'SumZE' represents a one element sum that *does not* contain its element.  
+
+      b) 'SumZF' represents a one element sum that *does* contain its element.
+
+      c) 'InjE' injects an element into a sum which does not contain any elements such that the sum will contain the injected element. 
+
+      d) 'InjF' injects an element into a sum which may or may not contain any elements, and preserves the containment status of 
+         the sum it was injected into. 
+
+      Suggestions for better constructor names are welcome. 
+
+5) PDataSum is a wrapper around a PDataSum' that contains exactly one of its elements. The structure of PDataSum' ensures that any 'PDataSum n 'True' 
+   will contain exactly one of its elements. An empty sum cannot be represented, and there is no way to inject a second element into a sum which already 
+   contains one element such that the resulting sum contains more than one of its elements. 
+
+6) We use a universally quantified continuation to indicate containment. I *think* we have to do it this way. 
+
+7) We need some helper utilities to reconstruct the Sum (which we have to do) in pmatch'. Those utilities are: 
+    
+    a) 'IndexedSum'. This is basically a singleton list a la SList from sop-core, except it can only represent nonempty lists, has an 
+       "explicit" inductive structure, and indicates the position of elements explicitly. 
+
+    b) A 'Length' type family. This doesn't work like 'length' from Prelude: 'Length '[a]' == 0, not 1. 
+
+    c) A type class 'IndexableSum' which is like SListI from sop-core, except for our 'IndexedSum' type. As far as I can tell it is impossible 
+       to reconstruct the sum without an inductive GADT that represents the structure of the sum. We need to conjure that structure out of thin air,
+       so we need a type class. 
+
+    d) Some dictionary stuff and an axiom to placate GHC and make this all typecheck.
+-}
+
 data PDataSum' :: Natural -> Bool -> [[PLabeledType]] -> PType where 
-  SumZE :: forall {x:: [PLabeledType]} {s :: S}
+  SumZ_ :: forall {x:: [PLabeledType]} {s :: S}
            . PDataSum' 0 'False '[x] s
 
-  SumZF :: forall {x:: [PLabeledType]} {s :: S}
+  SumZ :: forall {x:: [PLabeledType]} {s :: S}
            . (forall {out :: PType}. (Term s (PDataRecord x) -> Term s out) -> Term s out) 
            -> PDataSum' 0 'True '[x] s
 
-  InjE :: forall {n :: Natural} {x :: [PLabeledType]} {xss :: [[PLabeledType]]} {s :: S} 
+  Inj :: forall {n :: Natural} {x :: [PLabeledType]} {xss :: [[PLabeledType]]} {s :: S} 
         . (KnownNat (n+1))
         => (forall {out :: PType}. (Term s (PDataRecord x) -> Term s out) -> Term s out) 
         -> PDataSum' n 'False xss s 
         -> PDataSum' (n + 1) 'True (x ': xss) s 
 
-  InjF :: forall {n :: Natural} {b :: Bool} {x :: [PLabeledType]} {xss :: [[PLabeledType]]} {s :: S} 
+  Inj_ :: forall {n :: Natural} {b :: Bool} {x :: [PLabeledType]} {xss :: [[PLabeledType]]} {s :: S} 
         .  KnownNat (n+1)
         => PDataSum' n b xss s 
         -> PDataSum' (n + 1) b (x ': xss) s
@@ -224,7 +269,7 @@ type family Length (xss :: [k]) :: Nat where
 class IndexableSum (defs :: [k]) where 
   indexSum :: IndexedSum (Length defs) defs 
 
--- this is kind of a cheap hack and there's probably a better way to do this
+-- this is kind of a cheap hack and there's probably a better way 
 knownLength :: forall n ks. IndexedSum n ks -> Dict (KnownNat n)
 knownLength = \case 
   IxZ -> Dict 
@@ -243,7 +288,7 @@ addLength = UNSAFE.unsafeCoerce Refl
 -- maybe overlap-related pragmas?
 instance IndexableSum '[k] where 
   indexSum = IxZ 
-
+  
 instance IndexableSum ks => IndexableSum (k ': ks) where 
   indexSum = case addLength @k @ks of 
               Refl -> IxS (indexSum :: IndexedSum (Length ks) ks)
@@ -252,16 +297,17 @@ instance IndexableSum ks => IndexableSum (k ': ks) where
 instance IndexableSum defs => PlutusType (PDataSum defs) where
   type PInner (PDataSum defs) _ = PData 
 
+  -- this is reasonably straightforward. we don't need to worry about the SumZE case because it's impossible here 
   pcon' :: forall (s :: S) (b :: PType). PDataSum defs s -> Term s (PInner (PDataSum defs) b)
   pcon' (MkPDataSum s) = go s 
     where 
       go :: forall {n :: Natural} {defs :: [[PLabeledType]]} {s :: S}. PDataSum' n 'True defs s -> Term s (PInner (PDataSum defs) b)
       go = \case 
-        SumZF f ->  punsafeBuiltin PLC.ConstrData # (pconstant @PInteger (natVal $ Proxy @n)) # (f id) -- rewrite this with pconstrBuiltin
-        InjE f _ -> punsafeBuiltin PLC.ConstrData # (pconstant @PInteger (natVal $ Proxy @n)) # (f id) 
-        InjF pds -> go pds 
+        SumZ f ->  punsafeBuiltin PLC.ConstrData # (pconstant @PInteger (natVal $ Proxy @n)) # (f id) -- rewrite this with pconstrBuiltin?
+        Inj f _ -> punsafeBuiltin PLC.ConstrData # (pconstant @PInteger (natVal $ Proxy @n)) # (f id) 
+        Inj_ pds -> go pds 
 
-
+  -- this is more complicated 
   pmatch' :: forall (s :: S) (b :: PType)
            . Term s (PInner (PDataSum defs) b)
           -> (PDataSum defs s -> Term s b) 
@@ -270,30 +316,31 @@ instance IndexableSum defs => PlutusType (PDataSum defs) where
     plet (pasConstr #$ pforgetData $ pdata d) $ \d' ->
     plet (pfstBuiltin # d') $ \constr ->
     plet (psndBuiltin # d') $ \args ->
-      go 
-        constr args (indexSum :: IndexedSum (Length defs) defs) (f . MkPDataSum)                     
+      go constr args (indexSum :: IndexedSum (Length defs) defs) (f . MkPDataSum)                     
    where 
+    -- get the PInteger length of an IndexedSum. 
     iLen :: forall n as s. IndexedSum n as -> Term s PInteger 
     iLen ixed = pconstant $ natVal (Proxy @n) \\ knownLength ixed -- pconstant bad?
 
+    -- Used to "fill in" the remaining elements of the "right hand side" of a sum once we've determined which element the sum ought to contain.
     mkEmpty :: forall n as s. IndexedSum n as -> PDataSum' n 'False as s 
     mkEmpty = \case 
-      IxZ -> SumZE 
-      i@(IxS rest) -> InjF (mkEmpty rest) \\ knownLength i 
+      IxZ -> SumZ_
+      i@(IxS rest) -> Inj_ (mkEmpty rest) \\ knownLength i 
 
     go :: forall n as
-        . Term s PInteger 
-       -> Term s (PBuiltinList PData)  
-       -> IndexedSum (n) as
-       -> (PDataSum' n 'True as s -> Term s b)
-       -> Term s b 
+        . Term s PInteger -- the index of the constructor in the sum
+       -> Term s (PBuiltinList PData)  -- the inner representation of the record at the index of the first argument 
+       -> IndexedSum n as -- the GADT which represents the structure of the sum (can't construct it w/o this)
+       -> (PDataSum' n 'True as s -> Term s b) -- an accumulator function. represents an empty "left-hand side" of the sum 
+       -> Term s b                             
     go constr args ias@(IxS rest) g = 
-      pif (constr #== iLen ias)
-          (g $ InjE (\k -> k $ punsafeCoerce args) $ mkEmpty rest)
-          (go constr args rest (g . InjF))
-        \\ knownLength ias 
+      pif (constr #== iLen ias) -- if the index of the constructor matches the size of the sum
+          (g $ Inj (\k -> k $ punsafeCoerce args) $ mkEmpty rest) -- then cons an injection (which contains an element) onto an empty tail, and append the empty left hand side at the front
+          (go constr args rest (g . Inj_)) -- else recurse, "moving the accumulator forward by one"
+        \\ knownLength ias -- ghc is bad at math
 
-    go _ args IxZ g = g $ SumZF $ \k -> k $ punsafeCoerce args 
+    go _ args IxZ g = g $ SumZ $ \k -> k $ punsafeCoerce args -- if we've reached the end of the IndexedSum we have to do this, no point in checking that the indices match 
 
 instance PIsData (PDataSum defs) where
   pdata = punsafeCoerce
