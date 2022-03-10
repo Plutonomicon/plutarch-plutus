75c40eb15d22d2341208b87f1b8ebc6578bb6a13
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-03-10 17:14:10 +0530

    Add `PEq`/`POrd` deriving for data encoded types

--- Plutarch/DataRepr/Internal.hs
+++ Plutarch/DataRepr/Internal.hs
@@ -21,6 +21,8 @@ module Plutarch.DataRepr.Internal (
   pdropDataRecord,
   DerivePConstantViaData (..),
   pasDataSum,
+  pdToBuiltin,
+  LTReprHandlers (..),
 ) where
 
 import Data.Kind (Type)
@@ -61,13 +63,14 @@ import Plutarch (
   phoistAcyclic,
   plam,
   plet,
+  pmatch,
   pmatch',
   pto,
   (#),
   (#$),
   type (:-->),
  )
-import Plutarch.Bool (pif, (#==))
+import Plutarch.Bool (PBool, PEq, POrd, pif, (#<), (#<=), (#==), (#||))
 import Plutarch.Builtin (
   PAsData,
   PBuiltinList,
@@ -88,7 +91,7 @@ import Plutarch.Internal (S (SI))
 import Plutarch.Internal.Generic (MkSum (mkSum), PCode, PGeneric, gpfrom, gpto)
 import Plutarch.Lift (PConstant, PConstantRepr, PConstanted, PLift, pconstant, pconstantFromRepr, pconstantToRepr)
 import Plutarch.List (PListLike (pnil), pcons, pdrop, phead, ptail, ptryIndex)
-import Plutarch.TermCont (TermCont, hashOpenTerm, runTermCont)
+import Plutarch.TermCont (TermCont, hashOpenTerm, runTermCont, tcont, unTermCont)
 import Plutarch.Unsafe (punsafeCoerce)
 import qualified Plutus.V1.Ledger.Api as Ledger
 
@@ -121,6 +124,35 @@ instance PlutusType (PDataRecord '[]) where
   pcon' PDNil = pnil
   pmatch' _ f = f PDNil
 
+-- | This uses data equality. 'PEq' instances of elements don't make any difference.
+instance PEq (PDataRecord xs) where
+  x #== y = pdToBuiltin x #== pdToBuiltin y
+
+-- Lexicographic ordering based 'Ord' instances for 'PDataRecord'.
+
+instance POrd (PDataRecord '[]) where
+  _ #<= _ = pconstant True
+  _ #< _ = pconstant False
+
+instance (POrd x, PIsData x) => POrd (PDataRecord '[label ':= x]) where
+  l1 #< l2 = unTermCont $ do
+    PDCons x _ <- tcont $ pmatch l1
+    PDCons y _ <- tcont $ pmatch l2
+
+    pure $ pfromData x #< pfromData y
+  l1 #<= l2 = pdata (pto l1) #== pdata (pto l2) #|| l1 #< l2
+
+instance (POrd x, PIsData x, POrd (PDataRecord (x' ': xs))) => POrd (PDataRecord ((label ':= x) ': x' ': xs)) where
+  l1 #< l2 = unTermCont $ do
+    PDCons x xs <- tcont $ pmatch l1
+    PDCons y ys <- tcont $ pmatch l2
+
+    a <- tcont . plet $ pfromData x
+    b <- tcont . plet $ pfromData y
+
+    pure $ pif (a #< b) (pconstant True) $ pif (a #== b) (xs #< ys) $ pconstant False
+  l1 #<= l2 = pdata (pto l1) #== pdata (pto l2) #|| l1 #< l2
+
 {- | Cons a field to a data record.
 
 You can specify the label to associate with the field using type applications-
@@ -138,6 +170,10 @@ pdcons = punsafeCoerce $ pcons @PBuiltinList @PData
 -- | An empty 'PDataRecord'.
 pdnil :: Term s (PDataRecord '[])
 pdnil = punsafeCoerce $ pnil @PBuiltinList @PData
+
+-- | Convert a 'PDataRecord' into a builtin list of data values, losing type information in the process.
+pdToBuiltin :: Term s (PDataRecord xs) -> Term s (PBuiltinList PData)
+pdToBuiltin = punsafeCoerce
 
 data PLabeledType = Symbol := PType
 
@@ -177,6 +213,16 @@ data PDataSum (defs :: [[PLabeledType]]) (s :: S)
 instance PIsData (PDataSum defs) where
   pdata = punsafeCoerce
   pfromData = punsafeCoerce
+
+instance PEq (PDataSum defs) where
+  x #== y = pdata x #== pdata y
+
+instance MkLtReprHandler defs => POrd (PDataSum defs) where
+  x #< y = pmatchLT x y mkLtReprHandler
+  x #<= y = x #== y #|| x #< y
+
+pasDataSum :: PIsDataRepr a => Term s a -> Term s (PDataSum (PIsDataReprRepr a))
+pasDataSum = punsafeCoerce
 
 -- | If there is only a single variant, then we can safely extract it.
 punDataSum :: Term s (PDataSum '[def] :--> PDataRecord def)
@@ -260,11 +306,6 @@ pmatchDataSum d handlers =
               handler
               $ go common (idx + 1) rest constr
 
-{- | Use this for implementing the necessary instances for getting the `Data` representation.
- You must implement 'PIsDataRepr' to use this.
--}
-newtype PIsDataReprInstances (a :: PType) (s :: S) = PIsDataReprInstances (a s)
-
 -- TODO: This 'PMatch' constraint needs to be changed to 'PlutusType (breaking change).
 class (PMatch a, PIsData a) => PIsDataRepr (a :: PType) where
   type PIsDataReprRepr a :: [[PLabeledType]]
@@ -344,8 +385,77 @@ instance
       mkSOP :: NP (Term s) r -> SOP (Term s) (PCode s a)
       mkSOP = SOP . mkSum @_ @n @pcode
 
-pasDataSum :: PIsDataRepr a => Term s a -> Term s (PDataSum (PIsDataReprRepr a))
-pasDataSum = punsafeCoerce
+data LTReprHandlers (defs :: [[PLabeledType]]) (s :: S) where
+  LTRHNil :: LTReprHandlers '[] s
+  LTRHCons ::
+    (Term s (PDataRecord def) -> Term s (PDataRecord def) -> Term s PBool) ->
+    LTReprHandlers defs s ->
+    LTReprHandlers (def : defs) s
+
+-- | Optimized dual pmatch specialized for lexicographic '#<' (and potentially) '#<=' implementation.
+pmatchLT :: Term s (PDataSum defs) -> Term s (PDataSum defs) -> LTReprHandlers defs s -> Term s PBool
+pmatchLT d1 d2 (LTRHCons handler LTRHNil) = handler (punDataSum # d1) (punDataSum # d2)
+pmatchLT d1 d2 handlers = unTermCont $ do
+  a <- tcont . plet $ pasConstr #$ pforgetData $ pdata d1
+  b <- tcont . plet $ pasConstr #$ pforgetData $ pdata d2
+
+  cid1 <- tcont . plet $ pfstBuiltin # a
+  cid2 <- tcont . plet $ pfstBuiltin # b
+
+  pure $
+    pif
+      (cid1 #< cid2)
+      -- Left arg's constructor id is less, no need to continue.
+      (pconstant True)
+      $ pif
+        (cid1 #== cid2)
+        -- Matching constructors, compare fields now.
+        ( plet (psndBuiltin # a) $ \flds1 ->
+            plet (psndBuiltin # b) $ \flds2 ->
+              go 0 (applyHandlers flds1 flds2 handlers) cid1
+        )
+        -- Left arg's constructor id is greater, no need to continue.
+        $ pconstant False
+  where
+    applyHandlers :: Term s (PBuiltinList PData) -> Term s (PBuiltinList PData) -> LTReprHandlers defs s -> [Term s PBool]
+    applyHandlers _ _ LTRHNil = []
+    applyHandlers args1 args2 (LTRHCons handler rest) =
+      handler (punsafeCoerce args1) (punsafeCoerce args2) :
+      applyHandlers args1 args2 rest
+
+    go ::
+      Integer ->
+      [Term s PBool] ->
+      Term s PInteger ->
+      Term s PBool
+    go _ [] _ = error "pmatchDataSum2:go:empty handlers" -- empty 'PDataSum' shouldn't exist.
+    go idx [handler1, handler2] c =
+      pif
+        (fromInteger idx #== c)
+        handler1
+        handler2
+    go idx (handler : rest) c =
+      pif
+        (fromInteger idx #== c)
+        handler
+        $ go (idx + 1) rest c
+
+class MkLtReprHandler defs where
+  type FirstDef defs :: [PLabeledType]
+  mkLtReprHandler :: LTReprHandlers defs s
+
+instance POrd (PDataRecord def) => MkLtReprHandler '[def] where
+  type FirstDef '[def] = def
+  mkLtReprHandler = LTRHCons (#<) LTRHNil
+
+instance (POrd (PDataRecord def), MkLtReprHandler (def' ': defs)) => MkLtReprHandler (def ': def' ': defs) where
+  type FirstDef (def ': def' ': defs) = def
+  mkLtReprHandler = LTRHCons (#<) mkLtReprHandler
+
+{- | Use this for implementing the necessary instances for getting the `Data` representation.
+ You must implement 'PIsDataRepr' to use this.
+-}
+newtype PIsDataReprInstances (a :: PType) (s :: S) = PIsDataReprInstances (a s)
 
 instance PIsDataRepr a => PIsData (PIsDataReprInstances a) where
   pdata = punsafeCoerce
@@ -355,6 +465,15 @@ instance PIsDataRepr a => PlutusType (PIsDataReprInstances a) where
   type PInner (PIsDataReprInstances a) _ = PDataSum (PIsDataReprRepr a)
   pcon' (PIsDataReprInstances x) = pconRepr x
   pmatch' x f = pmatchRepr x (f . PIsDataReprInstances)
+
+-- | This uses data equality. 'PEq' instances of elements don't make any difference.
+instance PIsDataRepr a => PEq (PIsDataReprInstances a) where
+  x #== y = pdata x #== pdata y
+
+-- | This uses lexicographic ordering. Actually uses PDataSum '(#<)' implementation.
+instance (PIsDataRepr a, MkLtReprHandler (PIsDataReprRepr a)) => POrd (PIsDataReprInstances a) where
+  x #< y = pto x #< pto y
+  x #<= y = x #== y #|| x #< y
 
 newtype DerivePConstantViaData (h :: Type) (p :: PType) = DerivePConstantViaData h
 
