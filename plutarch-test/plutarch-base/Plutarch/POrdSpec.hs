85f7e3e6d77362dc71bf73305830b5d24da37e36
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-03-17 16:43:19 +0530

    Add golden tests for `POrd` derivation

--- plutarch-test/plutarch-base/Plutarch/POrdSpec.hs
+++ plutarch-test/plutarch-base/Plutarch/POrdSpec.hs
@@ -19,22 +19,83 @@ import Test.QuickCheck.Instances ()
 import Test.Syd
 import Test.Tasty.QuickCheck (Arbitrary, arbitrary, oneof, property)
 
-import Plutarch.Api.V1
+import Plutarch.Api.V1 (PAddress, PCredential (PPubKeyCredential, PScriptCredential), PMaybeData)
 import Plutarch.Lift (
   DerivePConstantViaNewtype (DerivePConstantViaNewtype),
   PConstant (PConstanted),
   PUnsafeLiftDecl (PLifted),
  )
 import Plutarch.Prelude
-import Plutarch.SpecTypes (PTriplet)
+
+import Plutarch.SpecTypes (PTriplet (PTriplet), Triplet (Triplet))
+import Plutarch.Test
 
 spec :: Spec
 spec = do
-  describe "ord.pisdata" $ do
-    propertySet @PBool "PBool"
-    propertySet @(PMaybeData PInteger) "PMaybeData PInteger"
-    propertySet @(PTriplet PInteger) "PMaybeData PInteger"
-    propertySet @PAddress' "PAddress"
+  describe "pisdata" $ do
+    describe "ord.property" $ do
+      propertySet @PBool "PBool"
+      propertySet @(PMaybeData PInteger) "PMaybeData PInteger"
+      propertySet @(PTriplet PInteger) "PMaybeData PInteger"
+      propertySet @PAddress' "PAddress"
+    describe "lt" . pgoldenSpec $ do
+      "PCredential" @\ do
+        let c1 = PubKeyCredential ""
+            c2 = ScriptCredential "41"
+        "derived" @\ ltWith (#<) c1 c2
+        "pmatch" @\ ltWith ltCred c1 c2
+        "pmatch-pdatarecord" @\ ltWith ltCred' c1 c2
+      "PTriplet" @\ do
+        let c1 = Triplet @Integer 1 2 3
+            c2 = Triplet 1 3 5
+        "derived" @\ ltWith (#<) c1 c2
+        "pmatch" @\ ltWith ltTrip c1 c2
+        "pmatch-pdatarecord" @\ ltWith ltTrip' c1 c2
+    describe "lte" . pgoldenSpec $ do
+      "PCredential" @\ do
+        let c1 = PubKeyCredential ""
+            c2 = ScriptCredential "41"
+        "derived" @\ lteWith (#<=) c1 c2
+        "pmatch" @\ lteWith lteCred c1 c2
+        "pmatch-pdatarecord" @\ lteWith lteCred' c1 c2
+      "PTriplet" @\ do
+        let c1 = Triplet @Integer 1 2 3
+            c2 = Triplet 1 3 5
+        "derived" @\ lteWith (#<=) c1 c2
+        "pmatch" @\ lteWith lteTrip c1 c2
+        "pmatch-pdatarecord" @\ lteWith lteTrip' c1 c2
+  where
+    ltWith ::
+      PLift p =>
+      (forall s. Term s p -> Term s p -> Term s PBool) ->
+      PLifted p ->
+      PLifted p ->
+      PlutarchGoldens
+    ltWith f x y = do
+      "true"
+        @| (pconstant x `f` pconstant y)
+        @-> passert
+      "false"
+        @| (pconstant y `f` pconstant x)
+        @-> passertNot
+    lteWith ::
+      PLift p =>
+      (forall s. Term s p -> Term s p -> Term s PBool) ->
+      PLifted p ->
+      PLifted p ->
+      PlutarchGoldens
+    lteWith f x y = do
+      "true"
+        @\ do
+          "eq"
+            @| (pconstant x `f` pconstant x)
+            @-> passert
+          "less"
+            @| (pconstant x `f` pconstant y)
+            @-> passert
+      "false"
+        @| (pconstant y `f` pconstant x)
+        @-> passertNot
 
 propertySet ::
   forall p.
@@ -93,3 +154,101 @@ instance Arbitrary Address' where
           , StakingPtr <$> arbitrary <*> arbitrary <*> arbitrary
           ]
       arbitraryMaybeStakingCred = oneof [pure Nothing, Just <$> arbitraryStakingCred]
+
+-- A bunch of boilerplate purely for a bit more informative benchmarks.
+
+-- manual 'pmatch' + manual field extraction impl.
+_pmatchHelperCred ::
+  (Term s PByteString -> Term s PByteString -> Term s PBool) ->
+  Term s PCredential ->
+  Term s PCredential ->
+  Term s PBool
+_pmatchHelperCred f cred1 cred2 = unTermCont $ do
+  x <- tcont $ pmatch cred1
+  y <- tcont $ pmatch cred2
+  pure $ case (x, y) of
+    (PPubKeyCredential a, PPubKeyCredential b) ->
+      pto (pfromData $ pfield @"_0" # a) `f` pto (pfromData $ pfield @"_0" # b)
+    (PPubKeyCredential _, PScriptCredential _) -> pconstant True
+    (PScriptCredential _, PPubKeyCredential _) -> pconstant False
+    (PScriptCredential a, PScriptCredential b) ->
+      pto (pfromData $ pfield @"_0" # a) `f` pto (pfromData $ pfield @"_0" # b)
+
+ltCred :: Term s PCredential -> Term s PCredential -> Term s PBool
+ltCred = _pmatchHelperCred (#<)
+
+lteCred :: Term s PCredential -> Term s PCredential -> Term s PBool
+lteCred = _pmatchHelperCred (#<=)
+
+-- manual 'pmatch' + 'PDataRecord' Ord impl.
+_pmatchDataRecHelperCred ::
+  (forall l. POrd (PDataRecord l) => Term s (PDataRecord l) -> Term s (PDataRecord l) -> Term s PBool) ->
+  Term s PCredential ->
+  Term s PCredential ->
+  Term s PBool
+_pmatchDataRecHelperCred f cred1 cred2 = unTermCont $ do
+  x <- tcont $ pmatch cred1
+  y <- tcont $ pmatch cred2
+  pure $ case (x, y) of
+    (PPubKeyCredential a, PPubKeyCredential b) -> a `f` b
+    (PPubKeyCredential _, PScriptCredential _) -> pconstant True
+    (PScriptCredential _, PPubKeyCredential _) -> pconstant False
+    (PScriptCredential a, PScriptCredential b) -> a `f` b
+
+ltCred' :: Term s PCredential -> Term s PCredential -> Term s PBool
+ltCred' = _pmatchDataRecHelperCred (#<)
+
+lteCred' :: Term s PCredential -> Term s PCredential -> Term s PBool
+lteCred' = _pmatchDataRecHelperCred (#<=)
+
+-- manual 'pmatch' + manual field extraction impl.
+ltTrip :: Term s (PTriplet PInteger) -> Term s (PTriplet PInteger) -> Term s PBool
+ltTrip trip1 trip2 = unTermCont $ do
+  a <- tcont $ pletFields @'["x", "y", "z"] trip1
+  b <- tcont $ pletFields @'["x", "y", "z"] trip2
+
+  x <- tcont . plet . pfromData $ hrecField @"x" a
+  x' <- tcont . plet . pfromData $ hrecField @"x" b
+  pure $
+    x #< x'
+      #|| ( x #== x'
+              #&& ( unTermCont $ do
+                      y <- tcont . plet . pfromData $ hrecField @"y" a
+                      y' <- tcont . plet . pfromData $ hrecField @"y" b
+                      pure $ y #< y' #|| (y #== y' #&& pfromData (hrecField @"z" a) #< pfromData (hrecField @"z" b))
+                  )
+          )
+
+lteTrip :: Term s (PTriplet PInteger) -> Term s (PTriplet PInteger) -> Term s PBool
+lteTrip trip1 trip2 = unTermCont $ do
+  a <- tcont $ pletFields @'["x", "y", "z"] trip1
+  b <- tcont $ pletFields @'["x", "y", "z"] trip2
+
+  x <- tcont . plet . pfromData $ hrecField @"x" a
+  x' <- tcont . plet . pfromData $ hrecField @"x" b
+  pure $
+    x #< x'
+      #|| ( x #== x'
+              #&& ( unTermCont $ do
+                      y <- tcont . plet . pfromData $ hrecField @"y" a
+                      y' <- tcont . plet . pfromData $ hrecField @"y" b
+                      pure $ y #< y' #|| (y #== y' #&& pfromData (hrecField @"z" a) #<= pfromData (hrecField @"z" b))
+                  )
+          )
+
+-- manual 'pmatch' + 'PDataRecord' Ord impl.
+_pmatchDataRecHelperTrip ::
+  (forall l. POrd (PDataRecord l) => Term s (PDataRecord l) -> Term s (PDataRecord l) -> Term s PBool) ->
+  Term s (PTriplet PInteger) ->
+  Term s (PTriplet PInteger) ->
+  Term s PBool
+_pmatchDataRecHelperTrip f trip1 trip2 = unTermCont $ do
+  PTriplet a <- tcont $ pmatch trip1
+  PTriplet b <- tcont $ pmatch trip2
+  pure $ a `f` b
+
+ltTrip' :: Term s (PTriplet PInteger) -> Term s (PTriplet PInteger) -> Term s PBool
+ltTrip' = _pmatchDataRecHelperTrip (#<)
+
+lteTrip' :: Term s (PTriplet PInteger) -> Term s (PTriplet PInteger) -> Term s PBool
+lteTrip' = _pmatchDataRecHelperTrip (#<=)
