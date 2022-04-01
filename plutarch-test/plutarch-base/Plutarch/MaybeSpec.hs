98eec12290a30259b62c9fd0b24027a29125ba15
Mario Blažević <blamario@protonmail.com>
2022-04-01 16:43:29 -0400

    Added tests for pmaybe and pfromJust

--- plutarch-test/plutarch-base/Plutarch/MaybeSpec.hs
+++ plutarch-test/plutarch-base/Plutarch/MaybeSpec.hs
@@ -3,7 +3,7 @@ module Plutarch.MaybeSpec (spec) where
 import Plutarch
 import Plutarch.Bool (PEq ((#==)))
 import Plutarch.Integer (PInteger)
-import Plutarch.Maybe (PMaybe (PJust, PNothing))
+import Plutarch.Maybe (PMaybe (PJust, PNothing), pfromJust, pmaybe)
 import Plutarch.Test
 import Test.Hspec
 
@@ -17,3 +17,12 @@ spec = do
       "false" @\ do
         "nothing-just" @| pcon @(PMaybe PInteger) PNothing #== pcon (PJust 42) @-> passertNot
         "just-just" @| pcon @(PMaybe PInteger) (PJust 24) #== pcon (PJust 42) @-> passertNot
+    "pfromJust" @\ do
+      "nothing" @| pfromJust # pcon PNothing @-> pfails
+      "just" @| pfromJust # pcon (PJust 42) #== (42 :: Term _ PInteger) @-> passert
+    "pmaybe" @\ do
+      "nothing" @| pmaybe # 0 # psucc # pcon PNothing #== (0 :: Term _ PInteger) @-> passert
+      "just" @| pmaybe # 0 # psucc # pcon (PJust 41) #== 42 @-> passert
+
+psucc :: ClosedTerm (PInteger :--> PInteger)
+psucc = phoistAcyclic (plam (+1))
