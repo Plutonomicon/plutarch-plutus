ee2349f8ac029a5c08fc06ba2d58b6830038724d
Sridhar Ratnakumar <srid@srid.ca>
2022-04-04 14:38:40 -0400

    Fix build

--- plutarch-test/common/Plutarch/Test/Run.hs
+++ plutarch-test/common/Plutarch/Test/Run.hs
@@ -23,7 +23,7 @@ noUnusedGoldens :: Spec -> IO ()
 noUnusedGoldens spec = do
   -- A second traversal here (`runSpecM`) can be obviated after
   -- https://github.com/hspec/hspec/issues/649
-  usedGoldens <- goldenPathsUsedBy <$> runSpecM spec
+  usedGoldens <- goldenPathsUsedBy . snd <$> runSpecM spec
   unusedGoldens usedGoldens >>= \case
     [] -> pure ()
     unused -> do
