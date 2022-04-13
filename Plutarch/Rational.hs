bbba37d19cc95ba97b227c34769438f3c46e2a42
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-04-13 19:35:23 +0530

    Force rational invariant in `PlutusType` instance

--- Plutarch/Rational.hs
+++ Plutarch/Rational.hs
@@ -11,7 +11,7 @@ module Plutarch.Rational (
 
 import Data.Ratio (denominator, numerator)
 import qualified GHC.Generics as GHC
-import Generics.SOP (Generic, I (I))
+import Generics.SOP (Generic)
 import Plutarch (
   PlutusType (..),
   Term,
@@ -40,12 +40,29 @@ import Plutarch.List (PListLike (pcons, phead, pnil, ptail), pmap)
 import Plutarch.Pair (PPair (..))
 import Plutarch.Show (PShow (pshow'), pshow)
 import Plutarch.Trace (ptraceError)
-import Plutarch.Unsafe (punsafeCoerce)
+import Plutarch.Unsafe (punsafeCoerce, punsafeFrom)
+import Plutarch.Lift (pconstant)
 
 data PRational s
   = PRational (Term s PInteger) (Term s PInteger)
   deriving stock (GHC.Generic)
-  deriving anyclass (Generic, PlutusType)
+  deriving anyclass (Generic)
+
+instance PlutusType PRational where
+  type PInner PRational b = (PInteger :--> PInteger :--> b) :--> b
+  pcon' (PRational x y) = failOnZero # y #$ plam $ \f -> f # x # y
+    where
+      failOnZero = phoistAcyclic $
+        plam $ \n x ->
+          pif
+            (n #== 0)
+            (ptraceError "pcon(PRational): division by 0")
+            x
+  pmatch' ratF f = ratF #$ plam $ \a b -> f $ PRational a b
+
+-- | 'PRational' builder that skips the "non-zero" check for denominator.
+punsafeRat :: Term s PInteger -> Term s PInteger -> Term s PRational
+punsafeRat x y = punsafeFrom $ plam $ \f -> f # x # y
 
 instance PShow PRational where
   pshow' _ x =
@@ -111,7 +128,7 @@ instance Num (Term s PRational) where
           preduce #$ pmatch x $
             \(PRational xn xd) ->
               pmatch y $ \(PRational yn yd) ->
-                pcon $ PRational (xn * yd + yn * xd) (xd * yd)
+                punsafeRat (xn * yd + yn * xd) (xd * yd)
       )
       # x'
       # y'
@@ -123,7 +140,7 @@ instance Num (Term s PRational) where
             #$ pmatch x
             $ \(PRational xn xd) ->
               pmatch y $ \(PRational yn yd) ->
-                pcon $ PRational (xn * yd - yn * xd) (xd * yd)
+                punsafeRat (xn * yd - yn * xd) (xd * yd)
       )
       # x'
       # y'
@@ -135,7 +152,7 @@ instance Num (Term s PRational) where
             #$ pmatch x
             $ \(PRational xn xd) ->
               pmatch y $ \(PRational yn yd) ->
-                pcon $ PRational (xn * yn) (xd * yd)
+                punsafeRat (xn * yn) (xd * yd)
       )
       # x'
       # y'
@@ -144,7 +161,7 @@ instance Num (Term s PRational) where
     phoistAcyclic
       ( plam $ \x ->
           pmatch x $ \(PRational xn xd) ->
-            pcon $ PRational (negate xn) xd
+            punsafeRat (negate xn) xd
       )
       # x'
 
@@ -152,7 +169,7 @@ instance Num (Term s PRational) where
     phoistAcyclic
       ( plam $ \x ->
           pmatch x $ \(PRational xn xd) ->
-            pcon $ PRational (abs xn) (abs xd)
+            punsafeRat (abs xn) (abs xd)
       )
       # x'
 
@@ -169,14 +186,14 @@ instance Num (Term s PRational) where
       )
       # x''
 
-  fromInteger n = pcon $ PRational (fromInteger n) 1
+  fromInteger n = punsafeFrom $ plam $ \f -> f # pconstant n # 1
 
 instance Fractional (Term s PRational) where
   recip x' =
     phoistAcyclic
       ( plam $ \x ->
           pmatch x $ \(PRational xn xd) ->
-            pfailOnZero # xn # (pcon (PRational xd xn))
+            pcon $ PRational xd xn
       )
       # x'
 
@@ -187,23 +204,13 @@ instance Fractional (Term s PRational) where
             #$ pmatch x
             $ \(PRational xn xd) ->
               pmatch y $ \(PRational yn yd) ->
-                plet (xd * yn) $ \den ->
-                  pfailOnZero # den
-                    #$ pcon (PRational (xn * yd) den)
+                plet (xd * yn) $ \den -> pcon $ PRational (xn * yd) den
       )
       # x'
       # y'
 
   fromRational r =
-    pcon $ PRational (fromInteger $ numerator r) (fromInteger $ denominator r)
-
-pfailOnZero :: Term s (PInteger :--> a :--> a)
-pfailOnZero = phoistAcyclic $
-  plam $ \n x ->
-    pif
-      (n #== 0)
-      (ptraceError "pfailOnZero: division by 0")
-      x
+    punsafeRat (pconstant $ numerator r) (pconstant $ denominator r)
 
 preduce :: Term s (PRational :--> PRational)
 preduce = phoistAcyclic $
@@ -211,7 +218,7 @@ preduce = phoistAcyclic $
     pmatch x $ \(PRational xn xd) ->
       plet (pgcd # xn # xd) $ \r ->
         plet (signum xd) $ \s ->
-          pcon $ PRational (s * pdiv # xn # r) (s * pdiv # xd # r)
+          punsafeRat (s * pdiv # xn # r) (s * pdiv # xd # r)
 
 pgcd :: Term s (PInteger :--> PInteger :--> PInteger)
 pgcd = phoistAcyclic $
@@ -245,7 +252,7 @@ pdenominator :: Term s (PRational :--> PInteger)
 pdenominator = phoistAcyclic $ plam $ \x -> pmatch x $ \(PRational _ d) -> d
 
 pfromInteger :: Term s (PInteger :--> PRational)
-pfromInteger = phoistAcyclic $ plam $ \n -> pcon $ PRational n 1
+pfromInteger = phoistAcyclic $ plam $ \n -> punsafeRat n 1
 
 pround :: Term s (PRational :--> PInteger)
 pround = phoistAcyclic $
