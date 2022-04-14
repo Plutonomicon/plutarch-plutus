b52e4e4646845f0ab8e1bae44b8e68e06cfacfa9
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-04-14 18:50:44 +0530

    Add `PNonZero`

---
+++ Plutarch/NonZero.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE UndecidableInstances #-}
+
+module Plutarch.NonZero (PNonZero, pnonZero, pnonZero') where
+
+import qualified GHC.Generics as GHC
+import Generics.SOP (Generic, HasDatatypeInfo, I (I))
+import Plutarch.Bool (PEq, POrd, pif, (#==))
+import Plutarch.Builtin (PIsData)
+import Plutarch.Integer (PInteger, PIntegral)
+import Plutarch.Internal.Other (
+  DerivePNewtype (DerivePNewtype),
+  PlutusType,
+  Term,
+  pcon,
+  phoistAcyclic,
+  plam,
+  type (:-->),
+ )
+import Plutarch.Maybe (PMaybe (PJust, PNothing))
+import Plutarch.Show (PShow)
+import Plutarch.Trace (ptraceError)
+
+newtype PNonZero s = PNonZero (Term s PInteger)
+  deriving stock (GHC.Generic)
+  deriving anyclass (Generic, HasDatatypeInfo, PShow)
+  deriving (PlutusType, PIsData, PEq, POrd, PIntegral) via (DerivePNewtype PNonZero PInteger)
+
+-- | Build a 'PNonZero' from a 'PInteger'. Yields 'PNothing' if argument is zero.
+pnonZero :: Term s (PInteger :--> PMaybe PNonZero)
+pnonZero = phoistAcyclic $
+  plam $ \i ->
+    pif
+      (i #== 0)
+      (pcon PNothing)
+      $ pcon . PJust . pcon $ PNonZero i
+
+-- | Partial version of 'pnonZero'. Errors if argument is zero.
+pnonZero' :: Term s (PInteger :--> PNonZero)
+pnonZero' = phoistAcyclic $
+  plam $ \i ->
+    pif
+      (i #== 0)
+      (ptraceError "pnonZero: building with 0")
+      $ pcon $ PNonZero i
