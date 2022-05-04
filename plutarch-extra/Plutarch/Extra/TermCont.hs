01bdff0fc4d78f80a80d013167ff72a5ced8b19b
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-05-04 21:58:07 +0530

    Add more `TermCont` utilities

--- plutarch-extra/Plutarch/Extra/TermCont.hs
+++ plutarch-extra/Plutarch/Extra/TermCont.hs
@@ -1,13 +1,21 @@
-{- | TermCont-related adapters for Plutarch functions.
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
-  TODO: More functions (pletFieldsC, ptraceC, ...) need to be added here.
--}
+-- | TermCont-related adapters for Plutarch functions.
 module Plutarch.Extra.TermCont (
   pletC,
   pmatchC,
+  pletFieldsC,
+  ptraceC,
+  pguardC,
+  pguardC',
+  ptryFromC,
 ) where
 
+import Plutarch.DataRepr
+import Plutarch.DataRepr.Internal.Field
 import Plutarch.Prelude
+import Plutarch.Reducible
+import Plutarch.TryFrom
 
 -- | Like `plet` but works in a `TermCont` monad
 pletC :: Term s a -> TermCont s (Term s a)
@@ -16,3 +24,61 @@ pletC = tcont . plet
 -- | Like `pmatch` but works in a `TermCont` monad
 pmatchC :: PMatch a => Term s a -> TermCont s (a s)
 pmatchC = tcont . pmatch
+
+-- | Like `pletFields` but works in a `TermCont` monad.
+pletFieldsC ::
+  forall fs a s b ps bs.
+  ( PDataFields a
+  , ps ~ PFields a
+  , bs ~ Bindings ps fs
+  , BindFields ps bs
+  ) =>
+  Term s a ->
+  TermCont @b s (HRec (BoundTerms ps bs s))
+pletFieldsC x = tcont $ pletFields @fs x
+
+{- | Like `ptrace` but works in a `TermCont` monad.
+
+=== Example ===
+
+@
+foo :: Term s PUnit
+foo = unTermCont $ do
+  ptraceC "returning unit!"
+  pure $ pconstant ()
+@
+-}
+ptraceC :: Term s PString -> TermCont s ()
+ptraceC s = tcont $ \f -> ptrace s (f ())
+
+{- | Trace a message and raise error if 'cond' is false. Otherwise, continue.
+
+=== Example ===
+
+@
+onlyAllow42 :: Term s (PInteger :--> PUnit)
+onlyAllow42 = plam $ \i -> unTermCont $ do
+  pguardC "expected 42" $ i #== 42
+  pure $ pconstant ()
+@
+-}
+pguardC :: Term s PString -> Term s PBool -> TermCont s ()
+pguardC s cond = tcont $ \f -> pif cond (f ()) $ ptraceError s
+
+{- | Stop computation and return given term if 'cond' is false. Otherwise, continue.
+
+=== Example ===
+
+@
+is42 :: Term s (PInteger :--> PBool)
+is42 = plam $ \i -> unTermCont $ do
+  pguardC "expected 42" (pconstant False) $ i #== 42
+  pure $ pconstant True
+@
+-}
+pguardC' :: Term s a -> Term s PBool -> TermCont @a s ()
+pguardC' r cond = tcont $ \f -> pif cond (f ()) r
+
+-- | 'TermCont' producing version of 'ptryFrom'.
+ptryFromC :: forall b r a s. PTryFrom a b => Term s a -> TermCont @r s (Term s b, Reduce (PTryFromExcess a b s))
+ptryFromC = tcont . ptryFrom
