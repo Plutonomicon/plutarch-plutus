88916a081d8b0276ba3af3b380f495afa9f06aff
chase <44284917+TotallyNotChase@users.noreply.github.com>
2022-05-16 23:01:07 +0530

    `PTryFrom` instances for `PNonZero`

--- Plutarch/NonZero.hs
+++ Plutarch/NonZero.hs
@@ -2,10 +2,12 @@
 
 module Plutarch.NonZero (PNonZero, pnonZero, ptryNonZero) where
 
+import Data.Functor.Const (Const)
 import qualified GHC.Generics as GHC
 import Generics.SOP (Generic, HasDatatypeInfo, I (I))
+
 import Plutarch.Bool (PEq, POrd, pif, (#==))
-import Plutarch.Builtin (PIsData)
+import Plutarch.Builtin (PAsData, PData, PIsData, pdata)
 import Plutarch.Integer (PInteger, PIntegral)
 import Plutarch.Internal.Other (
   DerivePNewtype (DerivePNewtype),
@@ -14,16 +16,32 @@ import Plutarch.Internal.Other (
   pcon,
   phoistAcyclic,
   plam,
+  plet,
+  (#),
   type (:-->),
  )
 import Plutarch.Maybe (PMaybe (PJust, PNothing))
+import Plutarch.Reducible (Flip)
 import Plutarch.Show (PShow)
+import Plutarch.TermCont (runTermCont, tcont)
 import Plutarch.Trace (ptraceError)
+import Plutarch.TryFrom (PTryFrom (PTryFromExcess, ptryFrom'), ptryFrom)
 
 newtype PNonZero s = PNonZero (Term s PInteger)
   deriving stock (GHC.Generic)
   deriving anyclass (Generic, HasDatatypeInfo, PShow)
   deriving (PlutusType, PIsData, PEq, POrd, PIntegral) via (DerivePNewtype PNonZero PInteger)
+
+instance PTryFrom PInteger PNonZero where
+  type PTryFromExcess PInteger PNonZero = Const ()
+  ptryFrom' opq = runTermCont $ pure (ptryNonZero # opq, ())
+
+instance PTryFrom PData (PAsData PNonZero) where
+  type PTryFromExcess PData (PAsData PNonZero) = Flip Term PNonZero
+  ptryFrom' opq = runTermCont $ do
+    (_, i) <- tcont $ ptryFrom @(PAsData PInteger) opq
+    res <- tcont . plet $ ptryNonZero # i
+    pure (pdata res, res)
 
 -- | Build a 'PNonZero' from a 'PInteger'. Yields 'PNothing' if argument is zero.
 pnonZero :: Term s (PInteger :--> PMaybe PNonZero)
