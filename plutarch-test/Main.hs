eb402180e63c4e31b1926a2f16fc7d7f40203e76
Las Safin <me@las.rs>
2022-06-01 17:19:43 +0000

    Fix issue with fourmolu

--- plutarch-test/Main.hs
+++ plutarch-test/Main.hs
@@ -21,19 +21,17 @@ main :: IO ()
 main = do
   setLocaleEncoding utf8
 
-{-
--- We test for unused goldens, but do so only in GHC 9. Because, under GHC 8
--- certain modules are disabled (see the CPP below) which leads to legitimately
--- unused goldens detected leading to false positive in test failure.
-#if MIN_VERSION_GLASGOW_HASKELL(9,0,0,0)
-  noUnusedGoldens =<< hspecAndReturnForest spec
-#else
-  hspec spec
-#endif
--}
-
   -- FIXME: Re-enable unused golden checks
+  -- Old:
+  -- We test for unused goldens, but do so only in GHC 9. Because, under GHC 8
+  -- certain modules are disabled (see the CPP below) which leads to legitimately
+  -- unused goldens detected leading to false positive in test failure.
+  -- #if MIN_VERSION_GLASGOW_HASKELL(9,0,0,0)
+  --  noUnusedGoldens =<< hspecAndReturnForest spec
+  -- #else
   hspec spec
+
+-- #endif
 
 spec :: Spec
 spec = do
