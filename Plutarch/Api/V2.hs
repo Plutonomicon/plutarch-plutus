c4e359bcfff4e9aee73aec51045aadd15c793df6
mangoiv <mail@mangoiv.com>
2022-07-03 13:39:34 +0200

    removed nonsense from V2.hs and added TODO list

--- Plutarch/Api/V2.hs
+++ Plutarch/Api/V2.hs
@@ -1,12 +1,6 @@
 module Plutarch.Api.V2 (
   ) where
 
-import qualified Plutarch.Api.V1 as V1
-import qualified Plutus.Api.V2 as PlutusV2
-
-{- TODO:
-    - scriptHash and
-      (validatorHash, stakeValidatorHash, mintingPolicyHash)
-    - dataHash and
-      (datumHash, redeemerHash)
--}
+-- TODO:
+-- - meaningful reesports
+-- - add util functions that are also part of PlutusLedgerApi.V2
