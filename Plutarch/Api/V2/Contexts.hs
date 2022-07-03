5176221f0392186695d597952188ef72d3a8cd4f
mangoiv <mail@mangoiv.com>
2022-07-03 13:36:12 +0200

    finished adding types

--- Plutarch/Api/V2/Contexts.hs
+++ Plutarch/Api/V2/Contexts.hs
@@ -1,12 +1,31 @@
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Plutarch.Api.V2.Contexts (
   PScriptContext (PScriptContext),
+  PTxInfo (PTxInfo),
+  PScriptPurpose (PMinting, PSpending, PRewarding, PCertifying),
 ) where
 
+import qualified Plutarch.Api.V1.Address as V1
+import qualified Plutarch.Api.V1.Crypto as V1
+import qualified Plutarch.Api.V1.DCert as V1
+import qualified Plutarch.Api.V1.Scripts as V1
+import qualified Plutarch.Api.V1.Time as V1
+import qualified Plutarch.Api.V1.Tuple as V1
 import qualified Plutarch.Api.V1.Value as V1
-import Plutarch.Api.V2.Tx (PTxInInfo)
+import Plutarch.Api.V2.Tx (PTxId, PTxInInfo, PTxOut, PTxOutRef)
 import qualified PlutusLedgerApi.V2 as Plutus
 
-import Plutarch.DataRepr (PDataFields)
+import Plutarch.DataRepr (
+  DerivePConstantViaData (DerivePConstantViaData),
+  PDataFields,
+ )
+import Plutarch.Lift (
+  PConstantDecl,
+  PLifted,
+  PUnsafeLiftDecl,
+ )
 import Plutarch.Prelude
 
 -- FIXME: add PDataFields to Prelude
@@ -25,6 +44,11 @@ newtype PScriptContext (s :: S)
   deriving stock (Generic)
   deriving anyclass (PlutusType, PIsData, PDataFields, PEq)
 
+instance DerivePlutusType PScriptContext where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl PScriptContext where type PLifted _ = Plutus.ScriptContext
+deriving via (DerivePConstantViaData Plutus.ScriptContext PScriptContext) instance PConstantDecl Plutus.ScriptContext
+
 -- | A pending transaction. This is the view as seen by the validator script.
 newtype PTxInfo (s :: S)
   = PTxInfo
@@ -32,15 +56,15 @@ newtype PTxInfo (s :: S)
           s
           ( PDataRecord
               '[ "inputs" ':= PBuiltinList (PAsData PTxInInfo) -- Transaction inputs
-               , "refInputs" ':= PBuiltinList (PAsData PTxInInfo)
+               , "referenceInputs" ':= PBuiltinList (PAsData PTxInInfo)
                , "outputs" ':= PBuiltinList (PAsData PTxOut) -- Transaction outputs
-               , "fee" ':= V1.PValue V1.Sorted V1.Positive -- The fee paid by this transaction.
-               , "mint" ':= V1.PValue V1.Sorted V1.NoGuarantees -- The value minted by the transaction.
-               , "dcert" ':= PBuiltinList (PAsData PDCert) -- Digests of the certificates included in this transaction.
-               , "wdrl" ':= PBuiltinList (PAsData (PTuple PStakingCredential PInteger)) -- Staking withdrawals
-               , "validRange" ':= PPOSIXTimeRange -- The valid range for the transaction.
-               , "signatories" ':= PBuiltinList (PAsData PPubKeyHash) -- Signatories attesting that they all signed the tx.
-               , "datums" ':= PBuiltinList (PAsData (PTuple PDatumHash PDatum))
+               , "fee" ':= V1.PValue 'V1.Sorted 'V1.Positive -- The fee paid by this transaction.
+               , "mint" ':= V1.PValue 'V1.Sorted 'V1.NoGuarantees -- The value minted by the transaction.
+               , "dcert" ':= PBuiltinList (PAsData V1.PDCert) -- Digests of the certificates included in this transaction.
+               , "wdrl" ':= PBuiltinList (PAsData (V1.PTuple V1.PStakingCredential PInteger)) -- Staking withdrawals
+               , "validRange" ':= V1.PPOSIXTimeRange -- The valid range for the transaction.
+               , "signatories" ':= PBuiltinList (PAsData V1.PPubKeyHash) -- Signatories attesting that they all signed the tx.
+               , "datums" ':= PBuiltinList (PAsData (V1.PTuple V1.PDatumHash V1.PDatum))
                , "id" ':= PTxId -- The hash of the pending transaction.
                ]
           )
@@ -49,3 +73,19 @@ newtype PTxInfo (s :: S)
   deriving anyclass (PlutusType, PIsData, PDataFields, PEq)
 
 instance DerivePlutusType PTxInfo where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl PTxInfo where type PLifted _ = Plutus.TxInfo
+deriving via (DerivePConstantViaData Plutus.TxInfo PTxInfo) instance PConstantDecl Plutus.TxInfo
+
+data PScriptPurpose (s :: S)
+  = PMinting (Term s (PDataRecord '["_0" ':= V1.PCurrencySymbol]))
+  | PSpending (Term s (PDataRecord '["_0" ':= PTxOutRef]))
+  | PRewarding (Term s (PDataRecord '["_0" ':= V1.PStakingCredential]))
+  | PCertifying (Term s (PDataRecord '["_0" ':= V1.PDCert]))
+  deriving stock (Generic)
+  deriving anyclass (PlutusType, PIsData, PEq)
+
+instance DerivePlutusType PScriptPurpose where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl PScriptPurpose where type PLifted PScriptPurpose = Plutus.ScriptPurpose
+deriving via (DerivePConstantViaData Plutus.ScriptPurpose PScriptPurpose) instance PConstantDecl Plutus.ScriptPurpose
--- Plutarch/Api/V2/Tx.hs
+++ Plutarch/Api/V2/Tx.hs
@@ -1,48 +1,48 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
+
 module Plutarch.Api.V2.Tx (
-  PTxInInfo (PTxInInfo),
+  PTxOutRef (PTxOutRef),
   PTxOut (PTxOut),
+  PTxId (PTxId),
+  PTxInInfo (PTxInInfo),
   POutputDatum (POutputDatumHash, PNoOutputDatum, POutputDatum),
-  V1.PTxId (V1.PTxId),
-  V1.PTxOutRef (V1.PTxOutRef),
 ) where
 
 import qualified Plutarch.Api.V1.Address as V1
 import qualified Plutarch.Api.V1.Maybe as V1
 import qualified Plutarch.Api.V1.Scripts as V1
-import qualified Plutarch.Api.V1.Tx as V1
 import qualified Plutarch.Api.V1.Value as V1
 import Plutarch.DataRepr (
-  PDataFields,
   DerivePConstantViaData (DerivePConstantViaData),
+  PDataFields,
  )
-import Plutarch.Prelude
-import qualified PlutusLedgerApi.V2 as Plutus
 import Plutarch.Lift (
   PConstantDecl,
   PLifted,
   PUnsafeLiftDecl,
  )
+import Plutarch.Prelude
+import qualified PlutusLedgerApi.V2 as Plutus
 
--- | A input of the pending transaction.
-newtype PTxInInfo (s :: S)
-  = PTxInInfo
+-- | Reference to a transaction output with a index referencing which of the outputs is being referred to.
+newtype PTxOutRef (s :: S)
+  = PTxOutRef
       ( Term
           s
           ( PDataRecord
-              '[ "outRef" ':= V1.PTxOutRef
-               , "resolved" ':= PTxOut
+              '[ "id" ':= PTxId
+               , "idx" ':= PInteger
                ]
           )
       )
   deriving stock (Generic)
-  deriving anyclass (PlutusType, PIsData, PDataFields, PEq)
+  deriving anyclass (PlutusType, PIsData, PDataFields, PEq, POrd)
 
-instance DerivePlutusType PTxInInfo where type DPTStrat _ = PlutusTypeData
+instance DerivePlutusType PTxOutRef where type DPTStrat _ = PlutusTypeData
 
-instance PUnsafeLiftDecl PTxInInfo where type PLifted PTxInInfo = Plutus.TxInInfo
-deriving via (DerivePConstantViaData Plutus.TxInInfo PTxInInfo) instance PConstantDecl Plutus.TxInInfo
+instance PUnsafeLiftDecl PTxOutRef where type PLifted PTxOutRef = Plutus.TxOutRef
+deriving via (DerivePConstantViaData Plutus.TxOutRef PTxOutRef) instance PConstantDecl Plutus.TxOutRef
 
 -- | A transaction output. This consists of a target address, value and maybe a datum hash
 newtype PTxOut (s :: S)
@@ -60,20 +60,48 @@ newtype PTxOut (s :: S)
       )
   deriving stock (Generic)
   deriving anyclass (PlutusType, PIsData, PDataFields, PEq)
-instance DerivePlutusType PTxOut where type DPTStrat _ = PlutusTypeData
 
+instance DerivePlutusType PTxOut where type DPTStrat _ = PlutusTypeData
 
 instance PUnsafeLiftDecl PTxOut where type PLifted PTxOut = Plutus.TxOut
 deriving via (DerivePConstantViaData Plutus.TxOut PTxOut) instance PConstantDecl Plutus.TxOut
+
+newtype PTxId (s :: S)
+  = PTxId (Term s (PDataRecord '["_0" ':= PByteString]))
+  deriving stock (Generic)
+  deriving anyclass (PlutusType, PIsData, PDataFields, PEq, POrd)
+instance DerivePlutusType PTxId where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl PTxId where type PLifted PTxId = Plutus.TxId
+deriving via (DerivePConstantViaData Plutus.TxId PTxId) instance PConstantDecl Plutus.TxId
+
+-- | A input of the pending transaction.
+newtype PTxInInfo (s :: S)
+  = PTxInInfo
+      ( Term
+          s
+          ( PDataRecord
+              '[ "outRef" ':= PTxOutRef
+               , "resolved" ':= PTxOut
+               ]
+          )
+      )
+  deriving stock (Generic)
+  deriving anyclass (PlutusType, PIsData, PDataFields, PEq)
+
+instance DerivePlutusType PTxInInfo where type DPTStrat _ = PlutusTypeData
+
+instance PUnsafeLiftDecl PTxInInfo where type PLifted PTxInInfo = Plutus.TxInInfo
+deriving via (DerivePConstantViaData Plutus.TxInInfo PTxInInfo) instance PConstantDecl Plutus.TxInInfo
 
 -- | The datum attached to an output: either nothing, a datum hash or an inline datum (CIP 32)
 data POutputDatum (s :: S)
   = PNoOutputDatum (Term s (PDataRecord '[]))
   | POutputDatumHash (Term s (PDataRecord '["datumHash" ':= V1.PDatumHash]))
   | POutputDatum (Term s (PDataRecord '["outputDatum" ':= V1.PDatum]))
-  deriving stock Generic
+  deriving stock (Generic)
   deriving anyclass (PlutusType, PIsData, PEq)
--- FIXME: can we just anyclass derive DerivePlutusNewtype by applying it to strategy?
+
 instance DerivePlutusType POutputDatum where type DPTStrat _ = PlutusTypeData
 
 instance PUnsafeLiftDecl POutputDatum where type PLifted POutputDatum = Plutus.OutputDatum
