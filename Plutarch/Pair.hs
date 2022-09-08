4bb1ba88454540a771820d737448471bb3ef104e
cjay <680672+cjay@users.noreply.github.com>
2022-09-08 15:41:04 +0200

    PPair formatting

--- Plutarch/Pair.hs
+++ Plutarch/Pair.hs
@@ -1,7 +1,7 @@
 module Plutarch.Pair (PPair (..)) where
 
 import GHC.Generics (Generic)
-import Plutarch.Bool (pif, PEq ((#==)), PPartialOrd ((#<=), (#<)), POrd)
+import Plutarch.Bool (PEq ((#==)), POrd, PPartialOrd ((#<), (#<=)), pif)
 import Plutarch.Internal (PType, S, Term)
 import Plutarch.Internal.PlutusType (DPTStrat, DerivePlutusType, PlutusType, pmatch)
 import Plutarch.Internal.ScottEncoding (PlutusTypeScott)
@@ -24,15 +24,19 @@ instance (PPartialOrd a, PPartialOrd b) => PPartialOrd (PPair a b) where
   a #<= b = unTermCont $ do
     PPair a1 a2 <- tcont $ pmatch a
     PPair b1 b2 <- tcont $ pmatch b
-    pure $ pif (a1 #== b1)
-      (a2 #<= b2)
-      (a1 #<= b1)
+    pure $
+      pif
+        (a1 #== b1)
+        (a2 #<= b2)
+        (a1 #<= b1)
 
   a #< b = unTermCont $ do
     PPair a1 a2 <- tcont $ pmatch a
     PPair b1 b2 <- tcont $ pmatch b
-    pure $ pif (a1 #== b1)
-      (a2 #< b2)
-      (a1 #< b1)
+    pure $
+      pif
+        (a1 #== b1)
+        (a2 #< b2)
+        (a1 #< b1)
 
 instance (POrd a, POrd b) => POrd (PPair a b)
