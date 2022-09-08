c40b8fb0754567d2204873294df53752da3c08a8
cjay <680672+cjay@users.noreply.github.com>
2022-09-08 15:34:53 +0200

    Added PPartialOrd and POrd instances for PPair

--- Plutarch/Pair.hs
+++ Plutarch/Pair.hs
@@ -1,11 +1,12 @@
 module Plutarch.Pair (PPair (..)) where
 
 import GHC.Generics (Generic)
-import Plutarch.Bool (PEq)
+import Plutarch.Bool (pif, PEq ((#==)), PPartialOrd ((#<=), (#<)), POrd)
 import Plutarch.Internal (PType, S, Term)
-import Plutarch.Internal.PlutusType (DPTStrat, DerivePlutusType, PlutusType)
+import Plutarch.Internal.PlutusType (DPTStrat, DerivePlutusType, PlutusType, pmatch)
 import Plutarch.Internal.ScottEncoding (PlutusTypeScott)
 import Plutarch.Show (PShow)
+import Plutarch.TermCont (tcont, unTermCont)
 
 {- |
   Plutus encoding of Pairs.
@@ -18,3 +19,20 @@ data PPair (a :: PType) (b :: PType) (s :: S)
   deriving anyclass (PlutusType, PEq, PShow)
 
 instance DerivePlutusType (PPair a b) where type DPTStrat _ = PlutusTypeScott
+
+instance (PPartialOrd a, PPartialOrd b) => PPartialOrd (PPair a b) where
+  a #<= b = unTermCont $ do
+    PPair a1 a2 <- tcont $ pmatch a
+    PPair b1 b2 <- tcont $ pmatch b
+    pure $ pif (a1 #== b1)
+      (a2 #<= b2)
+      (a1 #<= b1)
+
+  a #< b = unTermCont $ do
+    PPair a1 a2 <- tcont $ pmatch a
+    PPair b1 b2 <- tcont $ pmatch b
+    pure $ pif (a1 #== b1)
+      (a2 #< b2)
+      (a1 #< b1)
+
+instance (POrd a, POrd b) => POrd (PPair a b)
