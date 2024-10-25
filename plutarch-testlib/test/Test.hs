13bd2d139604a08c0644b21ded75365d353c1853
t4ccer <t4ccer@gmail.com>
2024-10-25 13:22:01 -0600

    Port `ShowSpec` to new golden test setup

--- plutarch-testlib/test/Test.hs
+++ plutarch-testlib/test/Test.hs
@@ -17,6 +17,7 @@ import Plutarch.Test.Suite.Plutarch.POrd qualified as POrd
 import Plutarch.Test.Suite.Plutarch.Pair qualified as Pair
 import Plutarch.Test.Suite.Plutarch.Rational qualified as Rational
 import Plutarch.Test.Suite.Plutarch.Recursion qualified as Recursion
+import Plutarch.Test.Suite.Plutarch.Show qualified as Show
 import Plutarch.Test.Suite.PlutarchLedgerApi.Regressions qualified as Regressions
 import Plutarch.Test.Suite.PlutarchLedgerApi.V1 qualified as V1
 import Plutarch.Test.Suite.PlutarchLedgerApi.V2 qualified as V2
@@ -56,6 +57,7 @@ main = do
         , POrd.tests
         , Rational.tests
         , Recursion.tests
+        , Show.tests
         ]
     , testGroup
         "PlutarchLedgerApi"
