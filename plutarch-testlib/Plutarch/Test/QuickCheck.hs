94263e52ec6e1699b43739a8ab36065025b5cf4c
t4ccer <t4ccer@gmail.com>
2024-10-26 21:08:44 -0600

    Add scaffold for QuickCheck tests with the same interface as unit and goldens

---
+++ plutarch-testlib/Plutarch/Test/QuickCheck.hs
@@ -0,0 +1,62 @@
+module Plutarch.Test.QuickCheck (propEvalFail, propCompileFail, propEvalEqual) where
+
+import Data.Text qualified as Text
+import Plutarch (Config (NoTracing))
+import Plutarch.Prelude
+import Plutarch.Test.Unit (TermResult (Evaluated, FailedToCompile, FailedToEvaluate), evalTermResult)
+import Test.Tasty (TestName, TestTree)
+import Test.Tasty.QuickCheck (
+  Arbitrary,
+  arbitrary,
+  counterexample,
+  forAllShrinkShow,
+  property,
+  shrink,
+  testProperty,
+  (===),
+ )
+
+-- | Like `Plutarch.Test.Unit.testEvalFail` but generate terms
+propEvalFail ::
+  (Arbitrary a, Show a) =>
+  TestName ->
+  (a -> ClosedTerm b) ->
+  TestTree
+propEvalFail name mkTerm =
+  testProperty name $ forAllShrinkShow arbitrary shrink show $ \(input :: a) ->
+    case evalTermResult NoTracing (mkTerm input) of
+      FailedToCompile err -> counterexample ("Failed to compile: " <> Text.unpack err) False
+      FailedToEvaluate _ _ -> property True
+      Evaluated script _ -> counterexample ("Evaluated, but expected failure: " <> script) False
+
+-- | Like `Plutarch.Test.Unit.testCompileFail` but generate terms
+propCompileFail ::
+  (Arbitrary a, Show a) =>
+  TestName ->
+  (a -> ClosedTerm b) ->
+  TestTree
+propCompileFail name mkTerm =
+  testProperty name $ forAllShrinkShow arbitrary shrink show $ \(input :: a) ->
+    case evalTermResult NoTracing (mkTerm input) of
+      FailedToCompile _ -> property True
+      FailedToEvaluate err _ -> counterexample ("Failed to evaluate: " <> show err) False
+      Evaluated script _ -> counterexample ("Evaluated, but expected failure: " <> script) False
+
+-- | Like `Plutarch.Test.Unit.testEvalEqual` but generate terms
+propEvalEqual ::
+  (Arbitrary a, Show a) =>
+  TestName ->
+  -- | Actual
+  (a -> ClosedTerm b) ->
+  -- | Expected
+  (a -> ClosedTerm b) ->
+  TestTree
+propEvalEqual name mkTerm mkExpected =
+  testProperty name $ forAllShrinkShow arbitrary shrink show $ \(input :: a) ->
+    case evalTermResult NoTracing (mkTerm input) of
+      FailedToCompile err -> counterexample ("Failed to compile: " <> Text.unpack err) False
+      FailedToEvaluate err _ -> counterexample ("Failed to evaluate: " <> show err) False
+      Evaluated actual _ -> case evalTermResult NoTracing (mkExpected input) of
+        FailedToCompile err -> counterexample ("Failed to compile expected term: " <> Text.unpack err) False
+        FailedToEvaluate err _ -> counterexample ("Failed to evaluate expected term: " <> show err) False
+        Evaluated expected _ -> actual === expected
--- plutarch-testlib/Plutarch/Test/Unit.hs
+++ plutarch-testlib/Plutarch/Test/Unit.hs
@@ -5,6 +5,8 @@ module Plutarch.Test.Unit (
   testEvalFail,
   testEvalEqual,
   testEvalEqualTraces,
+  TermResult (..),
+  evalTermResult,
 ) where
 
 import Data.Text (Text)
@@ -16,7 +18,7 @@ import Plutarch (
   compile,
   printScript,
  )
-import Plutarch.Evaluate (evalScript)
+import Plutarch.Evaluate (EvalError, evalScript)
 import Plutarch.Prelude
 import Test.Tasty (TestName, TestTree)
 import Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
@@ -24,30 +26,26 @@ import Test.Tasty.HUnit (assertEqual, assertFailure, testCase)
 -- | Assert that term compiled and evaluated without errors
 testEval :: TestName -> ClosedTerm a -> TestTree
 testEval name term = testCase name $ do
-  compiledTerm <- case compile (Tracing LogDebug DetTracing) term of
-    Left err -> assertFailure $ "Failed to compile: " <> Text.unpack err
-    Right compiledTerm -> pure compiledTerm
-  case evalScript compiledTerm of
-    (Left err, _, _) -> assertFailure $ "Failed to eval: " <> show err
-    (Right _, _, _) -> pure ()
+  case evalTermResult (Tracing LogDebug DetTracing) term of
+    FailedToCompile err -> assertFailure $ "Failed to compile: " <> Text.unpack err
+    FailedToEvaluate err _ -> assertFailure $ "Failed to evaluate: " <> show err
+    Evaluated _ _ -> pure ()
 
 -- | Assert that term compiled correctly but evaluated with errors
 testEvalFail :: TestName -> ClosedTerm a -> TestTree
 testEvalFail name term = testCase name $ do
-  compiledTerm <- case compile NoTracing term of
-    Left err -> assertFailure $ "Failed to compile: " <> Text.unpack err
-    Right compiledTerm -> pure compiledTerm
-  case evalScript compiledTerm of
-    (Left _, _, _) -> pure ()
-    (Right evaluatedTerm, _, _) ->
-      assertFailure ("Evaluated, but expected failure:\n" <> printScript evaluatedTerm)
+  case evalTermResult NoTracing term of
+    FailedToCompile err -> assertFailure $ "Failed to compile: " <> Text.unpack err
+    FailedToEvaluate _ _ -> pure ()
+    Evaluated script _ -> assertFailure ("Evaluated, but expected failure:\n" <> script)
 
 -- | Assert that term failed to compile
 testCompileFail :: TestName -> ClosedTerm a -> TestTree
 testCompileFail name term = testCase name $ do
-  case compile NoTracing term of
-    Left _ -> pure ()
-    Right _ -> assertFailure "Compiled, but expected failure"
+  case evalTermResult NoTracing term of
+    FailedToCompile _ -> pure ()
+    FailedToEvaluate err _ -> assertFailure $ "Failed to evaluate: " <> show err
+    Evaluated script _ -> assertFailure $ "Evaluated, but expected failure: " <> script
 
 {- | Assert that term compiled and evaluated without errors and matches the expected value
 note that comparison is done on AST level, not by `Eq` or `PEq`
@@ -60,28 +58,36 @@ testEvalEqual ::
   ClosedTerm a ->
   TestTree
 testEvalEqual name term expectedTerm = testCase name $ do
-  compiledTerm <- case compile NoTracing term of
-    Left err -> assertFailure $ "Failed to compile: " <> Text.unpack err
-    Right compiledTerm -> pure compiledTerm
-  evaluatedTerm <- case evalScript compiledTerm of
-    (Left err, _, _) -> assertFailure $ "Failed to eval: " <> show err
-    (Right evaluatedTerm, _, _) -> pure evaluatedTerm
-  compiledExpected <- case compile NoTracing expectedTerm of
-    Left err -> assertFailure $ "Failed to compile expected value: " <> Text.unpack err
-    Right compiledTerm -> pure compiledTerm
-  evaluatedExpected <- case evalScript compiledExpected of
-    (Left err, _, _) -> assertFailure $ "Failed to eval expected value: " <> show err
-    (Right evaluatedTerm, _, _) -> pure evaluatedTerm
-  assertEqual "" (printScript evaluatedExpected) (printScript evaluatedTerm)
+  actual <- case evalTermResult NoTracing term of
+    FailedToCompile err -> assertFailure $ "Failed to compile: " <> Text.unpack err
+    FailedToEvaluate err _ -> assertFailure $ "Failed to evaluate: " <> show err
+    Evaluated script _ -> pure script
+  case evalTermResult NoTracing expectedTerm of
+    FailedToCompile err -> assertFailure $ "Failed to compile expected term: " <> Text.unpack err
+    FailedToEvaluate err _ -> assertFailure $ "Failed to evaluate expected term: " <> show err
+    Evaluated expected _ -> assertEqual "" expected actual
 
 {- | Assert that term compiled (with specified tracing level and `TracingMode.DetTracing`) and evaluated
 without errors produced traces that match expected value. Note that this succeeds even if script
 evaluated to error if traces still match
 -}
 testEvalEqualTraces :: TestName -> ClosedTerm a -> LogLevel -> [Text] -> TestTree
-testEvalEqualTraces name term traceLevel expected = testCase name $ do
-  compiledTerm <- case compile (Tracing traceLevel DetTracing) term of
-    Left err -> assertFailure $ "Failed to compile: " <> Text.unpack err
-    Right compiledTerm -> pure compiledTerm
-  let (_, _, traces) = evalScript compiledTerm
-  assertEqual "" expected traces
+testEvalEqualTraces name term traceLevel expected = testCase name $
+  case evalTermResult (Tracing traceLevel DetTracing) term of
+    FailedToCompile err -> assertFailure $ "Failed to compile: " <> Text.unpack err
+    FailedToEvaluate _ traces -> assertEqual "" expected traces
+    Evaluated _ traces -> assertEqual "" expected traces
+
+data TermResult
+  = FailedToCompile Text
+  | FailedToEvaluate EvalError [Text]
+  | Evaluated String [Text]
+
+evalTermResult :: Config -> ClosedTerm a -> TermResult
+evalTermResult config term =
+  case compile config term of
+    Left err -> FailedToCompile err
+    Right compiledTerm ->
+      case evalScript compiledTerm of
+        (Left err, _, traces) -> FailedToEvaluate err traces
+        (Right evaluated, _, traces) -> Evaluated (printScript evaluated) traces
