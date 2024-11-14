4b67fd3c199a97a12d1378a49237f380ece0b7a3
Seungheon Oh <seungheon.ooh@gmail.com>
2024-11-13 21:58:27 -0600

    Generate docs and haddock from staging document instead of master

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       ];
       debug = true;
       systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      hercules-ci.github-pages.branch = "master";
+      hercules-ci.github-pages.branch = "staging";
 
       perSystem = { config, system, lib, self', ... }:
         let
