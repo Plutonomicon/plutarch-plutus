676204dc5a21ca823242b2e828934f85a603e236
Koz Ross <koz.ross@retro-freedom.nz>
2024-12-03 14:36:37 +1300

    Reorganize Plutarch.Repr.Scott

--- Plutarch/Repr/Scott.hs
+++ Plutarch/Repr/Scott.hs
@@ -12,11 +12,11 @@ module Plutarch.Repr.Scott (
 ) where
 
 import Data.Kind (Type)
+import Data.Proxy (Proxy (Proxy))
 import GHC.Exts (Any)
-import Generics.SOP
+import Generics.SOP (Code, NP (Nil, (:*)), NS (S, Z), SOP (SOP))
 import Generics.SOP qualified as SOP
-import Generics.SOP.Constraint
-
+import Generics.SOP.Constraint (All, All2, Head, SListI, SListI2)
 import Plutarch.Internal.Eq (PEq, (#==))
 import Plutarch.Internal.PLam (plam)
 import Plutarch.Internal.PlutusType (
@@ -34,24 +34,37 @@ import Plutarch.Internal.PlutusType (
   pmatch',
  )
 import Plutarch.Internal.Quantification (PForall)
-import Plutarch.Internal.Term (PDelayed, S, Term, pdelay, pforce, phoistAcyclic, plam', punsafeCoerce, (#), (:-->))
+import Plutarch.Internal.Term (
+  PDelayed,
+  S,
+  Term,
+  pdelay,
+  pforce,
+  phoistAcyclic,
+  plam',
+  punsafeCoerce,
+  (#),
+  (:-->),
+ )
+import Plutarch.Repr.Internal (
+  PRec (PRec, unPRec),
+  PStruct (PStruct, unPStruct),
+  RecTypePrettyError,
+  StructSameRepr,
+  UnTermRec,
+  UnTermStruct,
+  grecEq,
+  gstructEq,
+  pletL,
+ )
 
-import Plutarch.Repr.Internal (PRec (PRec, unPRec), PStruct (PStruct, unPStruct), RecTypePrettyError, StructSameRepr, UnTermRec, UnTermStruct, grecEq, gstructEq, pletL)
+-- | @since WIP
+newtype PScottStruct (struct :: [[S -> Type]]) (s :: S) = PScottStruct
+  { unPScottStruct :: PStruct struct s
+  -- ^ @since WIP
+  }
 
-newtype PScottStruct (struct :: [[S -> Type]]) (s :: S) = PScottStruct {unPScottStruct :: PStruct struct s}
-newtype PScottRec (struct :: [S -> Type]) (s :: S) = PScottRec {unPScottRec :: PRec struct s}
-
--- This could be better
-newtype PScottStructInner a r s = PScottStructInner (Term s (ScottFn (ScottList a r) r))
-newtype PScottRecInner a r s = PScottRecInner (Term s (ScottFn a r))
-
--- What whatever unholy reason, quantification on constrain only works if like this
-class SListI (ScottList struct r) => PScottStructConstraint' struct r
-instance SListI (ScottList struct r) => PScottStructConstraint' struct r
-
-class (SListI struct, forall r. PScottStructConstraint' struct r) => PScottStructConstraint struct
-instance (SListI struct, forall r. PScottStructConstraint' struct r) => PScottStructConstraint struct
-
+-- | @since WIP
 instance forall struct. (SListI2 struct, PScottStructConstraint struct) => PlutusType (PScottStruct struct) where
   type PInner (PScottStruct struct) = PForall (PScottStructInner struct) -- try `Any`
   type PCovariant' (PScottStruct struct) = All2 PCovariant'' struct
@@ -60,6 +73,29 @@ instance forall struct. (SListI2 struct, PScottStructConstraint struct) => Plutu
   pcon' (PScottStruct x) = punsafeCoerce $ pconScottStruct @struct x
   pmatch' x f = pmatchScottStruct @struct (punsafeCoerce x) (f . PScottStruct)
 
+-- NOTE/TODO: Performance has not been tested for this. I feel like this would be terribly slow
+-- NOTE: Hoisting here is so that there's no duplicated computation on each argument.
+-- GHC freaks out when I put hoist part in a let.
+
+-- | @since WIP
+instance (PlutusType (PScottStruct struct), SListI2 struct, All2 PEq struct) => PEq (PScottStruct struct) where
+  x #== y =
+    phoistAcyclic
+      ( plam $ \x' y' ->
+          pmatch x' $ \(PScottStruct (PStruct x'')) ->
+            pmatch y' $ \(PScottStruct (PStruct y'')) ->
+              gstructEq x'' y''
+      )
+      # x
+      # y
+
+-- | @since WIP
+newtype PScottRec (struct :: [S -> Type]) (s :: S) = PScottRec
+  { unPScottRec :: PRec struct s
+  -- ^ @since WIP
+  }
+
+-- | @since WIP
 instance SListI struct => PlutusType (PScottRec struct) where
   type PInner (PScottRec struct) = PForall (PScottRecInner struct)
   type PCovariant' (PScottRec struct) = All PCovariant'' struct
@@ -68,8 +104,7 @@ instance SListI struct => PlutusType (PScottRec struct) where
   pcon' (PScottRec x) = punsafeCoerce $ pconScottRec x
   pmatch' x f = pmatchScottRec (punsafeCoerce x) (f . PScottRec)
 
---------------------------------------------------------------------------------
-
+-- | @since WIP
 instance All PEq struct => PEq (PScottRec struct) where
   x #== y =
     phoistAcyclic
@@ -81,19 +116,77 @@ instance All PEq struct => PEq (PScottRec struct) where
       # x
       # y
 
--- NOTE/TODO: Performance has not been tested for this. I feel like this would be terribly slow
--- NOTE: Hoisting here is so that there's no duplicated computation on each argument.
--- GHC freaks out when I put hoist part in a let.
-instance (PlutusType (PScottStruct struct), SListI2 struct, All2 PEq struct) => PEq (PScottStruct struct) where
-  x #== y =
-    phoistAcyclic
-      ( plam $ \x' y' ->
-          pmatch x' $ \(PScottStruct (PStruct x'')) ->
-            pmatch y' $ \(PScottStruct (PStruct y'')) ->
-              gstructEq x'' y''
-      )
-      # x
-      # y
+-- This could be better
+
+-- | @since WIP
+newtype PScottStructInner a r s = PScottStructInner (Term s (ScottFn (ScottList a r) r))
+
+-- | @since WIP
+newtype PScottRecInner a r s = PScottRecInner (Term s (ScottFn a r))
+
+-- | @since WIP
+newtype DeriveAsScottStruct (a :: S -> Type) s = DeriveAsScottStruct
+  { unDeriveAsScottStruct :: a s
+  -- ^ @since WIP
+  }
+
+-- | @since WIP
+instance
+  forall (a :: S -> Type) (struct :: [[S -> Type]]).
+  ( SOP.Generic (a Any)
+  , struct ~ UnTermStruct (a Any)
+  , SListI2 struct
+  , forall s. StructSameRepr s a struct
+  , PScottStructConstraint struct
+  ) =>
+  PlutusType (DeriveAsScottStruct a)
+  where
+  type PInner (DeriveAsScottStruct a) = PScottStruct (UnTermStruct (a Any))
+  type PCovariant' (DeriveAsScottStruct a) = (PCovariant' a)
+  type PContravariant' (DeriveAsScottStruct a) = (PContravariant' a)
+  type PVariant' (DeriveAsScottStruct a) = (PVariant' a)
+  pcon' (DeriveAsScottStruct x) =
+    pcon @(PScottStruct (UnTermStruct (a Any))) $ PScottStruct $ PStruct $ SOP.hcoerce $ SOP.from x
+  pmatch' x f =
+    pmatch @(PScottStruct (UnTermStruct (a Any))) x (f . DeriveAsScottStruct . SOP.to . SOP.hcoerce . unPStruct . unPScottStruct)
+
+-- | @since WIP
+newtype DeriveAsScottRec (a :: S -> Type) s = DeriveAsScottRec
+  { unDeriveAsScottRec :: a s
+  -- ^ @since WIP
+  }
+
+-- | @since WIP
+instance
+  forall (a :: S -> Type) (struct' :: [Type]) (struct :: [S -> Type]).
+  ( SOP.Generic (a Any)
+  , '[struct'] ~ Code (a Any)
+  , struct ~ UnTermRec struct'
+  , SListI struct
+  , forall s. StructSameRepr s a '[struct]
+  , RecTypePrettyError (Code (a Any))
+  ) =>
+  PlutusType (DeriveAsScottRec a)
+  where
+  type PInner (DeriveAsScottRec a) = PScottRec (UnTermRec (Head (Code (a Any))))
+  type PCovariant' (DeriveAsScottRec a) = (PCovariant' a)
+  type PContravariant' (DeriveAsScottRec a) = (PContravariant' a)
+  type PVariant' (DeriveAsScottRec a) = (PVariant' a)
+  pcon' (DeriveAsScottRec x) =
+    pcon $ PScottRec $ PRec $ SOP.unZ $ SOP.unSOP $ SOP.hcoerce $ SOP.from x
+  pmatch' x f =
+    pmatch x (f . DeriveAsScottRec . SOP.to . SOP.hcoerce . SOP . (Z @_ @_ @'[]) . unPRec . unPScottRec)
+
+-- Helpers
+
+-- What whatever unholy reason, quantification on constrain only works if like this
+class SListI (ScottList struct r) => PScottStructConstraint' struct r
+
+instance SListI (ScottList struct r) => PScottStructConstraint' struct r
+
+class (SListI struct, forall r. PScottStructConstraint' struct r) => PScottStructConstraint struct
+
+instance (SListI struct, forall r. PScottStructConstraint' struct r) => PScottStructConstraint struct
 
 --------------------------------------------------------------------------------------
 
@@ -117,23 +210,25 @@ newtype PLamL' s b as = PLamL' {unPLamL' :: (NP (Term s) as -> Term s b) -> Term
 
 -- Explicitly variadic `plam`.
 plamL' :: SListI as => (NP (Term s) as -> Term s b) -> Term s (ScottFn' as b)
-plamL' = unPLamL' $ para_SList (PLamL' \f -> f Nil) (\(PLamL' prev) -> PLamL' \f -> plam' \a -> prev \as -> f (a :* as))
+plamL' =
+  unPLamL' $ SOP.para_SList (PLamL' \f -> f Nil) (\(PLamL' prev) -> PLamL' \f -> plam' \a -> prev \as -> f (a :* as))
 
 newtype PLamL s b as = PLamL {unPLamL :: (NP (Term s) as -> Term s b) -> Term s (ScottFn as b)}
 
 -- `pdelay`s the 0-arity case.
 plamL :: SListI as => (NP (Term s) as -> Term s b) -> Term s (ScottFn as b)
-plamL = unPLamL $ case_SList (PLamL \f -> pdelay $ f Nil) (PLamL plamL')
+plamL = unPLamL $ SOP.case_SList (PLamL \f -> pdelay $ f Nil) (PLamL plamL')
 
 newtype PAppL' s r as = PAppL' {unPAppL' :: Term s (ScottFn' as r) -> NP (Term s) as -> Term s r}
 
 pappL' :: SListI as => Term s (ScottFn' as c) -> NP (Term s) as -> Term s c
-pappL' = unPAppL' $ para_SList (PAppL' \f Nil -> f) (\(PAppL' prev) -> PAppL' \f (x :* xs) -> prev (f # x) xs)
+pappL' =
+  unPAppL' $ SOP.para_SList (PAppL' \f Nil -> f) (\(PAppL' prev) -> PAppL' \f (x :* xs) -> prev (f # x) xs)
 
 newtype PAppL s r as = PAppL {unPAppL :: Term s (ScottFn as r) -> NP (Term s) as -> Term s r}
 
 pappL :: forall as r s. SListI as => Term s (ScottFn as r) -> NP (Term s) as -> Term s r
-pappL = unPAppL $ case_SList (PAppL \f Nil -> pforce f) (PAppL pappL')
+pappL = unPAppL $ SOP.case_SList (PAppL \f Nil -> pforce f) (PAppL pappL')
 
 -- Note, we don't have to use delay unit here because when value is unit, that will be the only branch that will get run.
 pconScottRec ::
@@ -159,7 +254,7 @@ newtype GPCon' s r as = GPCon' {unGPCon' :: NP (Term s) (ScottList as r) -> PStr
 
 gpcon' :: SListI2 as => NP (Term s) (ScottList as r) -> PStruct as s -> Term s r
 gpcon' = unGPCon' $
-  cpara_SList
+  SOP.cpara_SList
     (Proxy @SListI)
     (GPCon' \Nil -> \case {})
     \(GPCon' prev) -> GPCon' \(arg :* args) -> \case
@@ -184,7 +279,7 @@ gpmatch' ::
   SListI2 as =>
   (PStruct as s -> Term s r) ->
   NP (Term s) (ScottList as r)
-gpmatch' = unGPMatch' $ cpara_SList (Proxy @SListI) (GPMatch' (const Nil)) \(GPMatch' prev) -> GPMatch' \f ->
+gpmatch' = unGPMatch' $ SOP.cpara_SList (Proxy @SListI) (GPMatch' (const Nil)) \(GPMatch' prev) -> GPMatch' \f ->
   plamL (\args -> f (PStruct $ SOP $ Z args)) :* prev (\(PStruct (SOP x)) -> f (PStruct $ SOP (S x)))
 
 pmatchScottStruct ::
@@ -196,45 +291,3 @@ pmatchScottStruct ::
   (PStruct struct s -> Term s r) ->
   Term s r
 pmatchScottStruct xs f = pappL (punsafeCoerce xs) (gpmatch' f)
-
---------------------------------------------------------------------------------
-
-newtype DeriveAsScottStruct (a :: S -> Type) s = DeriveAsScottStruct {unDeriveAsScottStruct :: a s}
-
-instance
-  forall (a :: S -> Type) (struct :: [[S -> Type]]).
-  ( SOP.Generic (a Any)
-  , struct ~ UnTermStruct (a Any)
-  , SListI2 struct
-  , forall s. StructSameRepr s a struct
-  , PScottStructConstraint struct
-  ) =>
-  PlutusType (DeriveAsScottStruct a)
-  where
-  type PInner (DeriveAsScottStruct a) = PScottStruct (UnTermStruct (a Any))
-  type PCovariant' (DeriveAsScottStruct a) = (PCovariant' a)
-  type PContravariant' (DeriveAsScottStruct a) = (PContravariant' a)
-  type PVariant' (DeriveAsScottStruct a) = (PVariant' a)
-  pcon' (DeriveAsScottStruct x) = pcon @(PScottStruct (UnTermStruct (a Any))) $ PScottStruct $ PStruct $ hcoerce $ from x
-  pmatch' x f = pmatch @(PScottStruct (UnTermStruct (a Any))) x (f . DeriveAsScottStruct . to . hcoerce . unPStruct . unPScottStruct)
-
-newtype DeriveAsScottRec (a :: S -> Type) s = DeriveAsScottRec {unDeriveAsScottRec :: a s}
-
-instance
-  forall (a :: S -> Type) (struct' :: [Type]) (struct :: [S -> Type]).
-  ( SOP.Generic (a Any)
-  , '[struct'] ~ Code (a Any)
-  , struct ~ UnTermRec struct'
-  , SListI struct
-  , forall s. StructSameRepr s a '[struct]
-  , RecTypePrettyError (Code (a Any))
-  ) =>
-  PlutusType (DeriveAsScottRec a)
-  where
-  type PInner (DeriveAsScottRec a) = PScottRec (UnTermRec (Head (Code (a Any))))
-  type PCovariant' (DeriveAsScottRec a) = (PCovariant' a)
-  type PContravariant' (DeriveAsScottRec a) = (PContravariant' a)
-  type PVariant' (DeriveAsScottRec a) = (PVariant' a)
-  pcon' (DeriveAsScottRec x) =
-    pcon $ PScottRec $ PRec $ unZ $ unSOP $ hcoerce $ from x
-  pmatch' x f = pmatch x (f . DeriveAsScottRec . to . hcoerce . SOP . (Z @_ @_ @'[]) . unPRec . unPScottRec)
