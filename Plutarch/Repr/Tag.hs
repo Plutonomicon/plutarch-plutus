4d2a8060bfcb2e77ed9770431daef25e45fc8d3d
Koz Ross <koz.ross@retro-freedom.nz>
2024-12-03 14:42:52 +1300

    Reorganize Plutarch.Repr.Tag

--- Plutarch/Repr/Tag.hs
+++ Plutarch/Repr/Tag.hs
@@ -3,7 +3,13 @@
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
 
-module Plutarch.Repr.Tag where
+module Plutarch.Repr.Tag (
+  PTag (..),
+  DeriveAsTag (..),
+  TagLiftHelper (..),
+) where
+
+import Data.Proxy (Proxy (Proxy))
 
 -- parts have been commented. They will have to be restored after PLiftalbe rework
 -- import Data.Coerce (coerce)
@@ -11,15 +17,21 @@ import Data.Kind (Type)
 
 -- import GHC.Exts (Any)
 import GHC.Generics qualified as GHC
-import GHC.TypeError
-import GHC.TypeLits
-import Generics.SOP
+import GHC.TypeError (ErrorMessage (ShowType, Text, (:$$:), (:<>:)), TypeError)
+import GHC.TypeLits (type (+))
+import Generics.SOP (
+  All,
+  Code,
+  I,
+  K (K),
+  NP (Nil, (:*)),
+  NS (S, Z),
+  SOP (SOP),
+ )
 import Generics.SOP qualified as SOP
-
 import Plutarch.Builtin.Integer (PInteger)
 
 -- import Plutarch.Builtin.Opaque (POpaque, popaque)
-
 import Plutarch.Internal.Lift (pconstant)
 import Plutarch.Internal.PlutusType (
   PContravariant',
@@ -31,20 +43,84 @@ import Plutarch.Internal.PlutusType (
   pmatch',
  )
 import Plutarch.Internal.Term (S, Term)
-
 import Plutarch.Repr.Internal (groupHandlers)
 import Plutarch.Repr.Newtype (DeriveAsNewtype (DeriveAsNewtype))
 import Plutarch.TermCont (pletC, unTermCont)
 
-newtype PTag (struct :: [S -> Type]) (s :: S) = PTag {unPTag :: Term s PInteger}
+-- | @since WIP
+newtype PTag (struct :: [S -> Type]) (s :: S) = PTag
+  { unPTag :: Term s PInteger
+  -- ^ @since WIP
+  }
   deriving stock (GHC.Generic)
 
+-- | @since WIP
 instance SOP.Generic (PTag struct s)
+
+-- | @since WIP
 deriving via DeriveAsNewtype (PTag struct) instance PlutusType (PTag struct)
 
-newtype DeriveAsTag (a :: S -> Type) s = DeriveAsTag {unDeriveAsTag :: a s}
+-- | @since WIP
+newtype DeriveAsTag (a :: S -> Type) s = DeriveAsTag
+  { unDeriveAsTag :: a s
+  -- ^ @since WIP
+  }
+
+{- | This derives tag-only PlutusType automatically. Resulted instances will use `PInteger` as underlying type, making this much more efficient than using regular Data/Scott/SOP based encoding. As name suggests, types with no-argument constructors can use this.
+
+Example:
+@@
+data PFoo s = A | B | C | D | E
+  deriving stock (GHC.Generic, Show)
+  deriving anyclass (PEq, PIsData)
+  deriving (PlutusType, PLiftable)
+    via DeriveAsTag PFoo
+
+instance SOP.Generic (PFoo s)
+@@
+
+@since WIP
+-}
+instance (forall s. TagTypeConstraints s a struct) => PlutusType (DeriveAsTag a) where
+  type PInner (DeriveAsTag a) = PInteger
+  type PCovariant' (DeriveAsTag a) = (PCovariant' a)
+  type PContravariant' (DeriveAsTag a) = (PContravariant' a)
+  type PVariant' (DeriveAsTag a) = (PVariant' a)
+  pcon' :: forall s. DeriveAsTag a s -> Term s (PInner (DeriveAsTag a))
+  pcon' (DeriveAsTag x) =
+    pconstant @PInteger $ toInteger $ SOP.hindex $ SOP.from x
+
+  pmatch' :: forall s b. Term s (PInner (DeriveAsTag a)) -> (DeriveAsTag a s -> Term s b) -> Term s b
+  pmatch' tag f = unTermCont $ do
+    -- plet here because tag might be a big computation
+    tag' <- pletC tag
+    let
+      g :: SOP I (Code (a s)) -> Term s b
+      g = f . DeriveAsTag . SOP.to
+
+      go :: forall x xs. IsEmpty x => TagMatchHandler s b xs -> TagMatchHandler s b (x ': xs)
+      go (TagMatchHandler rest) =
+        TagMatchHandler $ \idx handle ->
+          K (idx, handle $ SOP $ Z Nil) :* rest (idx + 1) (\(SOP x) -> handle $ SOP $ S x)
+
+      handlers' :: TagMatchHandler s b struct
+      handlers' = SOP.cpara_SList (Proxy @IsEmpty) (TagMatchHandler $ \_ _ -> Nil) go
+
+      handlers :: [(Integer, Term s b)]
+      handlers = SOP.hcollapse $ unTagMatchHandler handlers' 0 g
+
+    pure $ groupHandlers handlers tag'
+
+-- | @since WIP
+newtype TagLiftHelper r struct = TagLiftHelper
+  { unTagLiftHelper :: Integer -> (SOP I struct -> r) -> r
+  -- ^ @since WIP
+  }
+
+-- Helpers
 
 class x ~ '[] => IsEmpty (x :: [k])
+
 instance x ~ '[] => IsEmpty '[]
 
 type family TagTypePrettyError' n (xs :: [[Type]]) :: Bool where
@@ -66,50 +142,6 @@ instance (SOP.Generic (a s), TagTypePrettyError (Code (a s)), Code (a s) ~ struc
 newtype TagMatchHandler s b struct = TagMatchHandler
   { unTagMatchHandler :: Integer -> (SOP I struct -> Term s b) -> NP (K (Integer, Term s b)) struct
   }
-
-{- | This derives tag-only PlutusType automatically. Resulted instances will use `PInteger` as underlying type, making this much more efficient than using regular Data/Scott/SOP based encoding. As name suggests, types with no-argument constructors can use this.
-
-Example:
-@@
-data PFoo s = A | B | C | D | E
-  deriving stock (GHC.Generic, Show)
-  deriving anyclass (PEq, PIsData)
-  deriving (PlutusType, PLiftable)
-    via DeriveAsTag PFoo
-
-instance SOP.Generic (PFoo s)
--}
-instance (forall s. TagTypeConstraints s a struct) => PlutusType (DeriveAsTag a) where
-  type PInner (DeriveAsTag a) = PInteger
-  type PCovariant' (DeriveAsTag a) = (PCovariant' a)
-  type PContravariant' (DeriveAsTag a) = (PContravariant' a)
-  type PVariant' (DeriveAsTag a) = (PVariant' a)
-  pcon' :: forall s. DeriveAsTag a s -> Term s (PInner (DeriveAsTag a))
-  pcon' (DeriveAsTag x) =
-    pconstant @PInteger $ toInteger $ hindex $ from x
-
-  pmatch' :: forall s b. Term s (PInner (DeriveAsTag a)) -> (DeriveAsTag a s -> Term s b) -> Term s b
-  pmatch' tag f = unTermCont $ do
-    -- plet here because tag might be a big computation
-    tag' <- pletC tag
-    let
-      g :: SOP I (Code (a s)) -> Term s b
-      g = f . DeriveAsTag . to
-
-      go :: forall x xs. IsEmpty x => TagMatchHandler s b xs -> TagMatchHandler s b (x ': xs)
-      go (TagMatchHandler rest) =
-        TagMatchHandler $ \idx handle ->
-          K (idx, handle $ SOP $ Z Nil) :* rest (idx + 1) (\(SOP x) -> handle $ SOP $ S x)
-
-      handlers' :: TagMatchHandler s b struct
-      handlers' = cpara_SList (Proxy @IsEmpty) (TagMatchHandler $ \_ _ -> Nil) go
-
-      handlers :: [(Integer, Term s b)]
-      handlers = hcollapse $ unTagMatchHandler handlers' 0 g
-
-    pure $ groupHandlers handlers tag'
-
-newtype TagLiftHelper r struct = TagLiftHelper {unTagLiftHelper :: Integer -> (SOP I struct -> r) -> r}
 
 -- instance
 --   ( PlutusType (DeriveAsTag a)
