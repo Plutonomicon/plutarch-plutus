1ed287fbdbfb69bd4dce9c3bfa79dc612846f26b
Koz Ross <koz.ross@retro-freedom.nz>
2024-12-03 13:35:44 +1300

    Reorganize Plutarch.Repr.Internal

--- Plutarch/Repr/Internal.hs
+++ Plutarch/Repr/Internal.hs
@@ -18,78 +18,55 @@ module Plutarch.Repr.Internal (
 import Data.Kind (Type)
 import Data.List (groupBy, sortBy)
 import Data.Proxy (Proxy (Proxy))
-import GHC.TypeError
-import Generics.SOP (All, All2, AllZipN, Code, I, K (K), LiftedCoercible, NP (Nil, (:*)), NS (S, Z), Prod, SListI, SOP (SOP), ccompare_SOP, hcliftA2, hcollapse, para_SList)
+import GHC.TypeError (ErrorMessage (Text), TypeError)
+import Generics.SOP (
+  All,
+  All2,
+  AllZipN,
+  Code,
+  I,
+  K (K),
+  LiftedCoercible,
+  NP (Nil, (:*)),
+  NS (S, Z),
+  Prod,
+  SListI,
+  SOP (SOP),
+  ccompare_SOP,
+  hcliftA2,
+  hcollapse,
+  para_SList,
+ )
 import Generics.SOP qualified as SOP
-
 import Plutarch.Builtin.Bool (PBool, pif)
 import Plutarch.Builtin.Integer (PInteger, pconstantInteger)
-
 import Plutarch.Internal.Eq (PEq, (#==))
 import Plutarch.Internal.Lift (pconstant)
 import Plutarch.Internal.Term (Dig, S, Term, plet)
 import Plutarch.Internal.TermCont (hashOpenTerm, unTermCont)
 
-newtype PStruct (struct :: [[S -> Type]]) (s :: S) = PStruct {unPStruct :: SOP (Term s) struct}
-newtype PRec (struct :: [S -> Type]) (s :: S) = PRec {unPRec :: NP (Term s) struct}
+-- | @since WIP
+newtype PStruct (struct :: [[S -> Type]]) (s :: S) = PStruct
+  { unPStruct :: SOP (Term s) struct
+  -- ^ @since WIP
+  }
 
-newtype PLetL s r as = PLetL {unPLetL :: NP (Term s) as -> (NP (Term s) as -> Term s r) -> Term s r}
+-- | @since WIP
+newtype PRec (struct :: [S -> Type]) (s :: S) = PRec
+  { unPRec :: NP (Term s) struct
+  -- ^ @since WIP
+  }
 
-pletL' :: SListI as => NP (Term s) as -> (NP (Term s) as -> Term s r) -> Term s r
-pletL' = unPLetL $ para_SList
-  (PLetL \Nil f -> f Nil)
-  \(PLetL prev) -> PLetL \(x :* xs) f -> plet x \x' ->
-    prev xs (\xs' -> f (x' :* xs'))
-
+-- | @since WIP
 pletL :: All SListI as => SOP (Term s) as -> (SOP (Term s) as -> Term s r) -> Term s r
 pletL (SOP (Z x)) f = pletL' x \x' -> f (SOP $ Z x')
 pletL (SOP (S xs)) f = pletL (SOP xs) \(SOP xs') -> f (SOP $ S xs')
-
-class
-  ( SOP.Generic (a s)
-  , AllZipN @Type (Prod SOP) (LiftedCoercible I (Term s)) (Code (a s)) struct
-  , AllZipN @Type (Prod SOP) (LiftedCoercible (Term s) I) struct (Code (a s))
-  ) =>
-  StructSameRepr s a struct
-instance
-  ( SOP.Generic (a s)
-  , AllZipN @Type (Prod SOP) (LiftedCoercible I (Term s)) (Code (a s)) struct
-  , AllZipN @Type (Prod SOP) (LiftedCoercible (Term s) I) struct (Code (a s))
-  ) =>
-  StructSameRepr s a struct
-
-type family UnTermRec (struct :: [Type]) :: [S -> Type] where
-  UnTermRec '[] = '[]
-  UnTermRec (Term _ a ': rest) = a ': UnTermRec rest
-
-type family UnTermStruct' (struct :: [[Type]]) :: [[S -> Type]] where
-  UnTermStruct' '[] = '[]
-  UnTermStruct' (x ': rest) = UnTermRec x ': UnTermStruct' rest
-
-type UnTermStruct x = UnTermStruct' (Code x)
-
---------------------------------------------------------------------------------
-
-type RecTypePrettyError' :: forall {k}. [[k]] -> Bool
-type family RecTypePrettyError' (xs :: [[k]]) :: Bool where
-  RecTypePrettyError' (_ ': '[]) = 'True
-  RecTypePrettyError' (_ ': _) =
-    TypeError
-      ('Text "Deriving record encoding only works with types with single constructor. More than one constructor is found.")
-
-type RecTypePrettyError struct = RecTypePrettyError' struct ~ 'True
-
---------------------------------------------------------------------------------
-
-pands :: [Term s PBool] -> Term s PBool
-pands [] = pconstant True
-pands [x'] = x'
-pands (x' : xs') = pif x' (pands xs') (pconstant False)
 
 -- NOTE/TODO: These will generate large blob code, not too efficient unfortunately.
 -- We are stuck with this for SOP and Scott however. Need some benchmark
 -- reason: https://github.com/IntersectMBO/plutus/pull/5440
 
+-- | @since WIP
 grecEq ::
   forall (s :: S) (struct :: [S -> Type]).
   All PEq struct =>
@@ -98,6 +75,7 @@ grecEq ::
   Term s PBool
 grecEq x y = pands $ hcollapse $ hcliftA2 (Proxy @PEq) (\a b -> K (a #== b)) x y
 
+-- | @since WIP
 gstructEq ::
   forall (s :: S) (struct :: [[S -> Type]]).
   All2 PEq struct =>
@@ -119,6 +97,8 @@ gstructEq x y =
 
 {- | This function handles optimization of function that require multiple handlers by checking hashes of each
 | handler item and merging them in a way it will minimize size and cost of all computation
+
+@since WIP
 -}
 groupHandlers :: forall (s :: S) (r :: S -> Type). [(Integer, Term s r)] -> Term s PInteger -> Term s r
 groupHandlers handlers idx = unTermCont $ do
@@ -156,9 +136,63 @@ groupHandlers handlers idx = unTermCont $ do
       pgo' ((is, t) : rest) = buildIfs is t $ pgo' rest
       pgo' [] = error "impossible"
 
-      -- So that GHC doesn't bitch
+      -- So that GHC doesn't complain
       _a = pgo'
       _b = pgo
      in
       -- first one seems to be faster
       pgo groupedHandlers
+
+-- | @since WIP
+class
+  ( SOP.Generic (a s)
+  , AllZipN @Type (Prod SOP) (LiftedCoercible I (Term s)) (Code (a s)) struct
+  , AllZipN @Type (Prod SOP) (LiftedCoercible (Term s) I) struct (Code (a s))
+  ) =>
+  StructSameRepr s a struct
+
+instance
+  ( SOP.Generic (a s)
+  , AllZipN @Type (Prod SOP) (LiftedCoercible I (Term s)) (Code (a s)) struct
+  , AllZipN @Type (Prod SOP) (LiftedCoercible (Term s) I) struct (Code (a s))
+  ) =>
+  StructSameRepr s a struct
+
+-- | @since WIP
+type family UnTermRec (struct :: [Type]) :: [S -> Type] where
+  UnTermRec '[] = '[]
+  UnTermRec (Term _ a ': rest) = a ': UnTermRec rest
+
+-- | @since WIP
+type UnTermStruct x = UnTermStruct' (Code x)
+
+-- | @since WIP
+type RecTypePrettyError struct = RecTypePrettyError' struct ~ 'True
+
+-- Helpers
+
+newtype PLetL s r as = PLetL {unPLetL :: NP (Term s) as -> (NP (Term s) as -> Term s r) -> Term s r}
+
+pletL' :: SListI as => NP (Term s) as -> (NP (Term s) as -> Term s r) -> Term s r
+pletL' = unPLetL $ para_SList
+  (PLetL \Nil f -> f Nil)
+  \(PLetL prev) -> PLetL \(x :* xs) f -> plet x \x' ->
+    prev xs (\xs' -> f (x' :* xs'))
+
+type family UnTermStruct' (struct :: [[Type]]) :: [[S -> Type]] where
+  UnTermStruct' '[] = '[]
+  UnTermStruct' (x ': rest) = UnTermRec x ': UnTermStruct' rest
+
+type RecTypePrettyError' :: forall {k}. [[k]] -> Bool
+type family RecTypePrettyError' (xs :: [[k]]) :: Bool where
+  RecTypePrettyError' (_ ': '[]) = 'True
+  RecTypePrettyError' (_ ': _) =
+    TypeError
+      ('Text "Deriving record encoding only works with types with single constructor. More than one constructor is found.")
+
+--------------------------------------------------------------------------------
+
+pands :: [Term s PBool] -> Term s PBool
+pands [] = pconstant True
+pands [x'] = x'
+pands (x' : xs') = pif x' (pands xs') (pconstant False)
