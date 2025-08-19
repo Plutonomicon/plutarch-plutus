86f6f130e0d79c26989ba63fd0898409df07434f
Szabo Gergely <gege251@mailbox.org>
2025-08-19 17:53:40 +0900

    Add prop tests for PTryFrom

--- plutarch-testlib/test/Plutarch/Test/Suite/PlutarchLedgerApi/V2.hs
+++ plutarch-testlib/test/Plutarch/Test/Suite/PlutarchLedgerApi/V2.hs
@@ -1,8 +1,11 @@
 module Plutarch.Test.Suite.PlutarchLedgerApi.V2 (tests) where
 
+import Data.Kind (Type)
 import Plutarch.LedgerApi.V2 qualified as PLA
+import Plutarch.Prelude
 import Plutarch.Test.Laws (checkLedgerProperties)
-import Plutarch.Test.Utils (fewerTests)
+import Plutarch.Test.QuickCheck (propPTryFromRoundrip)
+import Plutarch.Test.Utils (fewerTests, typeName)
 import PlutusLedgerApi.V2.Orphans ()
 import Test.Tasty (TestTree, adjustOption, testGroup)
 
@@ -10,9 +13,33 @@ tests :: TestTree
 tests =
   testGroup
     "V2"
-    [ adjustOption (fewerTests 16) $ checkLedgerProperties @PLA.PScriptContext
-    , adjustOption (fewerTests 16) $ checkLedgerProperties @PLA.PTxInfo
-    , adjustOption (fewerTests 4) $ checkLedgerProperties @PLA.PTxOut
-    , adjustOption (fewerTests 4) $ checkLedgerProperties @PLA.PTxInInfo
-    , checkLedgerProperties @PLA.POutputDatum
+    [ adjustOption (fewerTests 16) $
+        testGroup
+          (typeName @(S -> Type) @PLA.PScriptContext)
+          [ checkLedgerProperties @PLA.PScriptContext
+          , propPTryFromRoundrip @PLA.PScriptContext
+          ]
+    , adjustOption (fewerTests 16) $
+        testGroup
+          (typeName @(S -> Type) @PLA.PTxInfo)
+          [ checkLedgerProperties @PLA.PTxInfo
+          , propPTryFromRoundrip @PLA.PTxInfo
+          ]
+    , adjustOption (fewerTests 4) $
+        testGroup
+          (typeName @(S -> Type) @PLA.PTxOut)
+          [ checkLedgerProperties @PLA.PTxOut
+          , propPTryFromRoundrip @PLA.PTxOut
+          ]
+    , adjustOption (fewerTests 4) $
+        testGroup
+          (typeName @(S -> Type) @PLA.PTxInInfo)
+          [ checkLedgerProperties @PLA.PTxInInfo
+          , propPTryFromRoundrip @PLA.PTxInInfo
+          ]
+    , testGroup
+        (typeName @(S -> Type) @PLA.POutputDatum)
+        [ checkLedgerProperties @PLA.POutputDatum
+        , propPTryFromRoundrip @PLA.POutputDatum
+        ]
     ]
