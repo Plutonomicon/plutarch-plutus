2817474970b74fbd310768dec931d3d64ab42b47
Szabo Gergely <gege251@mailbox.org>
2025-09-03 15:21:27 +0900

    Minor version bump of plutarch-testlib

--- plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
+++ plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
@@ -12,30 +12,35 @@ import Plutarch.Internal.Lift (
   PLiftable,
   PLifted (PLifted),
  )
-import Plutarch.Internal.PlutusType (PlutusType)
-import Plutarch.Prelude (S)
+import Plutarch.Prelude (PEq, PlutusType, S)
 import Plutarch.Repr.Tag (DeriveAsTag (DeriveAsTag))
-import Plutarch.Test.Laws (checkPLiftableLaws, checkPLiftableLawsForDeriveTags)
+import Plutarch.Test.Laws (checkPLiftableLaws, checkPLiftableLawsForDeriveTags, checkPlutusTypeLaws)
 import Prettyprinter (Pretty (pretty))
 import Test.QuickCheck.Arbitrary (Arbitrary (arbitrary))
 import Test.QuickCheck.Gen (elements)
 import Test.Tasty (TestTree, testGroup)
 
+-- | @since 1.0.2
 data PFoo (s :: S) = A | B | C | D | E
   deriving stock (GHC.Generic, Show, Eq)
+  deriving anyclass (PEq)
   deriving
     (PlutusType, PLiftable)
     via DeriveAsTag PFoo
 
+-- | @since 1.0.2
 instance Pretty (PFoo s) where
   pretty = pretty . show
 
+-- | @since 1.0.2
 instance SOP.Generic (PFoo s)
 
+-- | @since 1.0.2
 instance Arbitrary (PFoo s) where
   {-# INLINEABLE arbitrary #-}
   arbitrary = elements [A, B, C, D, E]
 
+-- | @since 1.0.2
 tests :: TestTree
 tests =
   testGroup
@@ -43,5 +48,6 @@ tests =
     ( mconcat
         [ checkPLiftableLawsForDeriveTags @PFoo
         , checkPLiftableLaws @PFoo
+        , checkPlutusTypeLaws @PFoo
         ]
     )
