117da53974cf1a46d8d596e0baad959b01bff8ed
Koz Ross <koz.ross@retro-freedom.nz>
2025-11-18 09:59:02 +1300

    Add pparseData goldens for DeriveAsTag types

--- plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
+++ plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
@@ -12,7 +12,19 @@ import Plutarch.Internal.Lift (
   PLiftable,
   PLifted (PLifted),
  )
-import Plutarch.Prelude (PBool (PFalse, PTrue), PEq, PlutusType, S, pcon, pmatch)
+import Plutarch.Internal.Parse (PValidateData, pparseData)
+import Plutarch.Prelude (
+  PBool (PFalse, PTrue),
+  PEq,
+  PInteger,
+  PIsData,
+  PlutusType,
+  S,
+  pcon,
+  pdata,
+  pforgetData,
+  pmatch,
+ )
 import Plutarch.Repr.Tag (DeriveAsTag (DeriveAsTag))
 import Plutarch.Test.Golden (goldenEval, plutarchGolden)
 import Plutarch.Test.Laws (checkPLiftableLaws, checkPLiftableLawsForDeriveTags, checkPlutusTypeLaws)
@@ -23,46 +35,43 @@ import Test.Tasty (TestTree, testGroup)
 
 data PFoo1 (s :: S) = A1
   deriving stock (GHC.Generic, Show, Eq)
-  deriving anyclass (SOP.Generic, PEq)
+  deriving anyclass (SOP.Generic, PEq, PIsData)
   deriving
-    (PlutusType, PLiftable)
+    (PlutusType, PLiftable, PValidateData)
     via DeriveAsTag PFoo1
 
 data PFoo2 (s :: S) = A2 | B2
   deriving stock (GHC.Generic, Show, Eq)
-  deriving anyclass (SOP.Generic, PEq)
+  deriving anyclass (SOP.Generic, PEq, PIsData)
   deriving
-    (PlutusType, PLiftable)
+    (PlutusType, PLiftable, PValidateData)
     via DeriveAsTag PFoo2
 
 data PFoo3 (s :: S) = A3 | B3 | C3
   deriving stock (GHC.Generic, Show, Eq)
-  deriving anyclass (SOP.Generic, PEq)
+  deriving anyclass (SOP.Generic, PEq, PIsData)
   deriving
-    (PlutusType, PLiftable)
+    (PlutusType, PLiftable, PValidateData)
     via DeriveAsTag PFoo3
 
 data PFoo4 (s :: S) = A4 | B4 | C4 | D4
   deriving stock (GHC.Generic, Show, Eq)
-  deriving anyclass (SOP.Generic, PEq)
+  deriving anyclass (SOP.Generic, PEq, PIsData)
   deriving
-    (PlutusType, PLiftable)
+    (PlutusType, PLiftable, PValidateData)
     via DeriveAsTag PFoo4
 
 -- | @since 1.0.2
 data PFoo (s :: S) = A | B | C | D | E
   deriving stock (GHC.Generic, Show, Eq)
-  deriving anyclass (PEq)
+  deriving anyclass (PEq, PIsData, SOP.Generic)
   deriving
-    (PlutusType, PLiftable)
+    (PlutusType, PLiftable, PValidateData)
     via DeriveAsTag PFoo
 
 -- | @since 1.0.2
 instance Pretty (PFoo s) where
   pretty = pretty . show
-
--- | @since 1.0.2
-instance SOP.Generic (PFoo s)
 
 -- | @since 1.0.2
 instance Arbitrary (PFoo s) where
@@ -84,13 +93,18 @@ tests =
         "derive-as-tag"
         [ goldenEval "pcon 1" (pcon A1)
         , goldenEval "pmatch 1" (pmatch (pcon A1) (\x -> if x == A1 then pcon PTrue else pcon PFalse))
+        , goldenEval "pparseData 1" (pparseData @PFoo1 . pforgetData . pdata @PInteger $ 0)
         , goldenEval "pcon 2" (pcon B2)
         , goldenEval "pmatch 2" (pmatch (pcon B2) (\x -> if x == B2 then pcon PTrue else pcon PFalse))
+        , goldenEval "pparseData 2" (pparseData @PFoo2 . pforgetData . pdata @PInteger $ 0)
         , goldenEval "pcon 3" (pcon C3)
         , goldenEval "pmatch 3" (pmatch (pcon C3) (\x -> if x == C3 then pcon PTrue else pcon PFalse))
+        , goldenEval "pparseData 3" (pparseData @PFoo3 . pforgetData . pdata @PInteger $ 0)
         , goldenEval "pcon 4" (pcon C4)
         , goldenEval "pmatch 4" (pmatch (pcon C4) (\x -> if x == C4 then pcon PTrue else pcon PFalse))
+        , goldenEval "pparseData 4" (pparseData @PFoo4 . pforgetData . pdata @PInteger $ 0)
         , goldenEval "pcon 5" (pcon C)
         , goldenEval "pmatch 5" (pmatch (pcon D) (\x -> if x == D then pcon PTrue else pcon PFalse))
+        , goldenEval "pparseData 5" (pparseData @PFoo . pforgetData . pdata @PInteger $ 0)
         ]
     ]
