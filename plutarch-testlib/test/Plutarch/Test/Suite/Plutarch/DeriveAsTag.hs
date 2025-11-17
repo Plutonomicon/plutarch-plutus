0f118ef6e06826db7564dd07fdebd5632f506342
Koz Ross <koz.ross@retro-freedom.nz>
2025-11-18 09:44:42 +1300

    Benchmarks for pcon and pmatch on DeriveAsTag

--- plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
+++ plutarch-testlib/test/Plutarch/Test/Suite/Plutarch/DeriveAsTag.hs
@@ -21,6 +21,34 @@ import Test.QuickCheck.Arbitrary (Arbitrary (arbitrary))
 import Test.QuickCheck.Gen (elements)
 import Test.Tasty (TestTree, testGroup)
 
+data PFoo1 (s :: S) = A1
+  deriving stock (GHC.Generic, Show, Eq)
+  deriving anyclass (SOP.Generic, PEq)
+  deriving
+    (PlutusType, PLiftable)
+    via DeriveAsTag PFoo1
+
+data PFoo2 (s :: S) = A2 | B2
+  deriving stock (GHC.Generic, Show, Eq)
+  deriving anyclass (SOP.Generic, PEq)
+  deriving
+    (PlutusType, PLiftable)
+    via DeriveAsTag PFoo2
+
+data PFoo3 (s :: S) = A3 | B3 | C3
+  deriving stock (GHC.Generic, Show, Eq)
+  deriving anyclass (SOP.Generic, PEq)
+  deriving
+    (PlutusType, PLiftable)
+    via DeriveAsTag PFoo3
+
+data PFoo4 (s :: S) = A4 | B4 | C4 | D4
+  deriving stock (GHC.Generic, Show, Eq)
+  deriving anyclass (SOP.Generic, PEq)
+  deriving
+    (PlutusType, PLiftable)
+    via DeriveAsTag PFoo4
+
 -- | @since 1.0.2
 data PFoo (s :: S) = A | B | C | D | E
   deriving stock (GHC.Generic, Show, Eq)
@@ -54,7 +82,15 @@ tests =
     , plutarchGolden
         "Tag encoding and decoding"
         "derive-as-tag"
-        [ goldenEval "pcon" (pcon C)
-        , goldenEval "pmatch" (pmatch (pcon D) (\x -> if x == D then pcon PTrue else pcon PFalse))
+        [ goldenEval "pcon 1" (pcon A1)
+        , goldenEval "pmatch 1" (pmatch (pcon A1) (\x -> if x == A1 then pcon PTrue else pcon PFalse))
+        , goldenEval "pcon 2" (pcon B2)
+        , goldenEval "pmatch 2" (pmatch (pcon B2) (\x -> if x == B2 then pcon PTrue else pcon PFalse))
+        , goldenEval "pcon 3" (pcon C3)
+        , goldenEval "pmatch 3" (pmatch (pcon C3) (\x -> if x == C3 then pcon PTrue else pcon PFalse))
+        , goldenEval "pcon 4" (pcon C4)
+        , goldenEval "pmatch 4" (pmatch (pcon C4) (\x -> if x == C4 then pcon PTrue else pcon PFalse))
+        , goldenEval "pcon 5" (pcon C)
+        , goldenEval "pmatch 5" (pmatch (pcon D) (\x -> if x == D then pcon PTrue else pcon PFalse))
         ]
     ]
