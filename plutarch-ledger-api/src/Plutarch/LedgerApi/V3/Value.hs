8e8aeebc97b8b398b0f4b85055ba5fcbce81c53f
Koz Ross <koz.ross@retro-freedom.nz>
2026-04-10 14:39:04 +1200

    More conversions

--- plutarch-ledger-api/src/Plutarch/LedgerApi/V3/Value.hs
+++ plutarch-ledger-api/src/Plutarch/LedgerApi/V3/Value.hs
@@ -1,6 +1,3 @@
--- Needed to ensure that we only try to convert Value-like things
-{-# OPTIONS_GHC -Wno-redundant-constraints #-}
-
 {- | Conversions between ledger API representations of @Value@s and the new
 'PBuiltinValue', plus some utility functions.
 
@@ -55,11 +52,18 @@ module Plutarch.LedgerApi.V3.Value (
   -- * Conversions
 
   -- ** Safe
+  pfromRawValue,
+  pfromSortedValue,
   pfromMintValue,
   ptoSortedValue,
+  ptoLedgerValue,
+  ptoMintValue,
 
   -- ** Unsafe
-  punsafeFromAnyValue,
+  punsafeFromRawValue,
+  punsafeFromSortedValue,
+  punsafeToLedgerValue,
+  punsafeToMintValue,
 
   -- * Construction
   pemptyBuiltinValue,
@@ -79,23 +83,37 @@ import Plutarch.Builtin.Value (
   punValueData,
   pvalueData,
  )
+import Plutarch.LedgerApi.AssocMap (PUnsortedMap)
+import Plutarch.LedgerApi.AssocMap qualified as AssocMap
 import Plutarch.LedgerApi.V3.MintValue (PMintValue)
-import Plutarch.LedgerApi.Value (PRawValue, PSortedValue)
+import Plutarch.LedgerApi.Value (PLedgerValue, PRawValue, PSortedValue)
 import Plutarch.LedgerApi.Value.CurrencySymbol (PCurrencySymbol)
 import Plutarch.LedgerApi.Value.TokenName (PTokenName)
 import Plutarch.Prelude (
   PAsData,
+  PBool (PFalse, PTrue),
+  PBuiltinList (PCons, PNil),
+  PBuiltinPair (PBuiltinPair),
   PInteger,
-  PIsData,
-  PSubtype,
   S,
   Term,
+  pcon,
   pconstant,
+  pdata,
+  pfix,
   pforgetData,
+  pfromData,
   phoistAcyclic,
+  pif,
   plam,
+  plet,
+  pmatch,
+  pto,
   pupcast,
   (#),
+  (#$),
+  (#<),
+  (#==),
   (:-->),
  )
 import Plutarch.Unsafe (punsafeCoerce)
@@ -111,16 +129,62 @@ pfromMintValue ::
   Term s (PAsData PMintValue) -> Term s PBuiltinValue
 pfromMintValue x = punValueData # pforgetData x
 
-{- | Convert /any/ @Data@ representation of a @Value@ into a 'PBuiltinValue'.
+{- | After verifying the conversion is safe, convert a 'PRawValue' @Data@
+representation into a 'PBuiltinValue'. To save on allocations, this is
+written in a continuation-passing style:
+
+* The second argument is what should happen if validation fails; and
+* The third argument is what should happen if validation succeeds.
+
+= Important note
+
+This operation is slow, as it has to check the entire input.
+
+@since wip
+-}
+pfromRawValue ::
+  forall (r :: S -> Type) (s :: S).
+  Term s PRawValue ->
+  Term s r ->
+  Term s (PBuiltinValue :--> r) ->
+  Term s r
+pfromRawValue x whenFail whenSucceed =
+  pif (pcanBeBuiltinValue # x) whenFail (whenSucceed #$ punValueData # pforgetData (pdata x))
+
+{- | Convert the @Data@ representation of a 'PRawValue' into a 'PBuiltinValue'
+while not doing any checks.
+
 This is /not/ safe, and will error if any of the invariants of
 'PBuiltinValue' are violated. Use with care.
 
 @since wip
 -}
-punsafeFromAnyValue ::
-  forall (a :: S -> Type) (s :: S).
-  (PSubtype a PRawValue, PIsData a) => Term s (PAsData a) -> Term s PBuiltinValue
-punsafeFromAnyValue x = punValueData # pforgetData x
+punsafeFromRawValue ::
+  forall (s :: S).
+  Term s (PAsData PRawValue) -> Term s PBuiltinValue
+punsafeFromRawValue x = punValueData # pforgetData x
+
+{- | As 'pfromRawValue', except for 'PSortedValue's instead. This is more
+efficient, as we only need to check for the absence of zero amounts.
+
+@since wip
+-}
+pfromSortedValue ::
+  forall (r :: S -> Type) (s :: S).
+  Term s PSortedValue ->
+  Term s r ->
+  Term s (PBuiltinValue :--> r) ->
+  Term s r
+pfromSortedValue x whenFail whenSucceed =
+  pif (pnoZeroAmounts # x) whenFail (whenSucceed #$ punValueData # pforgetData (pdata x))
+
+{- | As 'punsafeFromRawValue', except for 'PSortedValue's instead. The same
+caveats apply.
+
+@since wip
+-}
+punsafeFromSortedValue :: forall (s :: S). Term s (PAsData PRawValue) -> Term s PBuiltinValue
+punsafeFromSortedValue x = punValueData # pforgetData x
 
 {- | Convert a 'PBuiltinValue' into the @Data@ representation of a
 'PSortedValue'. This is done via a builtin (thus efficient), and is safe.
@@ -133,6 +197,56 @@ ptoSortedValue ::
   forall (s :: S).
   Term s PBuiltinValue -> Term s (PAsData PSortedValue)
 ptoSortedValue v = punsafeCoerce $ pvalueData # v
+
+{- | Checks for an Ada entry in the input 'PBuiltinValue'. If none is found,
+produce the second argument; otherwise, convert the 'PBuiltinValue' into a
+'PAsData' 'PLedgerValue' and call the third argument with it.
+
+@since wip
+-}
+ptoLedgerValue ::
+  forall (r :: S -> Type) (s :: S).
+  Term s PBuiltinValue ->
+  Term s r ->
+  Term s (PAsData PLedgerValue :--> r) ->
+  Term s r
+ptoLedgerValue v whenInvalid whenValid =
+  pif (plovelaceValueOf # v #== 0) whenInvalid (whenValid # punsafeCoerce (pvalueData # v))
+
+{- | As 'ptoLedgerValue', except the check is for the /absence/ of an Ada entry,
+and the third argument is called with a 'PAsData' 'PMintValue' instead when
+appropriate.
+
+@since wip
+-}
+ptoMintValue ::
+  forall (r :: S -> Type) (s :: S).
+  Term s PBuiltinValue ->
+  Term s r ->
+  Term s (PAsData PMintValue :--> r) ->
+  Term s r
+ptoMintValue v whenInvalid whenValid =
+  pif (plovelaceValueOf # v #== 0) (whenValid # punsafeCoerce (pvalueData # v)) whenInvalid
+
+{- | Convert a 'PBuiltinValue' into a 'PAsData' 'PLedgerValue' without checking
+anything. This will error if the input does not contain an Ada entry.
+
+@since wip
+-}
+punsafeToLedgerValue ::
+  forall (s :: S).
+  Term s PBuiltinValue -> Term s (PAsData PLedgerValue)
+punsafeToLedgerValue v = punsafeCoerce $ pvalueData # v
+
+{- | Convert a 'PBuiltinValue' into a 'PAsData' 'PLedgerValue' without checking
+anything. This will error if the input contains an Ada entry.
+
+@since wip
+-}
+punsafeToMintValue ::
+  forall (s :: S).
+  Term s PBuiltinValue -> Term s (PAsData PMintValue)
+punsafeToMintValue v = punsafeCoerce $ pvalueData # v
 
 {- | The 'PBuiltinValue' without any amounts.
 
@@ -197,3 +311,85 @@ padjustAmount ::
   Term s (PCurrencySymbol :--> PTokenName :--> PInteger :--> PBuiltinValue :--> PBuiltinValue)
 padjustAmount = phoistAcyclic $ plam $ \cs tn delta v ->
   pinsertCoin # pupcast cs # pupcast tn # delta # v
+
+-- Helpers
+
+pcanBeBuiltinValue :: forall (s :: S). Term s (PRawValue :--> PBool)
+pcanBeBuiltinValue = phoistAcyclic $ plam $ \v -> pmatch (pto (pto (pto v))) $ \case
+  PNil -> pcon PTrue
+  PCons x xs -> pmatch x $ \case
+    PBuiltinPair prevK innerMap ->
+      pif
+        (verifyInner # innerMap)
+        ( pmatch xs $ \case
+            PNil -> pcon PTrue
+            PCons y ys -> goOuter # pfromData prevK # y # ys
+        )
+        (pcon PFalse)
+  where
+    verifyInner ::
+      forall (s' :: S).
+      Term s' (PAsData (PUnsortedMap PTokenName PInteger) :--> PBool)
+    verifyInner = phoistAcyclic $ plam $ \innerMap -> pmatch (pto (pto (pfromData innerMap))) $ \case
+      -- Invalid to have an empty inner map
+      PNil -> pcon PFalse
+      PCons x xs -> pmatch x $ \case
+        PBuiltinPair prevK amount ->
+          pif
+            (pfromData amount #== 0)
+            (pcon PFalse)
+            ( pmatch xs $ \case
+                PNil -> pcon PTrue
+                PCons y ys -> goInner # pfromData prevK # y # ys
+            )
+    goOuter ::
+      forall (s' :: S).
+      Term
+        s'
+        ( PCurrencySymbol
+            :--> PBuiltinPair (PAsData PCurrencySymbol) (PAsData (PUnsortedMap PTokenName PInteger))
+            :--> PBuiltinList (PBuiltinPair (PAsData PCurrencySymbol) (PAsData (PUnsortedMap PTokenName PInteger)))
+            :--> PBool
+        )
+    goOuter = phoistAcyclic $ pfix $ \self -> plam $ \prevK curr rest -> pmatch curr $ \case
+      PBuiltinPair currK currV -> plet (pfromData currK) $ \currK' ->
+        pif
+          (prevK #< currK')
+          ( pif
+              (verifyInner # currV)
+              ( pmatch rest $ \case
+                  PNil -> pcon PTrue
+                  PCons y ys -> self # currK' # y # ys
+              )
+              (pcon PFalse)
+          )
+          (pcon PFalse)
+    goInner ::
+      forall (s' :: S).
+      Term
+        s'
+        ( PTokenName
+            :--> PBuiltinPair (PAsData PTokenName) (PAsData PInteger)
+            :--> PBuiltinList (PBuiltinPair (PAsData PTokenName) (PAsData PInteger))
+            :--> PBool
+        )
+    goInner = phoistAcyclic $ pfix $ \self -> plam $ \prevK curr rest -> pmatch curr $ \case
+      PBuiltinPair currK currV -> plet (pfromData currK) $ \currK' ->
+        pif
+          (prevK #< currK')
+          ( pif
+              (pfromData currV #== 0)
+              (pcon PFalse)
+              ( pmatch rest $ \case
+                  PNil -> pcon PTrue
+                  PCons y ys -> self # currK' # y # ys
+              )
+          )
+          (pcon PFalse)
+
+pnoZeroAmounts :: forall (s :: S). Term s (PSortedValue :--> PBool)
+pnoZeroAmounts = phoistAcyclic $ plam $ \v -> AssocMap.pall # go # AssocMap.pforgetSorted (pto v)
+  where
+    go :: forall (s' :: S). Term s' (AssocMap.PSortedMap PTokenName PInteger :--> PBool)
+    go = phoistAcyclic $ plam $ \m ->
+      AssocMap.pall # plam (\i -> pif (i #== 0) (pcon PFalse) (pcon PTrue)) # AssocMap.pforgetSorted m
