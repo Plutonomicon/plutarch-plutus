71cd45a2c49738e654ae5033a47a7f174424d8e8
Koz Ross <koz.ross@retro-freedom.nz>
2026-07-01 07:25:13 +1200

    Zeroable

---
+++ Plutarch/Numeric/Zeroable.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE UndecidableSuperClasses #-}
+
+module Plutarch.Numeric.Zeroable (
+  -- * Type class
+  PZeroable (..),
+
+  -- * Type
+  PNZInteger,
+) where
+
+import Data.Kind (Type)
+import Plutarch.Backend.S (S)
+import Plutarch.Backend.Term (Term, punsafeCoerce)
+import Plutarch.Numeric.Additive (PAdditiveMonoid)
+import Plutarch.Numeric.Helpers (pizero)
+import Plutarch.Numeric.Multiplicative (PMultiplicativeMonoid, PMultiplicativeSemigroup)
+import Plutarch.Primitive.Apply (PlutarchType (PRepresentation), pcoerce, (#))
+import Plutarch.Primitive.Bool (pif)
+import Plutarch.Primitive.BuiltinFun (pequalsInteger)
+import Plutarch.Primitive.Function ((:-->))
+import Plutarch.Primitive.Numeric (PInteger, PNatural, PPositive)
+
+-- | @since wip
+class
+  (PAdditiveMonoid a, PlutarchType (PNonZero a), PRepresentation (PNonZero a) ~ a) =>
+  PZeroable (a :: S -> Type)
+  where
+  type PNonZero a :: S -> Type
+  ptoNonZero ::
+    forall (r :: S -> Type) (s :: S).
+    Term s a ->
+    Term s r ->
+    Term s (PNonZero a :--> r) ->
+    Term s r
+
+-- | @since wip
+instance PZeroable PNatural where
+  type PNonZero PNatural = PPositive
+  ptoNonZero x whenZero whenNot =
+    pif
+      (pequalsInteger # pcoerce x # pizero)
+      whenZero
+      (whenNot # punsafeCoerce x)
+
+-- | @since wip
+data PNZInteger (s :: S)
+
+type role PNZInteger nominal
+
+-- | @since wip
+instance PlutarchType PNZInteger where
+  type PRepresentation PNZInteger = PInteger
+
+-- | @since wip
+instance PMultiplicativeSemigroup PNZInteger
+
+-- | @since wip
+instance PMultiplicativeMonoid PNZInteger
+
+-- | @since wip
+instance PZeroable PInteger where
+  type PNonZero PInteger = PNZInteger
+  ptoNonZero x whenZero whenNot =
+    pif
+      (pequalsInteger # x # pizero)
+      whenZero
+      (whenNot # punsafeCoerce x)
