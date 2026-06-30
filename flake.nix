0ad4d6d3fd77fcb5d225eb4859d0841108d41b0a
Koz Ross <koz.ross@retro-freedom.nz>
2026-07-01 07:42:38 +1200

    Retarget documentation branch

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       ];
       debug = true;
       systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      hercules-ci.github-pages.branch = "staging";
+      hercules-ci.github-pages.branch = "master";
 
       perSystem = { config, system, lib, self', ... }:
         let
