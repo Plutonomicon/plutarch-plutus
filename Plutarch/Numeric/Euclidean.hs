6ce5a0c89eb24027ec19f66cb18700c7220a442f
Koz Ross <koz.ross@retro-freedom.nz>
2026-07-08 10:34:08 +1200

    Laws

--- Plutarch/Numeric/Euclidean.hs
+++ Plutarch/Numeric/Euclidean.hs
@@ -31,7 +31,19 @@ import Plutarch.Primitive.Function ((:-->))
 import Plutarch.Primitive.Numeric (PInteger, PNatural)
 import PlutusCore qualified as PLC
 
--- | @since wip
+{- | = Laws
+
+1. @(('pdiv' # x # y) #* 'pcoerce' y) #+ ('pmod' # x # y)@ @=@ @x@
+2. @'pdiv' # 'pzero' # x@ @=@ @'pmod' # 'pzero' # x@ @=@ @'pzero'@
+3. @'pgcd' # pcoerce x # x@ @=@ @'pcoerce' x@
+4. @'pgcd' # ('pmod' # x # y) # y@ @=@ @'pgcd' # x # y@
+
+Additionally, 'pgcd' should be /morally/ commutative and associative. It is
+not possible to state these laws in general due to the 'PNonZero' requirement
+for the second argument.
+
+@since wip
+-}
 class (PZeroable a, PMultiplicativeMonoid a) => PEuclidean (a :: S -> Type) where
   pdiv :: Term s (a :--> PNonZero a :--> a)
   pmod :: Term s (a :--> PNonZero a :--> a)
