68686d587c777afbba18749966e32219b7b8b7a7
Koz Ross <koz.ross@retro-freedom.nz>
2026-08-06 12:11:29 +1200

    Tracing

---
+++ Plutarch/Primitive/Trace.hs
@@ -0,0 +1,43 @@
+module Plutarch.Primitive.Trace (
+  ptraceDebug,
+  ptraceError,
+) where
+
+import Control.Monad.Reader (ask)
+import Data.Kind (Type)
+import Plutarch.Backend.S (S)
+import Plutarch.Backend.Term (
+  Term (Term),
+  TermEnv (TermEnv),
+  TracingMode (DebugTracing, NoTracing),
+  asRawTerm,
+ )
+import Plutarch.Primitive.Apply (PlutarchType, (#))
+import Plutarch.Primitive.BuiltinFun (ptrace)
+import Plutarch.Primitive.String (PString)
+
+-- | @since wip
+ptraceDebug ::
+  forall (a :: S -> Type) (s :: S).
+  PlutarchType a =>
+  Term s PString ->
+  Term s a ->
+  Term s a
+ptraceDebug msg t = Term $ do
+  TermEnv mode <- ask
+  asRawTerm $ case mode of
+    DebugTracing -> ptrace # msg # t
+    _ -> t
+
+-- | @since wip
+ptraceError ::
+  forall (a :: S -> Type) (s :: S).
+  PlutarchType a =>
+  Term s PString ->
+  Term s a ->
+  Term s a
+ptraceError msg t = Term $ do
+  TermEnv mode <- ask
+  asRawTerm $ case mode of
+    NoTracing -> t
+    _ -> ptrace # msg # t
