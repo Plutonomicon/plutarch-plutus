a67273cda2b5b9ebc1f8505e93a16d8d5e530ee6
Koz Ross <koz.ross@retro-freedom.nz>
2026-08-10 11:12:24 +1200

    More tests

--- test/Debug.hs
+++ test/Debug.hs
@@ -8,6 +8,8 @@ import Plutarch.Backend.Term (
  )
 import Plutarch.Primitive.Bool (ptrue)
 import Plutarch.Primitive.Debug (pshow)
+import Plutarch.Primitive.Liftable (pconstant)
+import Plutarch.Primitive.Numeric (PByte, PInteger)
 import Plutarch.Primitive.String (PString)
 import Plutarch.Test.Golden (
   plutarchGoldenEvalWith,
@@ -23,9 +25,18 @@ goldens =
     , plutarchGoldenEvalWith debugTermEnv "pshow Bool" "Debug Case 1" case1
     , plutarchGoldenWith releaseTermEnv "pshow Bool" "Debug Case 1 tracing off" case1
     , plutarchGoldenEvalWith releaseTermEnv "pshow Bool" "Debug Case 1 tracing off" case1
+    , plutarchGoldenWith debugTermEnv "pshow Integer" "Debug Case 2" case2
+    , --    , plutarchGoldenEvalWith debugTermEnv "pshow Integer" "Debug Case 2" case2
+      plutarchGoldenWith debugTermEnv "pshow Byte" "Debug Case 3" case3
     ]
 
 -- Cases
 
 case1 :: forall (s :: S). Term s PString
 case1 = pshow ptrue
+
+case2 :: forall (s :: S). Term s PString
+case2 = pshow . pconstant @PInteger $ -12345
+
+case3 :: forall (s :: S). Term s PString
+case3 = pshow . pconstant @PByte $ 0x1F
